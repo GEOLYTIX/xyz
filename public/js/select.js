2c4305a8c1b8419b49eeb69ce8299b2a73c57d76
Agata Brok <agata.brok@geolytix.co.uk>
2018-02-12 15:59:31 +0000

    Merging with images branch - partial.

--- public/js/select.js
+++ public/js/select.js
@@ -297,6 +297,57 @@ module.exports = function Select(){
 
         // Add table element to the container.
         record.layer.drawer.appendChild(table);
+        
+        // Show images 
+        function addImages(record){
+            //console.log(record);
+            let id = record.letter,
+                feature = record.layer.qID,
+                images = record.layer.infoj.images.reverse() || [];
+            
+            let _tr = document.createElement('tr'),
+                _img_tr = document.createElement('tr'),
+                _td = document.createElement('td'),
+                _div = document.createElement('div');
+            
+            _img_tr.id = 'img-container_' + id;
+            _div.classList = 'img-container';
+            _td.classList = 'lv-0';
+            _td.colSpan = '2';
+            
+            // add images if there are any
+            if(images.length){
+                for(let image of images){
+                    let _img_td = document.createElement('td'),
+                        _img = document.createElement('img');
+                    
+                    _img.src =  localhost + 'images/' + image;
+                    _img_td.appendChild(_img);
+                    _img_tr.appendChild(_img_td);
+                }
+            }
+            // add image picker
+            let add_img = document.createElement('input'),
+                add_td = document.createElement('td'),
+                add_tr = document.createElement('tr');
+            
+            add_img.type = 'file';
+            add_img.id = 'images_' + id;
+            add_img.name = 'images_' + id;
+            add_img.accept = 'images/*';
+            
+            add_td.colSpan = '2';
+            
+            add_td.appendChild(add_img);
+            add_tr.appendChild(add_td);
+            
+            _div.appendChild(_img_tr);
+            _td.appendChild(_div);
+            _tr.appendChild(_td);
+            table.appendChild(_tr);
+            
+            table.appendChild(add_tr);
+        }
 
         // Filter empty features from select.layers;
         let freeRecords = _xyz.select.records.filter(function (record) {
@@ -310,42 +361,44 @@ module.exports = function Select(){
 
         // Populate the table from the features infoj object.
         Object.keys(record.layer.infoj).map(function (key) {
-            (function processDataRow(lv, key, val) {
-
-                if (typeof val === 'object') {
-                    if (key.charAt(key.length -1) != '_') addToTable(lv, key, '');
-                    Object.keys(val).map(function (key) {
-                        processDataRow(lv + 1, key, val[key]);
-                    });
-                } else {
-                    addToTable(lv, key, val);
-                }
-
-                function addToTable(lv, key, val) {
-                    let tr = document.createElement('tr');
-                    let td = document.createElement('td');
-                    td.className = 'lv-' + lv;
-                    td.textContent = key;
-                    tr.appendChild(td);
-
-                    if (isNaN(val)) {
-                        table.appendChild(tr);
-                        tr = document.createElement('tr');
-                        td = document.createElement('td');
-                        td.textContent = val;
-                        td.colSpan = '2';
-
+            if(key === 'images'){
+                addImages(record);
+                //console.log(record.layer.infoj[key]);
+            } else {
+                (function processDataRow(lv, key, val) {
+                    if (typeof val === 'object') {
+                        if (key.charAt(key.length -1) != '_') addToTable(lv, key, '');
+                        Object.keys(val).map(function (key) {
+                            processDataRow(lv + 1, key, val[key]);
+                        });
                     } else {
-                        td = document.createElement('td');
-                        td.textContent = val;
+                        addToTable(lv, key, val);
                     }
-
-                    td.className = 'val';
-                    tr.appendChild(td);
-                    table.appendChild(tr);
-                }
-
-            })(0, key, record.layer.infoj[key])
+                    
+                    function addToTable(lv, key, val) {
+                        let tr = document.createElement('tr');
+                        let td = document.createElement('td');
+                        td.className = 'lv-' + lv;
+                        td.textContent = key;
+                        tr.appendChild(td);
+                        
+                        if (isNaN(val)) {
+                            table.appendChild(tr);
+                            tr = document.createElement('tr');
+                            td = document.createElement('td');
+                            td.textContent = val;
+                            td.colSpan = '2';
+                        } else {
+                            td = document.createElement('td');
+                            td.textContent = val;
+                        }
+                        
+                        td.className = 'val';
+                        tr.appendChild(td);
+                        table.appendChild(tr);
+                    }
+                })(0, key, record.layer.infoj[key])
+            }
         });
     }
 }
