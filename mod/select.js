3c7aed17a6825fda2be9bfead50c3db45a53d644
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2018-04-26 14:11:26 +0100

    locus

--- mod/select.js
+++ mod/select.js
@@ -19,11 +19,11 @@ async function select(req, res) {
     req.body.infoj.forEach(entry => {
         if (entry.layer) {
             fields += `
-            (SELECT ${entry.aggregate}(${entry.field})
+            (SELECT ${entry.field.split('.')[0]}(${entry.field.split('.')[1]})
              FROM ${entry.layer.table}
              WHERE ST_Intersects(${entry.layer.table}.${entry.layer.geom || 'geom'}, ${table}.${geom})
              ${sql_filter}
-            ) AS ${entry.field},
+            ) AS "${entry.field}",
             `
             return
         }
@@ -50,14 +50,25 @@ async function select(req, res) {
     global.DBS[req.body.dbs].query(q, [id])
         .then(result => {
 
-            Object.keys(req.body.infoj).map(key => {
-                if (result.rows[0][req.body.infoj[key].field] || result.rows[0][req.body.infoj[key].field] == 0) {
-                    req.body.infoj[key].value = result.rows[0][req.body.infoj[key].field];
+
+            // Object.keys(req.body.infoj).map(key => {
+            //     if (result.rows[0][req.body.infoj[key].field] || result.rows[0][req.body.infoj[key].field] == 0) {
+            //         req.body.infoj[key].value = result.rows[0][req.body.infoj[key].field];
+            //     }
+            //     if (result.rows[0][req.body.infoj[key].subfield]) {
+            //         req.body.infoj[key].subvalue = result.rows[0][req.body.infoj[key].subfield];
+            //     }
+            // });
+
+            Object.values(req.body.infoj).map(entry => {
+                if (result.rows[0][entry.field] || result.rows[0][entry.field] == 0) {
+                    entry.value = result.rows[0][entry.field];
                 }
-                if (result.rows[0][req.body.infoj[key].subfield]) {
-                    req.body.infoj[key].subvalue = result.rows[0][req.body.infoj[key].subfield];
+                if (result.rows[0][entry.subfield]) {
+                    entry.subvalue = result.rows[0][entry.subfield];
                 }
             });
+
 
             res.status(200).json({
                 geomj: result.rows[0].geomj,
