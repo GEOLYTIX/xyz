4089b10979a5ebf8f7800aee454816badb857460
Agata Brok <agata.brok@geolytix.co.uk>
2018-05-21 17:17:03 +0100

    Filters - UI in progress.

--- public/js/layers_filters.js
+++ public/js/layers_filters.js
@@ -18,6 +18,240 @@ function applyFilters(layer){
 }
 
 function layerFilters(layer){
+    let filters = utils.createElement('div', {
+        classList: 'section expandable'
+    });
+    
+    utils._createElement({
+        tag: 'div',
+        options: {
+            className: 'btn_text cursor noselect',
+            textContent: 'Filtering'
+        },
+        appendTo: filters,
+        eventListener: {
+            event: 'click',
+            funct: e => {
+                e.stopPropagation();
+                utils.toggleExpanderParent({
+                    expandable: filters,
+                    accordeon: true,
+                    scrolly: document.querySelector('.mod_container > .scrolly')
+                })
+            }
+        }
+    });
+    
+    let block, title, content, remove_filter;
+    
+    function select_onchange(e){
+        let _val = this.value, _select = this;
+        
+        _select.options[_select.selectedIndex].disabled = true;
+        
+        function enable_option(select, val){
+            for(let opt of select.options){
+                if(opt.value === val) opt.disabled = false;
+            }
+        }
+        
+        function remove_filter_onclick(e){
+            e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode);
+            delete layer.filter[_val];
+            enable_option(_select, _val);
+            layer.getLayer();
+        }
+        
+        Object.keys(layer.infoj).map(function(key){
+            if(layer.infoj[key].filter && layer.infoj[key].field === _val){
+                
+                if(typeof(layer.infoj[key].filter) === "object"){
+                    
+                    block = utils.createElement('div', {
+                        classList: "block"
+                    });
+                
+                    title = utils.createElement('div', {
+                        innerHTML: layer.infoj[key].label + '<i class="material-icons">clear</i>',
+                        classList: "title",
+                        onclick: remove_filter_onclick
+                    }, block);
+                    
+                    Object.keys(layer.infoj[key].filter).map(function(_key){
+                        
+                        for(let val of layer.infoj[key].filter[_key]){
+                            
+                            let index = layer.infoj[key].filter[_key].indexOf(val);
+                            
+                            let options = {
+                                field: layer.infoj[key].field,
+                                operator: 'in',
+                                value: val
+                            }
+                            
+                            content = filter_checkbox(options, layer);
+                            block.appendChild(content);
+                        }
+                    });
+                    filters.appendChild(block);
+                
+                } else {
+                    
+                    if(layer.infoj[key].filter === "numeric"){
+                        
+                        block = utils.createElement('div', {
+                            classList: "block"
+                        });
+                        
+                        title = utils.createElement('div', {
+                            //textContent: layer.infoj[key].label,
+                            innerHTML: layer.infoj[key].label + '<i class="material-icons">clear</i>',
+                            classList: "title",
+                            onclick: remove_filter_onclick
+                        }, block);
+                        
+                        let options = {
+                            field: layer.infoj[key].field,
+                            label: layer.infoj[key].label,
+                            appendTo: block
+                        }
+                        
+                        filter_numeric(layer, options); 
+                        filters.appendChild(block);
+                    }
+                    
+                    if(layer.infoj[key].filter === "like" || layer.infoj[key].filter ==="match"){
+                        
+                        block = utils.createElement('div', {
+                            classList: "block"
+                        });
+                        
+                        title = utils.createElement('div', {
+                            //textContent: layer.infoj[key].label,
+                            innerHTML: layer.infoj[key].label + '<i class="material-icons">clear</i>',
+                            classList: "title",
+                            onclick: remove_filter_onclick
+                        }, block);
+                        
+                        let options = {
+                            field: layer.infoj[key].field,
+                            label: layer.infoj[key].label,
+                            appendTo: block
+                        }
+                        
+                        options.operator = layer.infoj[key].filter;
+                        
+                        filter_text(layer, options);
+                        filters.appendChild(block);
+                    }
+                    
+                    if(layer.infoj[key].filter === "date"){
+                        block = utils.createElement('div', {
+                            classList: "block"
+                        });
+                        
+                        title = utils.createElement('div', {
+                            //textContent: layer.infoj[key].label,
+                            classList: "title",
+                            innerHTML: layer.infoj[key].label + '<i class="material-icons">clear</i>',
+                            onclick: remove_filter_onclick
+                        }, block);
+                        
+                        let options = {
+                            field: layer.infoj[key].field,
+                            label: layer.infoj[key].label,
+                            appendTo: block
+                        }
+                        
+                        // filter date function
+                        filter_date(layer, options);
+                        filters.appendChild(block);
+                    }
+                }
+            }
+        });
+        //this.value = null;
+        this.selectedIndex = 0;
+    }
+    
+    let select = utils.createElement('select', {
+        onchange: select_onchange,
+        innerHTML: "<option selected>Select filter from list.</option>"
+    });
+    
+    Object.keys(layer.infoj).map(function(key){
+        if(layer.infoj[key].filter){
+            utils.createElement("option", {
+                value: layer.infoj[key].field,
+                textContent: layer.infoj[key].label
+            }, select);
+        }
+    });
+    
+    
+    function run_onclick(e){
+        //console.log('apply filter to layer');
+        layer.xhr.open('GET', host + 'q_aggregate?' + utils.paramString({
+            dbs: layer.dbs,
+            table_source: layer.table,
+            table_target: _xyz.locales[_xyz.locale].layers[layer.aggregate_layer].table,
+            filter: JSON.stringify(layer.filter),
+            geom_target: undefined,
+            geom_source: undefined
+        }));
+        
+        layer.xhr.onload = function(){
+            let json = JSON.parse(this.response);
+            //console.log(json);
+            if(this.status === 200){
+                _xyz.select.selectLayerFromEndpoint({
+                            layer: layer.aggregate_layer,
+                            table: _xyz.locales[_xyz.locale].layers[layer.aggregate_layer].table,
+                            id: json.id,
+                            marker: [json.lng, json.lat],
+                            filter: JSON.stringify(layer.filter)
+                        });
+            }
+        };
+        layer.xhr.send();
+        
+    }
+    
+    /*let reset_onclick = function(){
+        console.log('reset all filters');
+        /*let siblings = options.appendTo.children;
+        for(let sibling of siblings){
+            if(sibling.tagName === 'INPUT'){
+                sibling.value = '';
+            }
+        }
+        this.style.display = "none";
+        layer.filter[options.field] = {};
+        layer.getLayer();*/
+    /*}
+    
+    let reset = utils.createElement('div', {
+        classList: "btn_small cursor noselect",
+        textContent: "Reset",
+        onclick: reset_onclick
+    }, filters);*/
+    
+    filters.appendChild(select);
+    
+    let run = utils.createElement('div', {
+        classList: "btn_wide cursor noselect",
+        onclick: run_onclick,
+        textContent: "Run Output"
+    });
+    
+    run.style.display = "block";
+    //run.style.display = "none";
+    filters.appendChild(run);
+    
+    return filters;
+}
+
+/*function layerFilters2(layer){
     
     let filters = utils.createElement('div', {
         classList: 'section expandable'
@@ -148,7 +382,7 @@ function layerFilters(layer){
         
     });
     
-    function apply_to_area_onclick(e){
+    function run_onclick(e){
         //console.log('apply filter to layer');
         layer.xhr.open('GET', host + 'q_aggregate?' + utils.paramString({
             dbs: layer.dbs,
@@ -176,17 +410,17 @@ function layerFilters(layer){
         
     }
     
-    let apply_to_area = utils.createElement('div', {
+    let run = utils.createElement('div', {
         classList: "btn_wide cursor noselect",
-        onclick: apply_to_area_onclick,
+        onclick: run_onclick,
         textContent: "Run Output"
     });
     
-    apply_to_area.style.display = "block";
-    filters.appendChild(apply_to_area);
+    run.style.display = "block";
+    filters.appendChild(run);
 
     return filters;
-}
+}*/
 
 // create text filter
 function filter_text(layer, options){
