402970817ff9fa96be1cca83653b33b71782d085
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2018-05-25 19:40:09 +0100

    waterline for users and appsettings

--- server.js
+++ server.js
@@ -18,74 +18,76 @@ if (process.env.NODE_ENV === 'cluster') {
 
     if (cluster.isMaster) {
         console.log(`Master ${process.pid} is running`);
-    
+
         // Fork workers.
         for (let i = 0; i < numCPUs; i++) {
             cluster.fork();
         }
-    
+
         cluster.on('exit', (worker, code, signal) => {
             console.log(`worker ${worker.process.pid} died`);
         });
-        
-    } 
-}
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    const app = express();
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'ejs');
-
-    app.use(helmet.noCache());
-
-    // Set globals for API keys, Database connections, etc.
-    global.appRoot = path.resolve(__dirname);
-    global.site = (process.env.HOST || ('localhost:' + (process.env.PORT || '3000'))) + process.env.DIR;
-
-    global.KEYS = {};
-    Object.keys(process.env).map(key => {
-        if (key.split('_')[0] === 'KEY') {
-            global.KEYS[key.split('_')[1]] = process.env[key];
-        }
-    });
-
-    const { Client } = require('pg');
-    global.DBS = {};
-    Object.keys(process.env).map(key => {
-        if (key.split('_')[0] === 'DBS') {
-            global.DBS[key.split('_')[1]] = new Client({ connectionString: process.env[key] });
-
-            // connect reconnect issue
-            global.DBS[key.split('_')[1]].connect();
-        }
-    });
-
-    app.use(process.env.DIR || '', express.static(path.join(__dirname, 'public')));
-    if (morgan) app.use(morgan('dev'));
-    app.use(cookieParser());
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
-    
-     // for blobs that contain image
-    app.use(bodyParser.raw({limit: '50mb'}));
-
-    // Set app to use session with secret provided in environment settings.
-    if (process.env.OURSECRET) app.use(session({
-        secret: process.env.OURSECRET,
-        resave: true,
-        saveUninitialized: true
-    }));
-
-    if (process.env.LOGIN) {
-        const passport = require('./mod/' + 'passport');
-        app.use(passport.initialize());
-        app.use(passport.session());
     }
-
-    app.use(process.env.DIR || '', require('./router'));
-
-    process.on('unhandledRejection', err => console.error(err));
-
-    app.listen(port);
-    console.log('The magic happens on port ' + port);
 }
+
+
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(helmet.noCache());
+
+// Set globals for API keys, Database connections, etc.
+global.appRoot = path.resolve(__dirname);
+global.site = (process.env.HOST || ('localhost:' + (process.env.PORT || '3000'))) + (process.env.DIR || '');
+
+global.KEYS = {};
+Object.keys(process.env).map(key => {
+    if (key.split('_')[0] === 'KEY') {
+        global.KEYS[key.split('_')[1]] = process.env[key];
+    }
+});
+
+const { Client } = require('pg');
+global.DBS = {};
+Object.keys(process.env).map(key => {
+    if (key.split('_')[0] === 'DBS') {
+        global.DBS[key.split('_')[1]] = new Client({ connectionString: process.env[key] });
+
+        // connect reconnect issue
+        global.DBS[key.split('_')[1]].connect();
+    }
+});
+
+app.use(process.env.DIR || '', express.static(path.join(__dirname, 'public')));
+if (morgan) app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// for blobs that contain image
+app.use(bodyParser.raw({ limit: '50mb' }));
+
+// Set app to use session with secret provided in environment settings.
+if (process.env.OURSECRET) app.use(session({
+    secret: process.env.OURSECRET,
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Initialise waterline ORM for appsettings and user profiles.
+require('./waterline');
+
+if (process.env.LOGIN) {
+    const security = require('./security');
+    app.use(security.passport.initialize());
+    app.use(security.passport.session());
+}
+
+app.use(process.env.DIR || '', require('./router'));
+
+process.on('unhandledRejection', err => console.error(err));
+
+app.listen(port);
+console.log('The magic happens on port ' + port);
