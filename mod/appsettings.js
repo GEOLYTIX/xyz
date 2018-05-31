b55a4e792288ca1db91d8743cea5bf83b52aab05
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2018-05-31 16:47:16 +0100

    added restrictions key to remove objects from appsettings;

--- mod/appsettings.js
+++ mod/appsettings.js
@@ -1,4 +1,4 @@
-async function getAppSettings() {
+async function getAppSettings(req) {
 
     let settings = {};
 
@@ -21,6 +21,8 @@ async function getAppSettings() {
             }
         }
     };
+
+    if (req.user) settings = await removeRestrictions(settings, req);
 
     await setAppSettingsValues(settings);
 }
@@ -54,13 +56,41 @@ async function getSettingsFromFile() {
         JSON.parse(fs.readFileSync('./settings/' + process.env.APPSETTINGS.split(':').pop()), 'utf8') : {};
 }
 
+function removeRestrictions(settings, req) {
+    (function objectEval(o, parent, key) {
+
+        // check whether the object has restrictions.
+        if (checkForRestrictions(o)) {
+            // if the parent is an array splice the key index.
+            if (parent.length > 0) return parent.splice(parseInt(key),1);
+
+            // if the parent is an object delete the key from the parent.
+            return delete parent[key];
+        };
+
+        // iterate through the object tree.
+        Object.keys(o).forEach((key) => {
+            if (o[key] && typeof o[key] === 'object') objectEval(o[key], o, key);
+        })
+    })(settings)
+
+    function checkForRestrictions(o) {
+        return Object.entries(o).some((e) => {
+            // check whether an entry has 'restriction' as key and the value is not a member of the user object.
+            return e[0] === 'restriction' && !req.user[e[1]]
+        })
+    };
+
+    return settings;
+}
+
 function setAppSettingsValues(settings) {
     global.appSettings = settings;
 
     // Store all string keys in global array to check for SQL injections.
     global.appSettingsValues = [];
     (function objectEval(o) {
-        Object.keys(o).map(function (key) {
+        Object.keys(o).forEach((key) => {
             if (typeof key === 'string') global.appSettingsValues.push(key);
             if (typeof o[key] === 'string') global.appSettingsValues.push(o[key]);
             if (o[key] && typeof o[key] === 'object') objectEval(o[key]);
