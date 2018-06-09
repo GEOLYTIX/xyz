e7dcd5400900101c06f5993e0704bb03df248f4d
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2018-06-09 14:31:07 +0100

    fastify test

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
+process.on('unhandledRejection', err => console.error(err));
+
 const req_res = (m) => { try { return require.resolve(m) } catch (e) { console.log('Cannot resolve ' + m); return false } }
 
 const dotenv = req_res('dotenv') ? require('dotenv') : null;
 if (dotenv) dotenv.load();
 
 const port = process.env.PORT || 3000;
-const express = require('express');
+
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const helmet = require('helmet');
-const morgan = req_res('morgan') ? require('morgan') : null;
 
-const app = express();
-app.set('views',  path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(helmet.noCache());
 
 // Set globals for API keys, Database connections, etc.
 global.appRoot = path.resolve(__dirname);
@@ -29,45 +21,37 @@ Object.keys(process.env).map(key => {
     }
 });
 
-const { Client } = require('pg');
-global.DBS = {};
-Object.keys(process.env).map(key => {
-    if (key.split('_')[0] === 'DBS') {
-        global.DBS[key.split('_')[1]] = new Client({ connectionString: process.env[key] });
+// const { Client } = require('pg');
+// global.DBS = {};
+// Object.keys(process.env).map(key => {
+//     if (key.split('_')[0] === 'DBS') {
+//         global.DBS[key.split('_')[1]] = new Client({ connectionString: process.env[key] });
 
-        // connect reconnect issue
-        global.DBS[key.split('_')[1]].connect();
-    }
-});
+//         // connect reconnect issue
+//         global.DBS[key.split('_')[1]].connect();
+//     }
+// });
 
-app.use(process.env.DIR || '', express.static(path.join(__dirname, 'public')));
-if (morgan) app.use(morgan('dev'));
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-// for blobs that contain image
-app.use(bodyParser.raw({ limit: '50mb' }));
-
-// Set app to use session with secret provided in environment settings.
-app.use(session({
-    secret: process.env.OURSECRET || 'ChinaCatSunflower',
-    resave: true,
-    saveUninitialized: true
-}));
 
 // Initialise waterline ORM for appsettings and user profiles.
-require('./waterline');
+// require('./waterline');
 
-if (process.env.LOGIN || process.env.ADMIN) {
-    const security = require('./security');
-    app.use(security.passport.initialize());
-    app.use(security.passport.session());
-}
 
-app.use(process.env.DIR || '', require('./router'));
+// Require the framework and instantiate it
+const fastify = require('fastify')()
 
-process.on('unhandledRejection', err => console.error(err));
+// Declare a route
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'world' })
+});
 
-app.listen(port);
-console.log('The magic happens on port ' + port);
+// Run the server!
+fastify.listen(port, function (err) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+
+  console.log('The magic happens on port ' + port);
+});
