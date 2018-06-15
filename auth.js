bf6e2ad0643fea90e57f1a2cc8b02519a616afec
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2018-06-15 21:33:24 +0100

    fastify auth ready for testing

--- auth.js
+++ auth.js
@@ -13,15 +13,28 @@ function routes(fastify) {
         });
     }
 
+    function loginView(req, res, msg) {
+        res
+            .type('text/html')
+            .send(require('jsrender')
+                .templates('./views/login.html')
+                .render({ dir: global.dir, msg: msg }));
+    }
+
+    fastify.route({
+        method: 'GET',
+        url: global.dir + '/logout',
+        handler: (req, res) => {
+            req.session = {};
+            loginView(req, res, 'Logged out from application.');
+        }
+    });
+
     fastify.route({
         method: 'GET',
         url: global.dir + '/login',
         handler: (req, res) => {
-            res
-                .type('text/html')
-                .send(require('jsrender')
-                    .templates('./views/login.html')
-                    .render({ dir: global.dir }))
+            loginView(req, res);
         }
     });
 
@@ -30,25 +43,31 @@ function routes(fastify) {
         url: global.dir + '/login',
         handler: async (req, res) => {
 
-            let user_db = await fastify.pg.users.connect();
-
+            var user_db = await fastify.pg.users.connect();
             result = await user_db.query(
                 `SELECT * FROM ${user_table} WHERE email = $1;`,
                 [req.body.email]
             );
-
             user_db.release();
 
-            if (require('bcrypt-nodejs').compareSync(req.body.password, result.rows[0].password)) {
+            let user = result.rows[0];
+
+            if (!user) return loginView(req, res, 'User account not found in access control list.');
+
+            if (!user.verified) return loginView(req, res, 'User account email not yet verified.');
+
+            if (!user.approved) return loginView(req, res, 'User account not yet approved by site administrator.');
+
+            if (require('bcrypt-nodejs').compareSync(req.body.password, user.password)) {
                 req.session.user = {
-                    email: result.rows[0].email,
-                    verified: result.rows[0].verified,
-                    approved: result.rows[0].approved,
-                    admin: result.rows[0].admin
+                    email: user.email,
+                    verified: user.verified,
+                    approved: user.approved,
+                    admin: user.admin
                 };
-                res.redirect(req.session.redirect);
+                return res.redirect(req.session.redirect || global.dir || '/');
             } else {
-                res.send({ result: 'farts' });
+                return loginView(req, res, 'Wrong password.');
             }
         }
     });
@@ -73,12 +92,10 @@ function routes(fastify) {
             const email = req.body.email;
 
             var user_db = await fastify.pg.users.connect();
-
             result = await user_db.query(
                 `SELECT * FROM ${user_table} WHERE email = $1;`,
                 [email]
             );
-
             user_db.release();
 
             let user = result.rows[0];
@@ -89,7 +106,6 @@ function routes(fastify) {
             // set password for existing user and remove existing verification.
             if (user) {
                 var user_db = await fastify.pg.users.connect();
-
                 await user_db.query(`
                 UPDATE ${user_table} SET
                     verified = false,
@@ -98,45 +114,42 @@ function routes(fastify) {
                 WHERE email = $1;`, [email]);
                 user_db.release();
 
-                require('./mailer').mail({
+                require('./mailer')({
                     to: user.email,
                     subject: `Please verify your GEOLYTIX account (password reset) on ${global.site}`,
                     text: `A new password has been set for this account. \n \n`
-                        + `Please verify that you are the account holder: ${req.protocol}://${global.site}/verify/${verificationtoken}`
+                        + `Please verify that you are the account holder: ${global.protocol}://${global.site}/admin/user/verify/${verificationtoken}`
                 });
 
-                return;
+                return loginView(req, res, `You have reset the password <br />`
+                + `A verification mail has been sent to your registered address. <br />`
+                + `Please follow the link in the email to verify that you are the account holder. <br />`
+                + `You must verify your account before you are able to login with your new password.`);
             }
 
             var user_db = await fastify.pg.users.connect();
             await user_db.query(`
             INSERT INTO ${user_table} (email, password, verificationtoken)
             SELECT
-                ${email} AS email,
-                ${password} AS password,
-                ${verificationtoken} AS verificationtoken;`);
+                '${email}' AS email,
+                '${password}' AS password,
+                '${verificationtoken}' AS verificationtoken;`);
             user_db.release();
 
-            require('./mailer').mail({
-                to: user.email,
-                subject: `Please verify your GEOLYTIX account on ${global.site}`,
-                text: `A new account for this email address has been registered with ${global.site}. \n \n`
-                    + `Please verify that you are the account holder: ${req.protocol}://${global.site}/verify/${verificationtoken} \n \n`
+            require('./mailer')({
+                to: email,
+                subject: `Please verify your GEOLYTIX account on ${req.headers.host}${global.dir}`,
+                text: `A new account for this email address has been registered with ${req.headers.host}${global.dir} \n \n`
+                    + `Please verify that you are the account holder: ${global.protocol}://${req.headers.host}${global.dir}/admin/user/verify/${verificationtoken} \n \n`
                     + `A site administrator must approve the account before you are able to login. \n \n`
                     + `You will be notified via email once an adimistrator has approved your account.`
             });
 
-            return;
-
-        }
-    });
-
-    fastify.route({
-        method: 'GET',
-        url: global.dir + '/logout',
-        handler: (req, res) => {
-            req.session = {};
-            res.redirect(global.dir || '/');
+            loginView(req, res, `We registered your email address in the access control list. <br />`
+                + `A verification mail has been sent to your registered address. <br />`
+                + `Please follow the link in the email to verify that you are the account holder. <br />`
+                + `A site administrator must approve the account before you are able to login. <br />`
+                + `You will be notified via email once an adimistrator has approved your account.`);
         }
     });
 
@@ -191,8 +204,8 @@ function routes(fastify) {
                 if (req.body.role === 'approved' && req.body.chk)
                     await require('./mailer')({
                         to: req.body.email,
-                        subject: `This account has been approved for ${req.headers.origin}`,
-                        text: `You are now able to log on to ${req.headers.origin}`
+                        subject: `This account has been approved for ${req.headers.host}${global.dir}`,
+                        text: `You are now able to log on to ${global.protocol}://${req.headers.host}${global.dir}`
                     });
 
                 if (update.rowCount === 0) res.code(500).send();
@@ -219,98 +232,117 @@ function routes(fastify) {
 
                     await require('./mailer')({
                         to: req.body.email,
-                        subject: `This ${req.headers.origin} account has been deleted.`,
-                        text: `You will no longer be able to log in to ${req.headers.origin}`
+                        subject: `This ${req.headers.host}${global.dir} account has been deleted.`,
+                        text: `You will no longer be able to log in to ${global.protocol}://${req.headers.host}${global.dir}`
                     });
 
                     res.code(200).send()
                 };
 
             }
-        });        
+        });
 
         // Check verification token and verify account
         fastify.route({
             method: 'GET',
-            url: global.dir + '/verify/:token',
-            beforeHandler: fastify.auth([fastify.authAdmin]),
+            url: global.dir + '/admin/user/verify/:token',
             handler: async (req, res) => {
 
-                // Find user account from matching token and timestamp.
-                let user = await global.ORM.collections.users.findOne({ verificationToken: req.params.token });
+                // Find user account from matching token.
+                var user_db = await fastify.pg.users.connect();
+                var result = await user_db.query(
+                    `SELECT * FROM ${user_table} WHERE verificationtoken = $1;`,
+                    [req.params.token]
+                );
+                user_db.release();
 
-                // Return if user account is not found.
+                user = result.rows[0];
+
                 if (!user) return res.send('Token not found.');
 
-                // Return if user account is not found.
-                if (user.verified) return res.send('User account has already been verfied.');
+                if (user.verified && user.admin) return res.redirect(global.dir || '/');
 
-                user = await global.ORM.collections.users
-                    .update({ email: user.email })
-                    .set({
-                        verified: true,
-                        verificationToken: require('crypto').randomBytes(20).toString('hex')
-                    })
-                    .fetch();
+                if (user.verified) return res.send('User account has already been verfied but is still awaiting administrator approval.');
 
-                user = user[0];
+                if (!user.approved){
+                    let approvaltoken = require('crypto').randomBytes(20).toString('hex');
 
-                // Get all admin accounts from the ORM users collection.
-                let admins = await global.ORM.collections.users.find({ admin: true });
-
-                if (admins.length === 0) {
-                    req.session.messages = ['No admin accounts were found'];
-                    return res.send('No admin accounts were found.');
+                    var user_db = await fastify.pg.users.connect();
+                    let update = await user_db.query(`
+                        UPDATE ${user_table} SET
+                            verified = true,
+                            approvaltoken = '${approvaltoken}'
+                        WHERE email = $1;`, [user.email]);
+                    user_db.release();
+    
+                    // Get all admin accounts from the ACL.
+                    var user_db = await fastify.pg.users.connect();
+                    var result = await user_db.query(`
+                        SELECT email
+                        FROM ${user_table}
+                        WHERE admin = true;`);
+                    user_db.release();
+    
+                    if (result.rowCount === 0) return res.send('No admin accounts were found.');
+    
+                    // Create an array of all admin account emails.
+                    let adminmail = result.rows.map(admin => admin.email);
+    
+                    // Sent an email to all admin account emails with a request to approve the new user account.
+                    require('./mailer')({
+                        to: adminmail,
+                        subject: `A new account has been verified on ${req.headers.host}${global.dir}`,
+                        text: `Please log into the admin panel ${global.protocol}://${req.headers.host}${global.dir}/admin/user to approve ${user.email} \n \n`
+                            + `You can also approve the account by following this link: ${global.protocol}://${req.headers.host}${global.dir}/admin/user/approve/${approvaltoken}`
+                    });
                 }
 
-                // Create an array of all admin account emails.
-                let adminmail = admins.map(admin => admin.email);
+                var user_db = await fastify.pg.users.connect();
+                let update = await user_db.query(`
+                    UPDATE ${user_table} SET
+                        verified = true
+                    WHERE email = $1;`, [user.email]);
+                user_db.release();
 
-                // Sent an email to all admin account emails with a request to approve the new user account.
-                require('./mailer').mail({
-                    to: adminmail,
-                    subject: `A new account has been verified on ${global.site}`,
-                    text: `Please log into the admin panel to approve ${user.email} \n \n`
-                        + `You can also approve the account by following this link: ${req.protocol}://${global.site}/approve/${user.verificationToken}`
-                });
+                if (user.approved) return res.redirect(global.dir || '/');
 
-                // Update session messages and send success notification to client.
-                req.session.messages = ['An email has been sent to the site administrator'];
                 res.send('The account has been verified and is awaiting administrator approval.');
-
             }
         });
 
         // Check verification token and approve account
         fastify.route({
             method: 'GET',
-            url: global.dir + '/approve/:token',
+            url: global.dir + '/admin/user/approve/:token',
             beforeHandler: fastify.auth([fastify.authAdmin]),
             handler: async (req, res) => {
 
-                // Find user account from matching token and timestamp.
-                let user = await global.ORM.collections.users
-                    .findOne({ verificationToken: req.params.token });
+                var user_db = await fastify.pg.users.connect();
+                var result = await user_db.query(
+                    `SELECT * FROM ${user_table} WHERE approvaltoken = $1;`,
+                    [req.params.token]
+                );
+                user_db.release();
 
-                // Return if the token is not found.
+                user = result.rows[0];
+
                 if (!user) return res.send('Token not found.');
 
-                user = await global.ORM.collections.users
-                    .update({ email: user.email })
-                    .set({ approved: true })
-                    .fetch();
+                var user_db = await fastify.pg.users.connect();
+                let update = await user_db.query(`
+                                    UPDATE ${user_table} SET
+                                        approved = true,
+                                        approvaltoken = '${user.approvaltoken}'
+                                    WHERE email = $1;`, [user.email]);
+                user_db.release();
 
-                user = user[0];
-
-                // Sent an email to the account holder to notify about the changes.
-                require('./mailer').mail({
+                require('./mailer')({
                     to: user.email,
-                    subject: `This account has been approved on ${global.site}`,
-                    text: `You are now able to log on to ${req.protocol}://${global.site}`
+                    subject: `This account has been approved on ${req.headers.host}${global.dir}`,
+                    text: `You are now able to log on to ${global.protocol}://${req.headers.host}${global.dir}`
                 });
 
                 res.send('The account has been approved by you. An email has been sent to the account holder.');
-
             }
         });
     }
