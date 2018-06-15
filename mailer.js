88d78c68ca8170c76b21d5f2c20cf7e2ef37001e
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2018-06-15 12:40:33 +0100

    admin panel;

--- mailer.js
+++ mailer.js
@@ -1,14 +1,10 @@
-function mail(options) {
+module.exports = mail => {
     if (!process.env.TRANSPORT) return console.log('Transport not set.');
 
     require('nodemailer').createTransport(process.env.TRANSPORT).sendMail({
         from: `\<${process.env.TRANSPORT.split(':')[1]}\>`,
-        to: options.to,
-        subject: options.subject,
-        text: options.text
+        to: mail.to,
+        subject: mail.subject,
+        text: mail.text
     });
 }
-
-module.exports = {
-    mail: mail
-};
