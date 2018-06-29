602456bebbd25c96ad1e7c68540e0cc23c13fbd1
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2018-06-29 01:59:07 +0100

    image upload

--- mod/images.js
+++ mod/images.js
@@ -2,11 +2,10 @@ const crypto = require('crypto');
 const images = process.env.IMAGES ? process.env.IMAGES.split(' ') : [];
 
 function save(req, res, fastify){
-    req.setEncoding('binary');
 
     let ts = Date.now(),
         sig = crypto.createHash('sha1').update(`folder=${images[4]}&timestamp=${ts}${images[2]}`).digest('hex');
-    
+   
     require('request').post({
         url: `https://api.cloudinary.com/v1_1/${images[3]}/image/upload`,
         body: {
@@ -20,22 +19,13 @@ function save(req, res, fastify){
     },
     async (err, response, body) => {
 
-        if(err){
-            console.error(err);
-            return
-        }
+        if(err) return console.error(err);
 
-        let q,
-            table = req.query.table,
+        let table = req.query.table,
             qID = req.query.qID == 'undefined' ? 'id' : req.query.qID,
             id = req.query.id;
-
-        // Check whether string params are found in the settings to prevent SQL injections.
-        //if (await require('./chk').chkVals([table, qID], res).statusCode === 406) return;
-
-        //if (await require('./chk').chkID(id, res).statusCode === 406) return;
-        
-        q = `
+       
+        var q = `
         UPDATE ${table} SET
             images = array_append(images, '${body.secure_url}')
         WHERE ${qID} = $1;`;
@@ -69,29 +59,17 @@ function remove(req, res, fastify){
     },
     async (err, response, body) => {
 
-        if (err) {
-            console.error(err);
-            return
-        }
+        if (err) return console.error(err);
 
-        let q,
-            table = req.query.table,
+        let table = req.query.table,
             qID = req.query.qID == 'undefined' ? 'id' : req.query.qID,
             id = req.query.id
             image_src = decodeURIComponent(req.query.image_src);
 
-        // Check whether string params are found in the settings to prevent SQL injections.
-        //if (await require('./chk').chkVals([table, qID], res).statusCode === 406) return;
-
-        //if (await require('./chk').chkID(id, res).statusCode === 406) return;
-
-        q = `
+        var q = `
         UPDATE ${table} SET
             images = array_remove(images, '${image_src}')
         WHERE ${qID} = $1;`;
-
-        // add filename to images field
-        `SELECT mvt FROM ${tilecache} WHERE z = ${z} AND x = ${x} AND y = ${y}`
 
         var db_connection = await fastify.pg[req.query.dbs].connect();
         await db_connection.query(q, [id]);
