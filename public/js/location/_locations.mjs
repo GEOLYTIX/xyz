20dfd90a8071ff54d6fcdad309f1288946bd342c
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2018-10-01 12:45:26 +0100

    location _def

--- public/js/location/_locations.mjs
+++ public/js/location/_locations.mjs
@@ -1,5 +1,7 @@
 import _xyz from '../_xyz.mjs';
 
+import * as _def from './_def.mjs';
+
 import addInfojToList from './table.mjs';
 
 import * as controls from './controls.mjs';
@@ -59,67 +61,7 @@ export default () => {
     // Create recordset if it doesn't exist yet.
     if (!_xyz.ws.select) _xyz.ws.select = {};
     
-    if (!_xyz.ws.select.records) _xyz.ws.select.records = [
-        {
-          letter: 'A',
-          color: '#9c27b0'
-        },
-        {
-          letter: 'B',
-          color: '#2196f3'
-        },
-        {
-          letter: 'C',
-          color: '#009688'
-        },
-        {
-          letter: 'D',
-          color: '#cddc39'
-        },
-        {
-          letter: 'E',
-          color: '#ff9800'
-        },
-        {
-          letter: 'F',
-          color: '#673ab7'
-        },
-        {
-          letter: 'G',
-          color: '#03a9f4'
-        },
-        {
-          letter: 'H',
-          color: '#4caf50'
-        },
-        {
-          letter: 'I',
-          color: '#ffeb3b'
-        },
-        {
-          letter: 'J',
-          color: '#ff5722'
-        },
-        {
-          letter: 'K',
-          color: '#0d47a1'
-        },
-        {
-          letter: 'L',
-          color: '#00bcd4'
-        },
-        {
-          letter: 'M',
-          color: '#8bc34a'
-        },
-        {
-          letter: 'N',
-          color: '#ffc107'
-        },
-        {
-          letter: 'O',
-          color: '#d32f2f'
-        }];
+    if (!_xyz.ws.select.records) _xyz.ws.select.records = _def.records;
 
     /*let filters = {};
     if(_xyz.hooks.filter) _xyz.hooks.select.split(',').forEeach(hook => {
