20d72bfacda479ac138a3632df785aca9f2f2bbe
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2018-10-08 15:03:11 +0100

    add eslint and lint utils.mjs

--- public/js/utils.mjs
+++ public/js/utils.mjs
@@ -2,273 +2,273 @@ export {default as svg_symbols} from './svg_symbols.mjs';
 
 // Find the index of node in childNodes of parentNode.
 export function indexInParent(node) {
-    if (!node) return -1;
+  if (!node) return -1;
 
-    let children = node.parentNode.childNodes,
-        num = 0;
+  let children = node.parentNode.childNodes,
+    num = 0;
 
-    for (let i = 0; i < children.length; i++) {
-        if (children[i] === node) return num;
-        if (children[i].nodeType === 1) num++;
-    }
+  for (let i = 0; i < children.length; i++) {
+    if (children[i] === node) return num;
+    if (children[i].nodeType === 1) num++;
+  }
 }
 
 // Debounce function.
 export function debounce(func, wait) {
-    let timeout;
+  let timeout;
 
-    return function () {
-        clearTimeout(timeout);
-        timeout = setTimeout(function () {
-            timeout = null;
-            func.apply(this, arguments);
-        }, wait);
-    };
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      timeout = null;
+      func.apply(this, arguments);
+    }, wait);
+  };
 }
 
 // Create param string for XHR request.
 export function paramString(param) {
-    let encodedString = '';
+  let encodedString = '';
 
-    Object.keys(param).forEach(key => {
-        if (param[key] && encodedString.length > 0) encodedString += '&';
-        if (param[key]) encodedString += encodeURI(key + '=' + param[key]);
-    });
+  Object.keys(param).forEach(key => {
+    if (param[key] && encodedString.length > 0) encodedString += '&';
+    if (param[key]) encodedString += encodeURI(key + '=' + param[key]);
+  });
 
-    return encodedString;
+  return encodedString;
 }
 
 // Function which expands the parent container of an expander element.
 export function toggleExpanderParent(params) {
 
-    if (!params.expandedTag) params.expandedTag = 'expanded';
-    if (!params.expandableTag) params.expandableTag = 'expandable';
+  if (!params.expandedTag) params.expandedTag = 'expanded';
+  if (!params.expandableTag) params.expandableTag = 'expandable';
 
-    // Check whether parent is expanded.
-    if (params.expandable.classList.contains(params.expandedTag)) {
+  // Check whether parent is expanded.
+  if (params.expandable.classList.contains(params.expandedTag)) {
 
-        // Remove expanded class.
-        params.expandable.classList.remove(params.expandedTag);
+    // Remove expanded class.
+    params.expandable.classList.remove(params.expandedTag);
 
-        // Actualize scrollbar of scrolly element.
-        if (params.scrolly) setTimeout(() => scrolly(params.scrolly), 400);
+    // Actualize scrollbar of scrolly element.
+    if (params.scrolly) setTimeout(() => scrolly(params.scrolly), 400);
 
-        return
-    }
+    return;
+  }
 
-    // Accordion: Collapse the parents siblings which are expanded.
-    if (params.accordeon) {
-        [...params.expandable.parentElement.children].forEach(expandable_sibling => {
-            expandable_sibling.classList.remove(params.expandedTag);
-            if (params.scrolly) setTimeout(() => scrolly(params.scrolly), 400);
-        });
-    }
+  // Accordion: Collapse the parents siblings which are expanded.
+  if (params.accordeon) {
+    [...params.expandable.parentElement.children].forEach(expandable_sibling => {
+      expandable_sibling.classList.remove(params.expandedTag);
+      if (params.scrolly) setTimeout(() => scrolly(params.scrolly), 400);
+    });
+  }
 
-    // Add expanded class to expandable element.
-    if (params.expandable.classList.contains(params.expandableTag)) {
-        params.expandable.classList.add(params.expandedTag);
-        if (params.scrolly) setTimeout(() => scrolly(params.scrolly), 400);
-    };
+  // Add expanded class to expandable element.
+  if (params.expandable.classList.contains(params.expandableTag)) {
+    params.expandable.classList.add(params.expandedTag);
+    if (params.scrolly) setTimeout(() => scrolly(params.scrolly), 400);
+  }
 }
 
 // Method to apply a left hand scroll bar to a container element.
 export function scrolly(el) {
 
-    let track = el.querySelector('.scrolly_track'),
-        bar = el.querySelector('.scrolly_bar');
+  let track = el.querySelector('.scrolly_track'),
+    bar = el.querySelector('.scrolly_bar');
 
+  bar.style.height = track.clientHeight * el.clientHeight / el.scrollHeight + 'px';
+  bar.style.top = track.clientHeight * el.scrollTop / el.scrollHeight + 'px';
+
+  // Update the bar when the scrolly element is scrolled.
+  el.addEventListener('scroll', () => {
     bar.style.height = track.clientHeight * el.clientHeight / el.scrollHeight + 'px';
     bar.style.top = track.clientHeight * el.scrollTop / el.scrollHeight + 'px';
+  });
 
-    // Update the bar when the scrolly element is scrolled.
-    el.addEventListener('scroll', () => {
-        bar.style.height = track.clientHeight * el.clientHeight / el.scrollHeight + 'px';
-        bar.style.top = track.clientHeight * el.scrollTop / el.scrollHeight + 'px';
-    });
+  // Event when the bar is clicked.
+  bar.addEventListener('mousedown', e => {
 
-    // Event when the bar is clicked.
-    bar.addEventListener('mousedown', e => {
+    e.preventDefault();
 
-        e.preventDefault();
+    let bar_offsetTop = bar.offsetTop,
+      e_pageY = e.pageY;
 
-        let bar_offsetTop = bar.offsetTop,
-            e_pageY = e.pageY;
+    // Update the scroll position of scrolly element and the position of the bar when mouse is moved in y direction.
+    let onMove = e => {
+      bar.style.top = Math.min(
+        track.clientHeight - bar.clientHeight,
+        Math.max(0, bar_offsetTop + e.pageY - e_pageY)) + 'px';
 
-        // Update the scroll position of scrolly element and the position of the bar when mouse is moved in y direction.
-        let onMove = e => {
-            bar.style.top = Math.min(
-                track.clientHeight - bar.clientHeight,
-                Math.max(0, bar_offsetTop + e.pageY - e_pageY)) + 'px';
+      el.scrollTop = (el.scrollHeight * bar.offsetTop / track.clientHeight);
+    };
 
-            el.scrollTop = (el.scrollHeight * bar.offsetTop / track.clientHeight);
-        };
+    document.addEventListener('mousemove', onMove);
 
-        document.addEventListener('mousemove', onMove);
-
-        document.addEventListener('mouseup', () => document.removeEventListener('mousemove', onMove));
-    });
+    document.addEventListener('mouseup', () => document.removeEventListener('mousemove', onMove));
+  });
 }
 
 // Create temporary textarea to copy string to clipboard.
 export function copyToClipboard(str) {
-    let textArea = document.createElement('textarea');
-    textArea.style.visibility = 'none';
-    textArea.value = str;
-    document.body.appendChild(textArea);
-    textArea.select();
-    document.execCommand('copy');
-    textArea.remove();
+  let textArea = document.createElement('textarea');
+  textArea.style.visibility = 'none';
+  textArea.value = str;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand('copy');
+  textArea.remove();
 }
 
 // Dom element factory.
 export function createElement(param) {
-    let el = document.createElement(param.tag);
+  let el = document.createElement(param.tag);
 
-    if (param.options) Object.keys(param.options).forEach(key => el[key] = param.options[key]);
+  if (param.options) Object.keys(param.options).forEach(key => el[key] = param.options[key]);
 
-    if (param.style) Object.keys(param.style).forEach(key => el.style[key] = param.style[key]);
+  if (param.style) Object.keys(param.style).forEach(key => el.style[key] = param.style[key]);
 
-    if (param.appendTo) param.appendTo.appendChild(el);
+  if (param.appendTo) param.appendTo.appendChild(el);
 
-    if (param.eventListener) el.addEventListener(param.eventListener.event, param.eventListener.funct);
+  if (param.eventListener) el.addEventListener(param.eventListener.event, param.eventListener.funct);
 
-    return el;
+  return el;
 }
 
 // Checkbox factory.
 export function checkbox(param) {
 
-    let checkbox = createElement({
-        tag: 'label',
-        options: {
-            textContent: param.label,
-            className: 'checkbox'
-        },
-        appendTo: param.appendTo
-    });
+  let checkbox = createElement({
+    tag: 'label',
+    options: {
+      textContent: param.label,
+      className: 'checkbox'
+    },
+    appendTo: param.appendTo
+  });
 
-    let input = createElement({
-        tag: 'input',
-        options: {
-            type: 'checkbox'
-        },
-        appendTo: checkbox
-    });
+  let input = createElement({
+    tag: 'input',
+    options: {
+      type: 'checkbox'
+    },
+    appendTo: checkbox
+  });
 
-    createElement({
-        tag: 'div',
-        options: {
-            className: 'checkbox_i'
-        },
-        appendTo: checkbox
-    });
+  createElement({
+    tag: 'div',
+    options: {
+      className: 'checkbox_i'
+    },
+    appendTo: checkbox
+  });
 
-    if (param.checked) input.checked = true;
+  if (param.checked) input.checked = true;
 
-    if (typeof (param.onChange) === 'function') input.addEventListener('change', param.onChange);
+  if (typeof (param.onChange) === 'function') input.addEventListener('change', param.onChange);
 }
 
 // Slider factory.
 export function slider(param) {
 
-    createElement({
-        tag: 'span',
-        options: {
-            textContent: param.title
-        },
-        appendTo: param.appendTo
-    });
+  createElement({
+    tag: 'span',
+    options: {
+      textContent: param.title
+    },
+    appendTo: param.appendTo
+  });
 
-    createElement({
-        tag: 'span',
-        options: {
-            textContent: param.default,
-            className: 'bold'
-        },
-        appendTo: param.appendTo
-    });
+  createElement({
+    tag: 'span',
+    options: {
+      textContent: param.default,
+      className: 'bold'
+    },
+    appendTo: param.appendTo
+  });
 
-    let range_div = createElement({
-        tag: 'div',
-        options: {
-            className: 'range'
-        },
-        appendTo: param.appendTo
-    });
+  let range_div = createElement({
+    tag: 'div',
+    options: {
+      className: 'range'
+    },
+    appendTo: param.appendTo
+  });
 
-    createElement({
-        tag: 'input',
-        options: {
-            type: 'range',
-            min: param.min,
-            value: param.value,
-            max: param.max,
-            oninput: param.oninput,
-        },
-        appendTo: range_div
-    });
+  createElement({
+    tag: 'input',
+    options: {
+      type: 'range',
+      min: param.min,
+      value: param.value,
+      max: param.max,
+      oninput: param.oninput,
+    },
+    appendTo: range_div
+  });
 }
 
 export function dropdown(param) {
 
-    if (param.title) createElement({
-        tag: 'div',
+  if (param.title) createElement({
+    tag: 'div',
+    options: {
+      textContent: param.title
+    },
+    appendTo: param.appendTo
+  });
+
+  let select = createElement({
+    tag: 'select',
+    appendTo: param.appendTo
+  });
+
+  if (param.entries.length) {
+
+    // Create select options from entries Array.
+    param.entries.forEach(entry => {
+      createElement({
+        tag: 'option',
         options: {
-            textContent: param.title
+          // Assign first value as text if entry is object.
+          textContent: typeof (entry) == 'object' ? Object.values(entry)[0] : entry,
+          // Assign first key as value if entry is object.
+          value: typeof (entry) == 'object' ? Object.keys(entry)[0] : entry
         },
-        appendTo: param.appendTo
+        appendTo: select
+      });
     });
 
-    let select = createElement({
-        tag: 'select',
-        appendTo: param.appendTo
+  } else {
+
+    // Create select options from Object if length is undefined.
+    Object.keys(param.entries).forEach(entry => {
+      createElement({
+        tag: 'option',
+        options: {
+          textContent: param.entries[entry][param.label] || entry,
+          value: param.entries[entry][param.val] || entry
+        },
+        appendTo: select
+      });
     });
+  }
 
-    if (param.entries.length) {
+  select.disabled = (select.childElementCount === 1);
 
-        // Create select options from entries Array.
-        param.entries.forEach(entry => {
-            createElement({
-                tag: 'option',
-                options: {
-                    // Assign first value as text if entry is object.
-                    textContent: typeof (entry) == 'object' ? Object.values(entry)[0] : entry,
-                    // Assign first key as value if entry is object.
-                    value: typeof (entry) == 'object' ? Object.keys(entry)[0] : entry
-                },
-                appendTo: select
-            })
-        });
+  select.onchange = param.onchange;
 
-    } else {
+  select.selectedIndex = getSelectOptionsIndex(select, param.selected);
 
-        // Create select options from Object if length is undefined.
-        Object.keys(param.entries).forEach(entry => {
-            createElement({
-                tag: 'option',
-                options: {
-                    textContent: param.entries[entry][param.label] || entry,
-                    value: param.entries[entry][param.val] || entry
-                },
-                appendTo: select
-            })
-        });
-    }
-
-    select.disabled = (select.childElementCount === 1);
-
-    select.onchange = param.onchange;
-
-    select.selectedIndex = getSelectOptionsIndex(select, param.selected);
-
-    // Get the index of the selected option from the select element.
-    function getSelectOptionsIndex(select, value) {
-        if (!value) return 0;
+  // Get the index of the selected option from the select element.
+  function getSelectOptionsIndex(select, value) {
+    if (!value) return 0;
         
-        for (let i = 0; i < select.length; i++) {
-            if (select[i].value == value) return i;
-        }
-    
-        return -1;
+    for (let i = 0; i < select.length; i++) {
+      if (select[i].value == value) return i;
     }
+    
+    return -1;
+  }
 }
