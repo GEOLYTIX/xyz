db591a0d3aae39902e7319865b9db2d64522efe7
Agata Brok <agatbr@gmail.com>
2018-12-27 17:57:31 +0100

    Added modules for HERE endoint.

--- public/js/layer/panel/draw/_draw.mjs
+++ public/js/layer/panel/draw/_draw.mjs
@@ -10,6 +10,8 @@ import point from './point.mjs';
 
 import catchment from './catchment.mjs';
 
+import isoline from './isoline.mjs';
+
 import polygon from './polygon.mjs';
 
 import finish from './finish.mjs';
@@ -125,6 +127,96 @@ export default layer => {
       activate: catchment,
       finish: finish
     });
+  }
+
+  if(layer.edit.isolines){
+
+    // set defaults
+    layer.edit.isolines = {
+      type: 'fastest',
+      rangetype: 'time'
+    };
+
+    let block = _xyz.utils.createElement({
+      tag: 'div',
+      options: {
+        classList: 'block',
+        padding: '5px'
+      },
+      appendTo: layer.edit.panel
+    });
+
+    _xyz.utils.dropdown({
+      title: 'Transport mode',
+      entries: {
+        'Driving': 'car',
+        'Walking': 'pedestrian',
+        'Public transport': 'publicTransport',
+        'Public transport live': 'publicTransportTimetable',
+        'Cargo': 'truck',
+        'High occupancy vehicle lane': 'carHOV',
+        'Cycling': 'bicycle'
+      },
+      onchange: e => {
+        layer.edit.isolines.mode = e.target.value;
+      },
+      appendTo: block
+    });
+    
+    _xyz.utils.dropdown({
+      title: 'Routing type',
+      entries: {
+        'Fastest': 'fastest',
+        'Shortest': 'shortest'
+      },
+      onchange: e => {
+        layer.edit.isolines.type = e.target.value;
+      },
+      appendTo: block
+    });
+
+    _xyz.utils.dropdown({
+      title: 'Range type',
+      entries: {
+        'Time (min)': 'time',
+        'Distance (km)': 'distance',
+        'Consumption': 'consumption'
+      },
+      onchange: e => {
+        layer.edit.isolines.rangetype = e.target.value;
+      },
+      appendTo: block
+    });
+
+    _xyz.utils.checkbox({
+      label: 'Use live traffic data',
+      onChange: e => {
+        layer.edit.isolines.traffic = e.target.checked;
+        console.log('if this is checked select departure time');
+      },
+      appendTo: block
+    });
+
+    _xyz.utils.slider({
+      title: 'Range of isoline: ',
+      min: 0, // 3 mins or 3 km
+      max: 60, // 60 mins or 60 km
+      value: 10,
+      appendTo: block,
+      oninput: e => {
+        layer.edit.isolines.range = parseInt(e.target.value);
+        e.target.parentNode.previousSibling.textContent = layer.edit.isolines.range;
+      }
+    });
+
+    _xyz.utils.createStateButton({
+      text: 'Isoline',
+      appendTo: layer.edit.panel,
+      layer: layer,
+      activate: isoline,
+      finish: finish
+    });
+    
   }
 
 };
