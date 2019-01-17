136fbb8fb589840fdd5d3201fdea9776eb793c51
Agata Brok <agatbr@gmail.com>
2019-01-17 14:25:18 +0000

    Updated nav method.

--- public/js/tableview/_tableview.mjs
+++ public/js/tableview/_tableview.mjs
@@ -14,14 +14,15 @@ export default _xyz => {
 
     if(!Object.values(_xyz.layers.list).some(layer => !!layer.tab)) return;
     
-    _xyz.tableview = utils;
+    //_xyz.tableview = utils;
+    //console.log(_xyz.tableview);
     _xyz.tableview.observe = observe;
       
     _xyz.view.desktop = {};
     _xyz.view.desktop.tableview = tableview(_xyz);
     _xyz.view.desktop.tableview.appendChild(layout(_xyz));
     
-    nav();
+    nav(_xyz);
   };
 
 };
