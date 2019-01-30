61c92a56604b54dcb874e0a78d27a949de15a27f
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2019-01-30 18:07:05 +0000

    point geometries without styling; scalebar position; attribution wrap;

--- public/js/xyz_leaflet/src/layer/_layer.mjs
+++ public/js/xyz_leaflet/src/layer/_layer.mjs
@@ -152,7 +152,8 @@ export default _xyz => {
     if (!params.icon) return _xyz.L.geoJson(params.json, {
       interactive: params.interactive || false,
       pane: params.pane || 'default',
-      style: params.style || {}
+      style: params.style || {},
+      pointToLayer: () => {}
     }).addTo(_xyz.map);
 
     return _xyz.L.geoJson(params.json, {
