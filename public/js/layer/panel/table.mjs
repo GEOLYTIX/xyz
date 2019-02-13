3d3a4964bfba54a6fdefb97548cd0b607802b65d
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2019-02-13 12:17:50 +0000

    tableview fix for map/leaflet view

--- public/js/layer/panel/table.mjs
+++ public/js/layer/panel/table.mjs
@@ -1,6 +1,6 @@
 export default (_xyz, layer) => {
 
-  if(!layer.tableview) return;
+  if(!layer.tableview || !layer.tableview.tables) return;
 
   // Create cluster panel and add to layer dashboard.
   layer.tableview.panel = _xyz.utils.createElement({
