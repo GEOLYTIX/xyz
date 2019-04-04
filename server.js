d9d59455598e9284b37c4d0e06c3fba844988cee
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2019-04-04 11:04:02 +0100

    added rawgithub as image source directive

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ function startFastify(){
           styleSrc: ['\'self\'', '\'unsafe-inline\'', 'fonts.googleapis.com'],
           fontSrc: ['\'self\'', 'fonts.gstatic.com'],
           scriptSrc: ['\'self\'', 'www.google.com', 'www.gstatic.com', '*.logrocket.io'],
-          imgSrc: ['\'self\'', '*.tile.openstreetmap.org', 'api.mapbox.com', 'res.cloudinary.com', 'data:']
+          imgSrc: ['\'self\'', '*.tile.openstreetmap.org', 'api.mapbox.com', 'res.cloudinary.com', 'raw.githubusercontent.com', 'data:']
         },
         setAllHeaders: true
       },
