8ede345f90f58bb9983a9d20a49829cf4c722abb
Agata Brok <agatbr@gmail.com>
2019-04-08 17:52:36 +0100

    Remove tab working for tableDefinitions.
    Need to implement orderedLists.

--- public/js/xyz_control/tableview/removeTab.mjs
+++ public/js/xyz_control/tableview/removeTab.mjs
@@ -14,7 +14,20 @@ export default _xyz => table => {
 
   _xyz.tableview.current_table = null;
 
+
+  if(document.querySelectorAll('#tableview ul.nav_bar-nav li').length < _xyz.tableview.max_tabs){
+    console.log(document.querySelectorAll('#tableview ul.nav_bar-nav li').length);
+    console.log(_xyz.tableview.tables.length);
+    console.log('unfold a tab!');
+    
+    if(_xyz.tableview.nav_dropdown.firstChild){
+      _xyz.tableview.nav_dropdown.firstChild.classList.remove('folded');
+      _xyz.tableview.nav_bar.appendChild(_xyz.tableview.nav_dropdown.firstChild);
+    }
+  }
+
   if (_xyz.tableview.node && _xyz.tableview.tables.length > 0) {
+
   	document.querySelectorAll('#tableview li')[_xyz.tableview.tables.length-1].classList.add('tab-current');
   	return _xyz.tableview.tables[_xyz.tableview.tables.length-1].activate();
   }
