e41235af3b45d9f4cf6a4a1165f888616daebdda
Agata Brok <agatbr@gmail.com>
2019-04-11 12:32:59 +0100

    Report: custom template requested from Github API.

--- routes/report.js
+++ routes/report.js
@@ -20,7 +20,7 @@ function route(fastify) {
         login: true
       })
     ]),
-    handler: view
+    handler: view 
   });
 
   fastify.route({
@@ -35,24 +35,61 @@ function route(fastify) {
 
 async function view(req, res, token = { access: 'public' }) {
 
-  // console.log(req.query.token);
-
   const config = global.workspace.current;
+
+  let github,
+      github_env = process.env.RESOURCES_GITHUB ? process.env.RESOURCES_GITHUB.split(' ') : null;
+
+  let local_html;
+  
+  try {
+    // try if template exists in repository
+    local_html = await require('fs').readFileSync(`${global.appRoot}/public/views/report/${req.query.template}.html`, 'utf8');
+  } catch (err) {
+    // apply fallback default template
+    local_html = await require('fs').readFileSync(`${global.appRoot}/public/views/report/map_location.html`, 'utf8');
+  }
+  
+  // send back local if no github resources
+  if(!github_env) sendBack(local_html);
+
+  // Github settings
+  github = {
+    access_token: github_env[0],
+    owner: github_env[1],
+    repo: github_env[2],
+    folder: github_env[3] || null
+  }
+
+  const url = `https://api.github.com/repos/${github.owner}/${github.repo}/contents/${github.folder ? `${github.folder}/` : ``}${req.query.template}.html?access_token=${github.access_token}`;
+
+  try {
+    // Get file meta from Github
+    const fetched = await require(global.appRoot + '/mod/fetch')(url);
+    // Process file content
+    let base64 = fetched.content,
+      buff = Buffer.from(base64, 'base64'),
+      html = buff.toString('utf8');
+      sendBack(html);
+  } catch (err) {
+    sendBack(local_html);
+  }
 
   // Check whether request comes from a mobile platform and set template.
   // const md = new Md(req.headers['user-agent']);
+  
+  function sendBack(html){
+    const tmpl = jsr.templates('./public/views/report.html');
 
-  const tmpl = jsr.templates('./public/views/report.html');
+    // Build the template with jsrender and send to client.
+    res.type('text/html').send(tmpl.render({
+      dir: global.dir,
+      title: config.title || 'GEOLYTIX | XYZ',
+      nanoid: nanoid(6),
+      token: req.query.token || token.signed || '""',
+      template: html || null,
+      script_js: 'views/report.js'
+    }));
+  }
 
-  let html = await require('fs').readFileSync(`${global.appRoot}/public/views/report/${req.query.template}.html`, 'utf8');
-
-  // Build the template with jsrender and send to client.
-  res.type('text/html').send(tmpl.render({
-    dir: global.dir,
-    title: config.title || 'GEOLYTIX | XYZ',
-    nanoid: nanoid(6),
-    token: req.query.token || token.signed || '""',
-    template: html || null,
-    script_js: 'views/report.js'
-  }));
 };
