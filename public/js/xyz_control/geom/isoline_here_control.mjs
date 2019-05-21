4525e1b5059cfb1de3c00143c96d75fd4495d8f7
Agata Brok <agatbr@gmail.com>
2019-05-21 11:57:08 +0100

    Here isolines - dropdowns in line with labels.

--- public/js/xyz_control/geom/isoline_here_control.mjs
+++ public/js/xyz_control/geom/isoline_here_control.mjs
@@ -1,10 +1,30 @@
 export default _xyz => param => {
 
+  let mode_container = _xyz.utils.createElement({
+    tag: 'div',
+    style: {
+      marginTop: '8px'
+    },
+    appendTo: param.container
+  });
+
+  _xyz.utils.createElement({
+    tag: 'span',
+    options: {
+      textContent: 'Mode'
+    },
+    appendTo: mode_container
+  });
+
 	_xyz.utils.dropdown({
-      title: 'Mode',
       label: 'label',
       val: 'val',
       selected: param.entry.edit.isoline_here.mode || 'car',
+      style: {
+        width: '70%',
+        float: 'right',
+        margin: 0
+      },
       entries: [
         {
           label: 'Driving',
@@ -26,14 +46,34 @@ export default _xyz => param => {
       onchange: e => {
         param.entry.edit.isoline_here.mode = e.target.value;
       },
-      appendTo: param.container
+      appendTo: mode_container
     });
 
+  let range_container = _xyz.utils.createElement({
+    tag: 'div',
+    style: {
+      marginTop: '8px'
+    },
+    appendTo: param.container
+  });
+
+  _xyz.utils.createElement({
+    tag: 'span',
+    options: {
+      textContent: 'Range'
+    },
+    appendTo: range_container
+  });
+
     _xyz.utils.dropdown({
-      title: 'Range',
       label: 'label',
       val: 'val',
       selected: param.entry.edit.isoline_here.rangetype || 'time',
+      style: {
+        width: '70%',
+        float: 'right',
+        margin: 0
+      },
       entries: [
         {
           label: 'Time (min)',
@@ -63,13 +103,16 @@ export default _xyz => param => {
         }
 
       },
-      appendTo: param.container
+      appendTo: range_container
     });
 
     param.entry.edit.isoline_here.minutes = param.entry.edit.isoline_here.minutes || 10;
 
     const slider_here_minutes = _xyz.utils.createElement({
       tag: 'div',
+      style: {
+        marginTop: '12px'
+      },
       appendTo: param.container
     });
 
@@ -91,6 +134,7 @@ export default _xyz => param => {
     const slider_here_distance = _xyz.utils.createElement({
       tag: 'div',
       style: {
+        marginTop: '12px',
         display: 'none'
       },
       appendTo: param.container
