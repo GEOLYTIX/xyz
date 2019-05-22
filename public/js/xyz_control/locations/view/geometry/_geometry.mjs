86e6658342765795d3755ffe410c42e1f8970de8
Agata Brok <agatbr@gmail.com>
2019-05-22 11:08:26 +0100

    Geometry - fix to Mapbox isoline settings.

--- public/js/xyz_control/locations/view/geometry/_geometry.mjs
+++ public/js/xyz_control/locations/view/geometry/_geometry.mjs
@@ -165,7 +165,7 @@ export default _xyz => entry => {
 
     if(entry.edit.isoline_mapbox && entry.edit.isoline_mapbox.slider){
 
-      td.appendChild(entry.edit.container);
+      if(!entry.ctrl.toggle.checked) td.appendChild(entry.edit.container);
 
       _xyz.geom.isoline_mapbox_control({
         entry: entry,
