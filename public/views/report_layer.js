f34cc6afe65a4d0445e73fb9742ff381c0d81a91
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2019-05-24 18:44:12 +0100

    layer report;

---
+++ public/views/report_layer.js
@@ -0,0 +1,34 @@
+const params = {};
+
+// Take hooks from URL and store as current hooks.
+window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, (match, key, value) => {
+  params[key] = decodeURI(value);
+});
+
+_xyz({
+  host: document.head.dataset.dir,
+  token: document.body.dataset.token,
+  locale: params.locale,
+  callback: init
+});
+
+function init(_xyz) {
+
+  _xyz.mapview.create({
+    scrollWheelZoom: true,
+    target: document.getElementById('report_map')
+  });
+
+  const layer = _xyz.layers.list[params.layer];
+
+  layer.view();
+
+  layer.show();
+
+  if (layer.style.theme) {
+
+    document.getElementById('report_left').appendChild(layer.style.legend);
+
+  }
+
+}
