134159dfe7d77e2a3d7b89c161016797a999af7f
Agata Brok <agatbr@gmail.com>
2019-05-30 10:53:25 +0100

    Mvt legend - line symbol for styles with undefined fillOpacity.

--- public/js/xyz_control/layers/view/panel/style/legend/polyCategorized.mjs
+++ public/js/xyz_control/layers/view/panel/style/legend/polyCategorized.mjs
@@ -18,15 +18,30 @@ export default _xyz => layer => {
 
     let cat_style = Object.assign({}, layer.style.default, cat[1]);
 
+    if(cat_style.fillOpacity === undefined) {
+
+     legend.append('line')
+      .attr('x1', 4)
+      .attr('y1', y + 10)
+      .attr('x2', 18)
+      .attr('y2', y + 10)
+      .style('stroke', cat_style.color)
+      .style('stroke-width', cat_style.weight || 1); 
+  
+    } else {
+
     legend.append('rect')
       .attr('x', 4)
       .attr('y', y + 3)
       .attr('width', 14)
       .attr('height', 14)
-      .style('fill', cat_style.fillColor)
+      .style('fill', cat_style.fillColor || '#FFF')
       .style('fill-opacity', cat_style.fillOpacity)
-      .style('stroke', cat_style.color);      
+      .style('stroke', cat_style.color)
+      .style('stroke-width', cat_style.weight || 1);      
       
+    } 
+
     legend.append('text')
       .attr('x', 25)
       .attr('y', y + 11)
@@ -56,18 +71,35 @@ export default _xyz => layer => {
       
     y += 20;
   });
+
+
       
   // Attach box for other/default categories.
   if (layer.style.theme.other) {
+
+    if(layer.style.default.fillOpacity === undefined) {
+
+     legend.append('line')
+      .attr('x1', 4)
+      .attr('y1', y + 10)
+      .attr('x2', 18)
+      .attr('y2', y + 10)
+      .style('stroke', layer.style.default.color)
+      .style('stroke-width', layer.style.default.weight || 1); 
+  
+    } else {
 
     legend.append('rect')
       .attr('x', 4)
       .attr('y', y + 3)
       .attr('width', 14)
       .attr('height', 14)
-      .style('fill', layer.style.default.fillColor)
+      .style('fill', layer.style.default.fillColor || '#FFF')
       .style('fill-opacity', layer.style.default.fillOpacity)
-      .style('stroke', layer.style.default.color);       
+      .style('stroke', layer.style.default.color)
+      .style('stroke-width', layer.style.default.weight || 1);   
+
+    }    
       
     // Attach text with filter on click for the other/default category.
     legend.append('text')
