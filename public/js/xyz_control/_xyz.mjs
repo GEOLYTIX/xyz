bb05691b6248c4b6e53a7d449593fcb73aeefbda
Robert <robert.hurst@geolytix.co.uk>
2019-06-12 14:49:19 +0200

    Version Change (v1.6.1)

--- public/js/xyz_control/_xyz.mjs
+++ public/js/xyz_control/_xyz.mjs
@@ -1,6 +1,6 @@
 export default () => ({
-  version: 'XYZ v1.6.0',
-  release: 'https://github.com/GEOLYTIX/xyz/releases/tag/v1.6.0',
+  version: 'XYZ v1.6.1',
+  release: 'https://github.com/GEOLYTIX/xyz/releases/tag/v1.6.1',
   gazetteer: {},
   host: null,
   hooks: null,
