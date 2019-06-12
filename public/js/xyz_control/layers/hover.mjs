129a729029c7cc21de370ba02e8cd2cddfb83f7a
Agata Brok <agatbr@gmail.com>
2019-06-12 11:49:08 +0100

    Fix to labels - exit when none defined.

--- public/js/xyz_control/layers/hover.mjs
+++ public/js/xyz_control/layers/hover.mjs
@@ -82,6 +82,9 @@ export default (_xyz, layer) => {
   };
 
   function toggle(param){
+
+    if(!layer.hover) return;
+
     if(typeof(layer.hover.permanent) === undefined) return;
     if(layer.hover.btn) layer.hover.btn.remove();
   
