dadbba17b87cfece239c0ef8c1fec4fba803bfda
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2019-06-27 12:53:27 +0100

    hyper dropdowns

--- public/js/xyz_control/geom/isoline_here_control.mjs
+++ public/js/xyz_control/geom/isoline_here_control.mjs
@@ -16,38 +16,38 @@ export default _xyz => param => {
     appendTo: mode_container
   });
 
-	_xyz.utils.dropdown({
-      label: 'label',
-      val: 'val',
-      selected: param.entry.edit.isoline_here.mode || 'car',
-      style: {
-        width: '70%',
-        float: 'right',
-        margin: 0
+  _xyz.utils.dropdown({
+    label: 'label',
+    val: 'val',
+    selected: param.entry.edit.isoline_here.mode || 'car',
+    style: {
+      width: '70%',
+      float: 'right',
+      margin: 0
+    },
+    entries: [
+      {
+        label: 'Driving',
+        val: 'car'
       },
-      entries: [
-        {
-          label: 'Driving',
-          val: 'car'
-        },
-        {
-          label: 'Walking',
-          val: 'pedestrian'
-        },
-        {
-          label: 'Cargo',
-          val: 'truck'
-        },
-        {
-          label: 'HOV lane',
-          val: 'carHOV'
-        },
-      ],
-      onchange: e => {
-        param.entry.edit.isoline_here.mode = e.target.value;
+      {
+        label: 'Walking',
+        val: 'pedestrian'
       },
-      appendTo: mode_container
-    });
+      {
+        label: 'Cargo',
+        val: 'truck'
+      },
+      {
+        label: 'HOV lane',
+        val: 'carHOV'
+      },
+    ],
+    onchange: e => {
+      param.entry.edit.isoline_here.mode = e.target.value;
+    },
+    appendTo: mode_container
+  });
 
   let range_container = _xyz.utils.createElement({
     tag: 'div',
@@ -65,92 +65,92 @@ export default _xyz => param => {
     appendTo: range_container
   });
 
-    _xyz.utils.dropdown({
-      label: 'label',
-      val: 'val',
-      selected: param.entry.edit.isoline_here.rangetype || 'time',
-      style: {
-        width: '70%',
-        float: 'right',
-        margin: 0
+  _xyz.utils.dropdown({
+    label: 'label',
+    val: 'val',
+    selected: param.entry.edit.isoline_here.rangetype || 'time',
+    style: {
+      width: '70%',
+      float: 'right',
+      margin: 0
+    },
+    entries: [
+      {
+        label: 'Time (min)',
+        val: 'time'
       },
-      entries: [
-        {
-          label: 'Time (min)',
-          val: 'time'
-        },
-        {
-          label: 'Distance (km)',
-          val: 'distance'
-        }
-      ],
-      onchange: e => {
-
-        param.entry.edit.isoline_here.rangetype = e.target.value;
-
-        if (e.target.value === 'time') {
-
-          slider_here_minutes.style.display = 'block';
-          slider_here_distance.style.display = 'none';
-
-        }
-
-        if (e.target.value === 'distance') {
-
-          slider_here_minutes.style.display = 'none';
-          slider_here_distance.style.display = 'block';
-
-        }
-
-      },
-      appendTo: range_container
-    });
-
-    param.entry.edit.isoline_here.minutes = param.entry.edit.isoline_here.minutes || 10;
-
-    const slider_here_minutes = _xyz.utils.createElement({
-      tag: 'div',
-      style: {
-        marginTop: '12px'
-      },
-      appendTo: param.container
-    });
-
-    _xyz.utils.slider({
-      title: 'Travel time in minutes: ',
-      default: param.entry.edit.isoline_here.minutes,
-      min: 5,
-      max: 60,
-      value: param.entry.edit.isoline_here.minutes,
-      appendTo: slider_here_minutes,
-      oninput: e => {
-        param.entry.edit.isoline_here.minutes = parseInt(e.target.value);
-        e.target.parentNode.previousSibling.textContent = param.entry.edit.isoline_here.minutes;
+      {
+        label: 'Distance (km)',
+        val: 'distance'
       }
-    });
+    ],
+    onchange: e => {
 
-    param.entry.edit.isoline_here.distance = param.entry.edit.isoline_here.distance || 10;
+      param.entry.edit.isoline_here.rangetype = e.target.value;
 
-    const slider_here_distance = _xyz.utils.createElement({
-      tag: 'div',
-      style: {
-        marginTop: '12px',
-        display: 'none'
-      },
-      appendTo: param.container
-    });
+      if (e.target.value === 'time') {
 
-    _xyz.utils.slider({
-      title: 'Travel distance in kilometer: ',
-      default: param.entry.edit.isoline_here.distance,
-      min: 5,
-      max: 60,
-      value: param.entry.edit.isoline_here.distance,
-      appendTo: slider_here_distance,
-      oninput: e => {
-        param.entry.edit.isoline_here.distance = parseInt(e.target.value);
-        e.target.parentNode.previousSibling.textContent = param.entry.edit.isoline_here.distance;
+        slider_here_minutes.style.display = 'block';
+        slider_here_distance.style.display = 'none';
+
       }
-    });
 
-}
+      if (e.target.value === 'distance') {
+
+        slider_here_minutes.style.display = 'none';
+        slider_here_distance.style.display = 'block';
+
+      }
+
+    },
+    appendTo: range_container
+  });
+
+  param.entry.edit.isoline_here.minutes = param.entry.edit.isoline_here.minutes || 10;
+
+  const slider_here_minutes = _xyz.utils.createElement({
+    tag: 'div',
+    style: {
+      marginTop: '12px'
+    },
+    appendTo: param.container
+  });
+
+  _xyz.utils.slider({
+    title: 'Travel time in minutes: ',
+    default: param.entry.edit.isoline_here.minutes,
+    min: 5,
+    max: 60,
+    value: param.entry.edit.isoline_here.minutes,
+    appendTo: slider_here_minutes,
+    oninput: e => {
+      param.entry.edit.isoline_here.minutes = parseInt(e.target.value);
+      e.target.parentNode.previousSibling.textContent = param.entry.edit.isoline_here.minutes;
+    }
+  });
+
+  param.entry.edit.isoline_here.distance = param.entry.edit.isoline_here.distance || 10;
+
+  const slider_here_distance = _xyz.utils.createElement({
+    tag: 'div',
+    style: {
+      marginTop: '12px',
+      display: 'none'
+    },
+    appendTo: param.container
+  });
+
+  _xyz.utils.slider({
+    title: 'Travel distance in kilometer: ',
+    default: param.entry.edit.isoline_here.distance,
+    min: 5,
+    max: 60,
+    value: param.entry.edit.isoline_here.distance,
+    appendTo: slider_here_distance,
+    oninput: e => {
+      param.entry.edit.isoline_here.distance = parseInt(e.target.value);
+      e.target.parentNode.previousSibling.textContent = param.entry.edit.isoline_here.distance;
+    }
+  });
+
+};
