07c159603a3d4c06568dd224c7917d15463b6315
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2019-09-02 18:51:23 +0100

    marker styles

--- public/js/xyz_openlayers/mapview/icon.mjs
+++ public/js/xyz_openlayers/mapview/icon.mjs
@@ -1,12 +1,17 @@
 export default _xyz => function(icon) {
 
-  if (icon.type) icon.url = _xyz.utils.svg_symbols(icon);
-   
-  // const svgHeight = icon.url.match(/height%3D%22(\d+)%22/);
-  // const iconHeight = svgHeight != null && Array.isArray(svgHeight) && svgHeight.length == 2 ? svgHeight[1] : 1000;
-  // const scale = (icon.iconSize || 40) / iconHeight;
+  if (icon.type && icon.type === 'circle') return new _xyz.mapview.lib.style.Circle({
+    radius: parseInt(icon.radius) || 10,
+    fill: icon.fillColor && new _xyz.mapview.lib.style.Fill({
+      color: _xyz.utils.chroma(icon.fillColor).alpha(icon.fillOpacity === 0 ? 0 : parseFloat(icon.fillOpacity) || 1).rgba()
+    }),
+    stroke: icon.strokeColor && new _xyz.mapview.lib.style.Stroke({
+      color: _xyz.utils.chroma(icon.strokeColor).alpha(icon.strokeColor === 0 ? 0 : parseFloat(icon.strokeOpacity) || 1).rgba(),
+      width: parseInt(icon.strokeWidth) || 1
+    })
+  });
 
-  if (icon.size) icon.scale = icon.size / 1000;
+  if (icon.type) icon.url = _xyz.utils.svg_symbols(icon);
 
   return new _xyz.mapview.lib.style.Icon({
     src: icon.url,
@@ -14,19 +19,4 @@ export default _xyz => function(icon) {
     anchor: icon.anchor || [0.5, 0.5],
   });
 
-
-  function circle() {
-
-    const image = new _xyz.mapview.lib.style.Circle({
-      radius: 7,
-      fill: new _xyz.mapview.lib.style.Fill({
-        color: 'rgba(0, 0, 0, 0.01)'
-      }),
-      stroke: new _xyz.mapview.lib.style.Stroke({
-        color: '#EE266D',
-        width: 2
-      })
-    });
-    
-  }
 };
