bf372c010c0795b5373de76a7f339124fb9529c4
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2019-09-18 08:32:14 +0100

    enable logger

--- server.js
+++ server.js
@@ -44,15 +44,15 @@ require('./mod/workspace/init')();
 // Set fastify
 const fastify = require('fastify')({
   trustProxy: true,
-  // logger: {
-  //   level: env.logs || 'error',
-  //   prettifier: require('pino-pretty'),
-  //   prettyPrint: {
-  //     errorProps: 'hint, detail',
-  //     levelFirst: true,
-  //     crlf: true
-  //   }
-  // }
+  logger: {
+    level: env.logs || 'error',
+    prettifier: require('pino-pretty'),
+    prettyPrint: {
+      errorProps: 'hint, detail',
+      levelFirst: true,
+      crlf: true
+    }
+  }
 });
 
 // Register fastify modules and routes.
