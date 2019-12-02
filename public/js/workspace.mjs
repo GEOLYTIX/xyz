f3bf29edd752cb0c4f8243b2e8c3baf2332de8bb
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2019-12-02 11:29:27 +0000

    load scripts with token;

--- public/js/workspace.mjs
+++ public/js/workspace.mjs
@@ -45,26 +45,20 @@ export default _xyz => {
 
   function loadLocale(callback) {
 
-    // Assign key from params of first in locales list.
-    const locale = (_xyz.locale && _xyz.workspace.locales[_xyz.locale])
-      || (_xyz.hooks && _xyz.hooks.current.locale)
-      || Object.keys(_xyz.workspace.locales)[0];
+    _xyz.locale = _xyz.locale || (_xyz.hooks && _xyz.hooks.current.locale) || Object.keys(_xyz.workspace.locales)[0];
 
     // Assigne workspace locales from locales list and input params.
-    _xyz.workspace.locale = Object.assign({ key: locale }, _xyz.workspace.locales[locale]);
+    _xyz.workspace.locale = Object.assign({ key: _xyz.locale }, _xyz.workspace.locales[_xyz.locale]);
 
     loadScripts(callback)
   };
 
-  function loadScripts(callback){
+  function loadScripts(callback) {
 
     if (!_xyz.workspace.locale.scripts) return loadLayers(callback);
 
-    const scripts = _xyz.workspace.locale.scripts.map(script=>_xyz.utils.loadScript(script));
-
-    Promise.all(scripts).then(()=>{
-      loadLayers(callback)
-    });
+    Promise.all(_xyz.workspace.locale.scripts.map(script => _xyz.utils.loadScript(script + '&token=' + _xyz.token)))
+      .then(() => loadLayers(callback));
   }
 
   function loadLayers(callback){
