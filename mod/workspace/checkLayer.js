da7c1601376e150619cde10bad5631ee329966ff
Agata Brok <agatbr@gmail.com>
2019-12-13 11:19:44 +0000

    Small fix.

--- mod/workspace/checkLayer.js
+++ mod/workspace/checkLayer.js
@@ -119,7 +119,7 @@ async function chkMVTCache(layer) {
 
       layer.mvt_fields = [];
 
-      Object.values(Object.assign({}, layer.style.themes)).map(t => mvt_fields.push(t.fieldfx ? `${t.fieldfx} AS ${t.field}` : t.field));
+      Object.values(Object.assign({}, layer.style.themes)).map(t => layer.mvt_fields.push(t.fieldfx ? `${t.fieldfx} AS ${t.field}` : t.field));
 
       //Object.values(Object.assign({},layer.style.themes)).map(t => layer.mvt_fields.push(t.field));
 
