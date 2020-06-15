7083359ba356aceef528fc50db791d4995dc2cee
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2020-06-15 13:01:17 +0100

    layer defaults in api

--- api/location.js
+++ api/location.js
@@ -1,5 +1,7 @@
 const auth = require('../mod/auth/handler')
 
+const defaults = require('../mod/workspace/defaults')
+
 const getWorkspace = require('../mod/workspace/getWorkspace')
 
 const _method = {
@@ -30,7 +32,19 @@ module.exports = async (req, res) => {
     <a href="https://geolytix.github.io/xyz/docs/develop/api/location/">Location API</a>`)
   }
 
-  req.params.layer = req.params.locale && workspace.locales[req.params.locale].layers[req.params.layer]
+  const locale = req.params.locale && workspace.locales[req.params.locale]
+
+  let layer = locale && locale.layers[req.params.layer] ||  workspace.templates[req.params.layer]
+
+  if (!layer) return res.status(400).send('Layer not found.')
+
+  layer.template && Object.assign(layer, workspace.templates[layer.template])
+
+  layer = Object.assign({key: req.params.layer}, defaults.layers[layer.format] || {}, layer)
+
+  layer.style = layer.style && Object.assign({}, defaults.layers[layer.format].style || {}, layer.style)
+
+  req.params.layer = layer
 
   if (!req.params.layer) {
     return res.status().send(`Failed to evaluate 'layer' param.<br><br>
