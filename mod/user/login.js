dfc26b9ef8c377819401136c96147ff4aaa933ab
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2020-09-22 14:43:42 +0100

    login language templates

--- mod/user/login.js
+++ mod/user/login.js
@@ -4,24 +4,25 @@ const { readFileSync } = require('fs')
 
 const { join } = require('path')
 
-const template = readFileSync(join(__dirname, '../../public/views/_login.html')).toString('utf8')
-
-const render = params => template.replace(/\$\{(.*?)\}/g, matched => params[matched.replace(/\$|\{|\}/g, '')] || '')
+const templates = {
+  english: readFileSync(join(__dirname, '../../public/views/_login.html')).toString('utf8'),
+  german: readFileSync(join(__dirname, '../../public/views/_login_german.html')).toString('utf8')
+}
 
 module.exports = async (req, res, msg) => {
 
   if (!acl) return res.send('No Access Control List.')
 
-  const rows = await acl(`select * from acl_schema.acl_table limit 1`)
+  const template = req.params.language && templates[req.params.language] || templates.english
 
-  if (rows instanceof Error) return res.send('Failed to connect with Access Control List.')
-
-  const html = render({
+  const params = {
     dir: process.env.DIR || '',
     redirect: req.body && req.body.redirect || req.url && decodeURIComponent(req.url),
     msg: msg || ' ',
     captcha: process.env.GOOGLE_CAPTCHA && process.env.GOOGLE_CAPTCHA.split('|')[0] || '',
-  })
+  }
+
+  const html = template.replace(/\$\{(.*?)\}/g, matched => params[matched.replace(/\$|\{|\}/g, '')] || '')
 
   res.send(html)
 }
