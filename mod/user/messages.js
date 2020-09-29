946357beda41c19d39fd05444a9a27ca8f2999c6
Agata Brok <agatbr@gmail.com>
2020-09-29 10:52:11 +0100

    Messages - added pl.

--- mod/user/messages.js
+++ mod/user/messages.js
@@ -2,6 +2,7 @@ module.exports = {
 	logout: {
 		en: `Logged out.`,
 		fr: `Deconnecté.`,
+		pl: 'Wylogowano.',
 		ja: `ログアウト`,
 		ko: `당신은 로그 아웃되었습니다`,
 		zh: `您已註銷`
@@ -9,6 +10,7 @@ module.exports = {
 	token_not_found: {
 		en: `Token not found. The token has probably been resolved already.`,
 		fr: `Token n’a pas été trouvé. Il a probablement déjà été utilisé.`,
+		pl: `Token wygasł. Prawdopodobnie został już wykorzystany.`,
 		ja: `トークンが見つかりません。 トークンはおそらくすでに解決されています。`,
 		ko: `토근이 발견되지 않았습니다. 이미 해결된 것 같습니다.`,
 		zh: `未找到相关令牌， 该令牌可能已解析`
@@ -16,6 +18,7 @@ module.exports = {
 	password_reset_verification: {
 		en: `Password will be reset after email verification.`,
 		fr: `Le mot de passe sera réinitialisé après la vérification par e-mail.`,
+		pl: `Hasło zostanie ustawione po weryfikacji konta przez wiadomości e-mail.`,
 		ja: `E-メール検証完了後にパスワードはリセットされます`,
 		ko: `이메일 확인 후 비밀번호가 재설정됩니다`,
 		zh: `电子邮件验证后，密码将被重置。`
@@ -23,6 +26,7 @@ module.exports = {
 	new_account_registered: {
 		en: `A new account has been registered and is awaiting email verification.`,
 		fr: `Un nouveau compte a été enregistré et il attend la vérification par e-mail.`,
+		pl: `Nowe konto zostało zarejestrowane i czeka na weryfikację przez wiadomość email.`,
 		ja: `新規アカウントの登録完了にてE-メール検証待ち`,
 		ko: `새로운 계정이 등록되었고 이메일 확인을 기다리고 있습니다.`,
 		zh: `已注册一个新帐户，正在等待电子邮件验证。`
@@ -30,6 +34,7 @@ module.exports = {
 	update_ok: {
 		en: `Update successful`,
 		fr: `Cette mise à jour a réussi.`,
+		pl: `Uaktualniono.`,
 		ja: `アップデートに成功しました`,
 		ko: `업데이트가 성공적으로 진행되었습니다.`,
 		zh: `更新成功`
@@ -37,6 +42,7 @@ module.exports = {
 	account_not_found: {
 		en: `No matching account found.`,
 		fr: `Ce compte n’a pas été trouvé.`,
+		pl: `Nie znaleziono tego konta.`,
 		ja: `アカウントが見つかりません。`,
 		ko: `매칭되는 계정이 없습니다.`,
 		zh: `找不到匹配的帐户`
@@ -44,6 +50,7 @@ module.exports = {
 	account_await_approval: {
 		en: `This account has been verified but requires administrator approval.`,
 		fr: `Le compte a été verifié mais il doit être approuvé par l'administrateur. `,
+		pl: `Konto zostało zweryfikowane i musi zostać zatwierdzone przez administratora.`,
 		ja: `本アカウント検証完了。アドミニストレーター承認が必要となります`,
 		ko: `이 계정은 확인되었으나 관리자의 승인이 필요합니다.`,
 		zh: `此帐户已通过验证，但需要管理员批准。`
@@ -51,6 +58,7 @@ module.exports = {
 	password_reset_ok: {
 		en: `Password has been reset.`,
 		fr: `Le mot de passe a été réinitialisé.`,
+		pl: `Ustawiono nowe hasło.`,
 		ja: `パスワードがリセットされました`,
 		ko: `비밀번호가 재설정되었습니다.`,
 		zh: `密码已重设`
@@ -58,6 +66,7 @@ module.exports = {
 	user_not_found: {
 		en: `User not found.`,
 		fr: `Ce nom d’utilisateur n’a pas été trouvé.`,
+		pl: `Nie znaleziono tej nazwy użytkownika.`,
 		ja: `ユーザーが見つかりません`,
 		ko: `사용자 미발견`,
 		zh: `找不到用户`
@@ -65,6 +74,7 @@ module.exports = {
 	user_blocked: {
 		en: `User blocked`,
 		fr: `Cet utilisateur est bloqué.`,
+		pl: `Konto zostało zablokowane.`,
 		ja: `ユーザーがブロックされました`,
 		ko: `사용자 봉쇄`,
 		zh: `用户被阻止`
@@ -72,6 +82,7 @@ module.exports = {
 	user_expired: {
 		en: `User approval has expired. Please re-register.`,
 		fr: `Les droits d'accès ont expiré. Veuillez vous réenregistrer.`,
+		pl: `Prawo dostępu wygasło. Zarejestruj się poownie.`,
 		ja: `ユーザーの承認は期限切れです。 再登録してください。`,
 		ko: `사용자 승인이 만료되었습니다. 다시 등록하십시오.`,
 		zh: `用戶批准已過期。 請重新註冊。`
@@ -79,6 +90,7 @@ module.exports = {
 	user_not_verified: {
 		en: `User not verified or approved`,
 		fr: `L’utilisateur n’a pas été vérifié ou approuvé.`,
+		pl: `Konto niezweryfikowane ani zatwierdzone.`,
 		ja: `ユーザーは確認または承認されていません`,
 		ko: `사용자 미확인 또는 미승인`,
 		zh: `用户未经验证或批准`
@@ -86,6 +98,7 @@ module.exports = {
 	account_blocked: {
 		en: `Account is blocked, please check email.`,
 		fr: `Le comte a été bloqué. Consultez votre boîte e-mail.`,
+		pl: `To konto zostało zablokowane. Sprawdź wiadomości e-mail.`,
 		ja: `アカウントがブロックされています。メールを確認してください`,
 		ko: `계정이 봉쇄되었습니다. 이메일을 확인해주십시오`,
 		zh: `帐户已被封锁，请检查电子邮件`
@@ -93,12 +106,14 @@ module.exports = {
 	token_failed: {
 		en: `Failed to create token`,
 		fr: `Impossible de créer token.`,
+		pl: `Nie można utworzyć tokenu.`,
 		ja: `トークンを作成できませんでした`,
 		ko: `토큰 생성에 실패했습니다.`,
 		zh: `创建令牌失败`
 	},
 	admin_approved: {
 		en: `The account has been approved by you. An email has been sent to the account holder.`,
+		pl: `Konto zostało zatwierdzone. Wysłano wiadomość na zarejestrowany adres e-mail.`,
 		fr: `Vous avez approuvé ce compte. Un e-mail a été envoyé au propriétaire du compte.`,
 		ja: `アカウントはあなたによって承認されました。 メールをアカウント所有者に送信しました。`,
 		ko: `귀하에 의해서 계정이 승인되었습니다. 계정 사용자에게 이메일이 발송되었습니다.`,
@@ -109,10 +124,12 @@ module.exports = {
 	},
 	missing_password: {
 		en: `Missing password`,
-		fr: `Mot de passe manquant`
+		fr: `Mot de passe manquant`,
+		pl: `Nie podano hasła.`
 	},
 	missing_email: {
 		en: `Missing email`,
-		fr: `E-mail manquant`
+		fr: `E-mail manquant`,
+		pl: `Nie podano adresu e-mail.`
 	}
 }
