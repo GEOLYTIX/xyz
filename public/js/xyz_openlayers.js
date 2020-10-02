e4e07b7caa62396f3f9f016075618c723907d76b
Agata Brok <agatbr@gmail.com>
2020-10-02 10:23:33 +0100

    Svg symbols - improved triangle marker.
    Refined corners and viewbox that doesn't clip.

--- public/js/xyz_openlayers.js
+++ public/js/xyz_openlayers.js
@@ -6,13 +6,13 @@
   <svg width=24 height=24 viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
     <circle cx=13 cy=13 fill='#333' r=10 opacity=0.4></circle>
     <circle cx=12 cy=12 r=10 fill=${e.fillColor||"#FFF"}>`;return e.layers&&Object.entries(e.layers).forEach(e=>{t.appendChild(R.node`<circle cx=12 cy=12 r=${10*parseFloat(e[0])} fill=${e[1]}>`)}),"data:image/svg+xml,"+encodeURIComponent(V.serializeToString(t))}function G(e){const t=R.node`
-  <svg width=24 height=24 viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>`;return t.appendChild(R.node`<path
-    fill=${e.fillColor||"#FFF"}
+  <svg width=32 height=24 viewBox='0 -2 20 20' xmlns='http://www.w3.org/2000/svg'>`;return t.appendChild(R.node`<path
+    fill=${e.fillColor||"#FFF"} stroke=${e.fillColor||"#FFF"} stroke-width=2
     d="M0,-19.6L17,9.8L-17,9.8Z" stroke-linejoin="round"
-    transform=${"translate(10 11) scale(0.6)"}/>`),e.layers&&Object.entries(e.layers).forEach(e=>{t.appendChild(R.node`<path
-    fill=${e[1]}
+    transform=${"translate(10 11) scale(0.6)"}/>`),e.layers&&Object.entries(e.layers).forEach(n=>{t.appendChild(R.node`<path
+    fill=${n[1]} stroke=${e.fillColor||"#FFF"} stroke-width=1
     d="M0,-19.6L17,9.8L-17,9.8Z" stroke-linejoin="round"
-    transform=${`translate(10 11) scale(${.6*e[0]})`}/>`)}),console.log(t.outerHTML),"data:image/svg+xml,"+encodeURIComponent(V.serializeToString(t))}function Y(e){const t=R.node`
+    transform=${`translate(10 11) scale(${.6*n[0]})`}/>`)}),"data:image/svg+xml,"+encodeURIComponent(V.serializeToString(t))}function Y(e){const t=R.node`
   <svg width=24 height=24 viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
     <rect fill='#333' opacity=0.3 width=20 height=20 x=2 y=2 rx=1></rect>
     <rect fill=${e.fillColor||"#FFF"} width=20 height=20 x=0 y=0 rx=1></rect>`;return e.layers&&Object.entries(e.layers).forEach(e=>{t.appendChild(R.node`<rect fill=${e[1]}
