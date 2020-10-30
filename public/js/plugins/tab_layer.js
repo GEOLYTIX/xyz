1e271d06e8cb2e6ae56bf170c795aa0fa186b6ae
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2020-10-30 17:32:11 +0000

    dataview plugin

--- public/js/plugins/tab_layer.js
+++ public/js/plugins/tab_layer.js
@@ -3,9 +3,59 @@ document.dispatchEvent(new CustomEvent('tab_layer', {
 
     _xyz.layers.plugins.tab_layer = layer => {
 
+
+      const table = {
+        active: true,
+        "target": _xyz.utils.html.node`<div>`,
+        "layer": layer,
+        "display": true,
+        "query": "global_cities_query",
+        "selectable": true,
+        "table": "geodata.global_glx_open_citiesoftheworld",
+        "columns": [
+          {
+            "field": "city_name",
+            "title": "City"
+          },
+          {
+            "field": "country",
+            "title": "Country"
+          },
+          {
+            "field": "pop_ghs",
+            "title": "Population (ghs)",
+            "hozAlign": "right",
+            "formatter": "money",
+            "formatterParams": {
+              "precision": 0
+            }
+          },
+          {
+            "field": "area",
+            "title": "Area (km2)",
+            "hozAlign": "right",
+            "formatter": "money",
+            "formatterParams": {
+              "precision": 1
+            }
+          },
+          {
+            "field": "pop_den",
+            "title": "Population Density",
+            "hozAlign": "right",
+            "formatter": "money",
+            "formatterParams": {
+              "precision": 1
+            }
+          }
+        ]
+      }
+
+      _xyz.dataviews.create(table)
+
       const tab = {
         title: 'Layer',
-        node: _xyz.utils.html.node`<div>So we all say`
+        node: table.target,
       }
 
       layer.view.appendChild(_xyz.utils.html.node`
@@ -14,7 +64,11 @@ document.dispatchEvent(new CustomEvent('tab_layer', {
           type="checkbox"
           onchange=${e => {
             tab.display = e.target.checked
-            if (tab.display) return _xyz.dataviews.tabview.add(tab)
+            if (tab.display) {
+              _xyz.dataviews.tabview.add(tab)
+              table.update()
+              return
+            }
             tab.remove()
           }}>
         </input>
