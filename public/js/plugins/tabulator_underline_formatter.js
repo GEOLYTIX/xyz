2c003d541d3a131bb857a2f0d5e68ef151baec49
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2020-11-18 12:14:20 +0000

    table column plugins

--- public/js/plugins/tabulator_underline_formatter.js
+++ public/js/plugins/tabulator_underline_formatter.js
@@ -1,15 +1,31 @@
-document.dispatchEvent(new CustomEvent('tabulator_conditional_formatter', {
+document.dispatchEvent(new CustomEvent('tabulator_underline_formatter', {
   detail: _xyz => {
+
+    const colours = {
+      "Red": "#ef5350",
+      "Amber": "#ffa000",
+      "Green": "#66bb6a"
+    }
 
     _xyz.dataviews.plugins.conditional_underline = (cell, formatterParams, onRendered) => {
 
-      if(!cell.getValue()) return;
+      const cellVal = cell.getValue()
 
-      console.log(cell)
-  
-      if(!formatterParams[`${cell.getValue()}`]) return cell.getValue();
-  
-      cell.getElement().style.backgroundColor = formatterParams[`${cell.getValue()}`];
+      if(!cellVal) return
+
+      console.log(cellVal)
+
+      const val = cellVal.split('|')[0]
+
+      const colour = cellVal.split('|')[1]
+
+      if (colour && colours[colour]) {
+
+        cell.getElement().style.borderBottom = `3px solid ${colours[colour]}`
+
+      }
+
+      return val
     
     }
 
