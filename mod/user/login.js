196aa8ba86c4915ec6c7f4b4a43c1459b85d6e00
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2020-12-02 14:41:02 +0000

    login view load

--- mod/user/login.js
+++ mod/user/login.js
@@ -1,20 +1,14 @@
 const acl = require('./acl')()
 
-const { readFileSync } = require('fs')
+const {
+  readFileSync
+} = require('fs')
 
-const { join } = require('path')
+const {
+  join
+} = require('path')
 
 const getToken = require('./token')
-
-const templates = {
-  en: readFileSync(join(__dirname, '../../public/views/login/_login_en.html')).toString('utf8'),
-  de: readFileSync(join(__dirname, '../../public/views/login/_login_de.html')).toString('utf8'),
-  fr: readFileSync(join(__dirname, '../../public/views/login/_login_fr.html')).toString('utf8'),
-  pl: readFileSync(join(__dirname, '../../public/views/login/_login_pl.html')).toString('utf8'),
-  ja: readFileSync(join(__dirname, '../../public/views/login/_login_ja.html')).toString('utf8'),
-  ko: readFileSync(join(__dirname, '../../public/views/login/_login_ko.html')).toString('utf8'),
-  zh: readFileSync(join(__dirname, '../../public/views/login/_login_zh.html')).toString('utf8')
-}
 
 module.exports = async (req, res, msg) => {
 
@@ -24,44 +18,53 @@ module.exports = async (req, res, msg) => {
 
     const token = await getToken(req)
 
-    if (token instanceof Error) return loginForm(req, res, token.message)
-   
+    if (token instanceof Error) return view(req, res, token.message)
+
     const cookie = `${process.env.TITLE}=${token.signed || token};HttpOnly;Max-Age=28800;Path=${process.env.DIR || '/'};SameSite=Strict${!req.headers.host.includes('localhost') && ';Secure' || ''}`
-  
+
     res.setHeader('Set-Cookie', cookie)
-  
+
     res.setHeader('location', `${req.body.redirect}`)
-  
+
     return res.status(302).send()
 
   }
 
-  loginForm (req, res, msg)
+  view(req, res, msg)
 
 }
 
-function loginForm (req, res, msg) {
+function view(req, res, msg) {
 
-    // Get the login template for language param or English if template doesn't exist
-    const template = templates[req.params.language] || templates.en
+  let template
 
-    // The redirect for a successful login.
-    const redirect = req.body && req.body.redirect ||
-      req.url && decodeURIComponent(req.url).replace(/login\=true/, '')
-    
-    const params = {
-      dir: process.env.DIR,
-      redirect: redirect,
-      language: req.params.language,
-      msg: msg || ' '
-    }
-  
-    // Render the login template with params.
-    const html = template.replace(/\$\{(.*?)\}/g, matched => params[matched.replace(/\$|\{|\}/g, '')] || '')
-  
-    // The login view will set the cookie to null.
-    res.setHeader('Set-Cookie', `${process.env.TITLE}=null;HttpOnly;Max-Age=0;Path=${process.env.DIR || '/'}`)
-  
-    res.send(html)
+  try {
+
+    template = readFileSync(join(__dirname, `../../public/views/login/_login_${req.params.language}.html`)).toString('utf8')
+
+  } catch {
+
+    template = readFileSync(join(__dirname, `../../public/views/login/_login_en.html`)).toString('utf8')
+
+  }
+
+  // The redirect for a successful login.
+  const redirect = req.body && req.body.redirect ||
+    req.url && decodeURIComponent(req.url).replace(/login\=true/, '')
+
+  const params = {
+    dir: process.env.DIR,
+    redirect: redirect,
+    language: req.params.language,
+    msg: msg || ' '
+  }
+
+  // Render the login template with params.
+  const html = template.replace(/\$\{(.*?)\}/g, matched => params[matched.replace(/\$|\{|\}/g, '')] || '')
+
+  // The login view will set the cookie to null.
+  res.setHeader('Set-Cookie', `${process.env.TITLE}=null;HttpOnly;Max-Age=0;Path=${process.env.DIR || '/'}`)
+
+  res.send(html)
 
 }
