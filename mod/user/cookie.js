4fc848069cba49cf512f427a77a3418fc73f7bf4
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2020-12-02 11:26:53 +0000

    strip token from url

--- mod/user/cookie.js
+++ mod/user/cookie.js
@@ -1,5 +1,3 @@
-const getToken = require('./token')
-
 const login = require('./login')
 
 const messages = require('./messages')
@@ -8,41 +6,22 @@ const jwt = require('jsonwebtoken')
 
 module.exports = async (req, res) => {
 
-  if (!req.body) {
-  
-    const cookie = req.cookies && req.cookies[process.env.TITLE]
+  const cookie = req.cookies && req.cookies[process.env.TITLE]
 
-    if (cookie) {
+  if (!cookie) return login(req, res, messages.no_cookie_found[req.params.language || 'en'] || `No cookie relating to this application found on request`)
 
-      return jwt.verify(
-        cookie,
-        process.env.SECRET,
-        async (err, user) => {
+  jwt.verify(
+    cookie,
+    process.env.SECRET,
+    async (err, user) => {
 
-          if (err) return err
+      if (err) return err
 
-          user.iat = new Date(user.iat * 1000)
-          user.exp = new Date(user.exp * 1000)
+      user.iat = new Date(user.iat * 1000)
+      user.exp = new Date(user.exp * 1000)
 
-          res.send(user)
+      res.send(user)
 
-        })
+    })
 
-    }
-
-    return login(req, res, messages.no_cookie_found[req.params.language || 'en'] || `No cookie relating to this application found on request`)
-
-  }
-
-  const token = await getToken(req)
-
-  if (token instanceof Error) return login(req, res, token.message)
- 
-  const cookie = `${process.env.TITLE}=${token.signed || token};HttpOnly;Max-Age=28800;Path=${process.env.DIR || '/'};SameSite=Strict${!req.headers.host.includes('localhost') && ';Secure' || ''}`
-
-  res.setHeader('Set-Cookie', cookie)
-
-  res.setHeader('location', `${req.body.redirect}`)
-
-  res.status(302).send()
 }
