d283f5c917937f32db5f06be137952e04c5caeeb
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2020-12-02 11:26:53 +0000

    strip token from url

--- mod/user/auth.js
+++ mod/user/auth.js
@@ -5,7 +5,7 @@ const acl = require('./acl')()
 module.exports = async (req, res) => {
 
   // Get token from params or cookie.
-  const token = req.params.api_key || req.params.token || req.cookies && req.cookies[process.env.TITLE]
+  const token = req.params.token || req.cookies && req.cookies[process.env.TITLE]
 
   // Return to api without token
   if (!token) return null
@@ -18,36 +18,38 @@ module.exports = async (req, res) => {
     if (err) return err
 
     // API keys have an api flag in the decoded token.
-    if (user.api) {
+    // if (user.api) {
 
-      // Get user from ACL.
-      var rows = await acl(`
-        SELECT * FROM acl_schema.acl_table
-        WHERE lower(email) = lower($1);`, [user.email])
+    //   // Get user from ACL.
+    //   var rows = await acl(`
+    //     SELECT * FROM acl_schema.acl_table
+    //     WHERE lower(email) = lower($1);`, [user.email])
 
-      if (rows instanceof Error) return rows
+    //   if (rows instanceof Error) return rows
 
-      const api_user = rows[0]
+    //   const api_user = rows[0]
 
-      // Check whether the stored key matches the provided key.
-      if (!api_user || !api_user.api || (api_user.api !== token)) {
-        return new Error('API Key mismatch')
-      }
+    //   // Check whether the stored key matches the provided key.
+    //   if (!api_user || !api_user.api || (api_user.api !== token)) {
+    //     return new Error('API Key mismatch')
+    //   }
             
-      return {
-        email: api_user.email,
-        roles: api_user.roles
-      }
-    }
+    //   return {
+    //     email: api_user.email,
+    //     roles: api_user.roles
+    //   }
+    // }
 
     // Token access must not have any admin rights. 
-    if (req.params.api_key || req.params.token) {
+    if (req.params.token) {
 
       delete req.params.key
       delete req.params.token
       delete user.admin
 
-      const cookie = jwt.sign(api_user || user, process.env.SECRET)
+      if (req.cookies && req.cookies[process.env.TITLE]) return user
+
+      const cookie = jwt.sign(user, process.env.SECRET)
 
       res.setHeader('Set-Cookie', `${process.env.TITLE}=${cookie};HttpOnly;Max-Age=28800;Path=${process.env.DIR || '/'};SameSite=Strict${!req.headers.host.includes('localhost') && ';Secure' || ''}`)
     }
