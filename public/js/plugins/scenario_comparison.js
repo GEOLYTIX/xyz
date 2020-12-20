419e9d5dc4776941a72653d5f7ecd073da9c6bc3
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2020-12-20 19:17:44 +0000

    popup header

--- public/js/plugins/scenario_comparison.js
+++ public/js/plugins/scenario_comparison.js
@@ -3,47 +3,112 @@ document.dispatchEvent(new CustomEvent('scenario_comparison', {
 
     _xyz.layers.plugins.scenario_comparison = async layer => {
 
+      layer.view.addEventListener('display-off', () => {
+
+        _xyz.layers.list.ying.remove()
+
+      })
+
+      layer.view.addEventListener('display-on', () => {
+
+        _xyz.layers.list.ying.show()
+
+      })
+
       const scenarios = await _xyz.query({
         query: 'scenario_list'
       })
 
-      layer.filter.current.scenario_id = {
-        eq: scenarios[0].scenario_id
+      const panel = layer.view.appendChild(_xyz.utils.html.node`
+      <div style="padding-right: 5px; grid-gap: 5px; display: grid;">`)
+
+      layerControls(_xyz.layers.list.ying)
+
+      panel.appendChild(_xyz.utils.html.node`
+      <div style="width: 100%; margin: 5px 0; border-bottom: 1px solid #999;">`)
+
+      layerControls(_xyz.layers.list.yang)
+
+      panel.appendChild(_xyz.utils.html.node`
+      <div style="width: 100%; margin: 5px 0; border-bottom: 1px solid #999;">`)
+
+      const report = panel.appendChild(_xyz.utils.html.node`
+      <a
+        target="_blank"
+        style="font-weight: bold; margin-bottom: 5px; color: #090;">Report View`)
+
+
+      function layerControls(_layer) {
+
+        panel.appendChild(_xyz.utils.html.node`
+          <button class="btn-drop">
+            <div
+              class="head"
+              onclick=${e => {
+                e.preventDefault();
+                e.target.parentElement.classList.toggle('active');
+              }}>
+              <span>${scenarios[0].scenario_name}</span>
+              <div class="icon"></div>
+            </div>
+            <ul>${scenarios.map(scenario => _xyz.utils.html.node`
+              <li onclick=${e => {
+
+                const drop = e.target.closest('.btn-drop')
+                drop.classList.toggle('active')
+
+                drop.querySelector('span').textContent = scenario.scenario_name
+
+                _layer.filter.current.scenario_id.eq = scenario.scenario_id
+
+                _layer.reload()
+
+                report.href = _xyz.host + '/view/report?' + _xyz.utils.paramString(
+                  Object.assign({
+                    locale: _xyz.locale.key,
+                    layers: _xyz.hooks.current.layers,
+                    lat: _xyz.hooks.current.lat,
+                    lng: _xyz.hooks.current.lng,
+                    z: _xyz.hooks.current.z,
+                    ying: _xyz.layers.list.ying.filter.current.scenario_id.eq,
+                    yang: _xyz.layers.list.yang.filter.current.scenario_id.eq
+                  })
+                )
+
+                tableUpdate()
+              
+              }}>${scenario.scenario_name}`)}`)
+
+     
+        panel.appendChild(_xyz.utils.html.node`
+          <button class="btn-drop">
+            <div
+              class="head"
+              onclick=${e => {
+                e.preventDefault();
+                e.target.parentElement.classList.toggle('active');
+              }}>
+              <span>${Object.keys(_layer.style.themes)[0]}</span>
+              <div class="icon"></div>
+            </div>
+            <ul>${Object.entries(_layer.style.themes).map(theme => _xyz.utils.html.node`
+              <li onclick=${e=>{
+                const drop = e.target.closest('.btn-drop')
+                drop.querySelector('span').textContent = theme[0]
+                drop.classList.toggle('active')
+                _layer.style.theme = theme[1]
+                _layer.reload()
+
+                _xyz.utils.render(_layer.style._legend, _xyz.layers.view.style.legend(_layer))
+
+                }}>${theme[0]}`)}`)
+
+        _layer.style._legend = panel.appendChild(_xyz.utils.html.node`<div>`)
+
+        _xyz.utils.render(_layer.style._legend, _xyz.layers.view.style.legend(_layer))
+
       }
 
-      layer.reload()
-
-      layer.view.insertBefore(_xyz.utils.html.node`
-        <div style="padding-right: 5px; margin-bottom: 5px;">
-        <button class="btn-drop">
-          <div
-            class="head"
-            onclick=${e => {
-              e.preventDefault();
-              e.target.parentElement.classList.toggle('active');
-            }}>
-            <span>${scenarios[0].scenario_name}</span>
-            <div class="icon"></div>
-          </div>
-          <ul>${scenarios.map(scenario => _xyz.utils.html.node`
-            <li onclick=${e => {
-
-              const drop = e.target.closest('.btn-drop')
-              drop.classList.toggle('active')
-
-              drop.querySelector('span').textContent = scenario.scenario_name
-
-              layer.filter.current.scenario_id.eq = scenario.scenario_id
-
-              layer.reload()
-              
-              tableUpdate()
-             
-            }}>${scenario.scenario_name}`)}`,
-        layer.view.querySelector('.drawer'))
-
-
-      if (layer.key === 'ying') return
 
       const bounds = _xyz.map && _xyz.mapview.getBounds(4326)
 
@@ -108,6 +173,18 @@ document.dispatchEvent(new CustomEvent('scenario_comparison', {
 
       _xyz.mapview.node.addEventListener('changeEnd', ()=>{
 
+        report.href = _xyz.host + '/view/report?' + _xyz.utils.paramString(
+          Object.assign({
+            locale: _xyz.locale.key,
+            layers: _xyz.hooks.current.layers,
+            lat: _xyz.hooks.current.lat,
+            lng: _xyz.hooks.current.lng,
+            z: _xyz.hooks.current.z,
+            ying: _xyz.layers.list.ying.filter.current.scenario_id.eq,
+            yang: _xyz.layers.list.yang.filter.current.scenario_id.eq
+          })
+        )
+
         if (!table.tab.classList.contains('active')) return;
 
         if (_xyz.map.getView().getZoom() < 12) return;
@@ -119,6 +196,8 @@ document.dispatchEvent(new CustomEvent('scenario_comparison', {
 
         const bounds = _xyz.mapview.getBounds(4326)
 
+        table.queryparams.ying = layer.filter.current.scenario_id.eq
+        table.queryparams.yang = _xyz.layers.list.yang.filter.current.scenario_id.eq,
         table.queryparams.xmin = bounds.west
         table.queryparams.ymin = bounds.south
         table.queryparams.xmax = bounds.east
