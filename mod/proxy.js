e413b0044076bd6e78b7ab4f018681efa9da5628
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2021-01-05 11:14:12 +0000

    encode for proxy

--- mod/proxy.js
+++ mod/proxy.js
@@ -2,15 +2,45 @@ const https = require('https')
 
 module.exports = (req, res) => {
 
+  let url = req.url.match(/\?.*/)
+
+  if (!url[0]) return
+
+  url = decodeURIComponent(url[0].substring(1))
+
+  // Find variables to be substituted.
+  url = url.replace(/\{(.*?)\}/g,
+
+    // Substitute matched variable with key value from process environment.
+    matched => process.env[`KEY_${matched.replace(/\{|\}/g, '')}`] || matched)
+
+  const proxy = https.request(url, _res => {
+    res.writeHead(_res.statusCode, _res.headers)
+    _res.pipe(res, {
+      end: true
+    })
+  })
+
+  req.pipe(proxy, {
+    end: true
+  })
+  
+}
+
+function test() {
+
   const _url = req.url.match(/\?.*/)
 
   if (!_url[0]) return
 
   // Find variables to be substituted.
-  const url = _url[0].substring(1).replace(/\$\{(.*?)\}/g,
+  const url = _url[0].substring(1)
+    .replace(/\$/,'?')
+    .replace(/\$/g,'&')
+    .replace(/\{(.*?)\}/g,
 
     // Substitute matched variable with key value from process environment.
-    matched => process.env[`KEY_${matched.replace(/\$|\{|\}/g, '')}`] || matched)
+    matched => process.env[`KEY_${matched.replace(/\{|\}/g, '')}`] || matched)
 
   const proxy = https.request(url, _res => {
     res.writeHead(_res.statusCode, _res.headers)
