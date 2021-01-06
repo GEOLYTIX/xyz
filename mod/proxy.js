bc289e0113a3ea04c9404a91b40e2db7ffe51b9d
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2021-01-06 11:31:40 +0000

    remove defaults file; clean proxy mod;

--- mod/proxy.js
+++ mod/proxy.js
@@ -26,31 +26,3 @@ module.exports = (req, res) => {
   })
   
 }
-
-function test() {
-
-  const _url = req.url.match(/\?.*/)
-
-  if (!_url[0]) return
-
-  // Find variables to be substituted.
-  const url = _url[0].substring(1)
-    .replace(/\$/,'?')
-    .replace(/\$/g,'&')
-    .replace(/\{(.*?)\}/g,
-
-    // Substitute matched variable with key value from process environment.
-    matched => process.env[`KEY_${matched.replace(/\{|\}/g, '')}`] || matched)
-
-  const proxy = https.request(url, _res => {
-    res.writeHead(_res.statusCode, _res.headers)
-    _res.pipe(res, {
-      end: true
-    })
-  })
-
-  req.pipe(proxy, {
-    end: true
-  })
-  
-}
