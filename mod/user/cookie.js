e741a3609e29bce46f6a94fb0b353a60cfb6db57
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2021-01-29 19:38:18 +0000

    idle timeout test

--- mod/user/cookie.js
+++ mod/user/cookie.js
@@ -8,6 +8,11 @@ module.exports = async (req, res) => {
 
   const cookie = req.cookies && req.cookies[process.env.TITLE]
 
+  if (req.params.destroy) {
+    res.setHeader('Set-Cookie', `${process.env.TITLE}=null;HttpOnly;Max-Age=0;Path=${process.env.DIR || '/'}`)
+    return res.send('This too shall pass')
+  }
+
   if (!cookie) return login(req, res, messages.no_cookie_found[req.params.language || 'en'] || `No cookie relating to this application found on request`)
 
   jwt.verify(
@@ -17,8 +22,16 @@ module.exports = async (req, res) => {
 
       if (err) return err
 
-      user.iat = new Date(user.iat * 1000)
-      user.exp = new Date(user.exp * 1000)
+      delete user.iat
+      delete user.exp
+
+      const token = jwt.sign(user, process.env.SECRET, {
+        expiresIn: 60
+      })
+
+      const cookie = `${process.env.TITLE}=${token};HttpOnly;Max-Age=60;Path=${process.env.DIR || '/'};SameSite=Strict${!req.headers.host.includes('localhost') && ';Secure' || ''}`
+
+      res.setHeader('Set-Cookie', cookie)
 
       res.send(user)
 
