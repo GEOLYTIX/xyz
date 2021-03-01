3699c91ecb3b168b1e04f53e7f71428c7ab12c4a
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2021-03-01 19:18:17 +0000

    comparison plugin example

--- public/js/plugins/compare_location.js
+++ public/js/plugins/compare_location.js
@@ -3,10 +3,38 @@ document.dispatchEvent(new CustomEvent('compare_location', {
 
     _xyz.locations.plugins.compare_location = entry => {
 
-      console.log(entry.location.layer)
+      _xyz.query(entry).then(response => {
 
-      entry.location.layer.comparison_tab.target
-        .appendChild(_xyz.utils.html.node`<div>${entry.location.id}`)
+        entry.location.layer.comparison_tab.columns++
+
+        const valArray = Object.values(response)
+
+        valArray.unshift(entry.location.id, entry.location.infoj[3].value)
+
+        const col = _xyz.utils.html.node`
+          <div style="display: contents;">${valArray.map((val, i)=>_xyz.utils.html`
+            <div
+              style="${`
+                border-bottom: 1px solid #ddd;
+                grid-column:${entry.location.layer.comparison_tab.columns};
+                grid-row:${i+1}`}">${val}`)}`
+
+        col.appendChild(_xyz.utils.html.node`
+          <div
+            class="hover"
+            style="${`
+              grid-column:${entry.location.layer.comparison_tab.columns};
+              grid-row:${valArray.length+1};
+              font-weight: bold;
+              color: red;`}"
+              onclick=${e=>{
+                col.remove()
+                //col.innerHTML = ''
+              }}>Remove`)
+
+        entry.location.layer.comparison_tab.target
+          .appendChild(col)
+      })
 
     }
 
--- public/js/plugins/comparison_tab.js
+++ public/js/plugins/comparison_tab.js
@@ -3,13 +3,72 @@ document.dispatchEvent(new CustomEvent('comparison_tab', {
 
     _xyz.layers.plugins.comparison_tab = layer => {
 
-      const target = _xyz.utils.html.node`<div>Locations`
+      const labels = [
+        'ID',
+        'Location',
+        'Population 2020',
+        'Population 2021',
+        'Population 2022',
+        'Population 2023',
+        'Population 2024',
+        'Population 2025',
+        'White %',
+        'Car Ownership %',
+        'House %',
+        'Flats %',
+        'Own Home %',
+        'Social Rented %',
+        'Private Rented %',
+        'Social Grade AB %',
+        'Social Grade C1 %',
+        'Social Grade C2 %',
+        'Social Grade DE %',
+        'Students %',
+        'OAC - Rural residents',
+        'OAC - Cosmopolitans',
+        'OAC - Ethnicity central',
+        'OAC - Multicultural metropolitans',
+        'OAC - Urbanites',
+        'OAC - Suburbanites',
+        'OAC - Constrained city dwellers',
+        'OAC - Hard-pressed living',
+        'Age 0 - 18 %',
+        'Age 18 -24 %',
+        'Age 25 - 44 %',
+        'Age 45 - 59 %',
+        'Age 60+ %',
+        'e-Cultural Creators',
+        'e-Professionals',
+        'e-Veterans',
+        'Youthful Urban Fringe',
+        'e-Rational Utilitarians',
+        'e-Mainstream',
+        'Passive and Uncommitted Users',
+        'Digital Seniors',
+        'Settled Offline Communities',
+        'e-Withdrawn'
+      ]
+
+      const target = _xyz.utils.html.node`
+      <div
+        style="
+          display:grid;
+          grid-template-columns: 240px repeat( auto-fit, minmax(0, 1fr) );
+          padding: 20px;">${labels.map((label, i) => _xyz.utils.html`
+            <div
+              style="${`
+                grid-column:1;
+                grid-row:${i+1};
+                font-weight: bold;
+                padding-right: 10px;
+                border-bottom: 1px solid #ddd`}">${label}`)}`
 
       layer.comparison_tab = {
         title: 'Comparison',
         display: true,
         target: target,
-        layer: layer
+        layer: layer,
+        columns: 1
       }
 
       _xyz.tabview.add(layer.comparison_tab)
