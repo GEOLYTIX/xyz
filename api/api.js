d3a92c52b3e2b19451e9b8129abe71e8feac6382
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2021-04-08 16:36:08 +0100

    missing dir redirect

--- api/api.js
+++ api/api.js
@@ -47,6 +47,13 @@ function IEdetect(sUsrAg) {
 
 module.exports = async (req, res) => {
 
+
+  // redirect if dir is missing in url path.
+  if (process.env.DIR && !req.url.match(process.env.DIR)) {
+    res.setHeader('location', `${process.env.DIR}`)
+    return res.status(302).send()
+  }
+
   if (req.headers && req.headers['user-agent'] && IEdetect(req.headers['user-agent'])) return res.send('Uh Oh... It looks like your request comes from an unsupported user agent (e.g. Internet Explorer)')
 
   if (req.url.match(/\/auth0/)) return auth0(req, res)
--- express.js
+++ express.js
@@ -64,5 +64,7 @@ app.get(`${process.env.DIR||''}/view/:template?`, api)
 
 app.get(`${process.env.DIR||''}/`, api)
 
+app.get(`/`, api)
+
 
 app.listen(process.env.PORT || 3000)
