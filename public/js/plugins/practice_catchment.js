4e63675fd13e823d7516d4110ab475bcbad9a398
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2021-05-11 17:44:36 +0100

    fix for basic theme cluster layer

--- public/js/plugins/practice_catchment.js
+++ public/js/plugins/practice_catchment.js
@@ -34,37 +34,37 @@ document.dispatchEvent(new CustomEvent('practice_catchment', {
         entry.listview.appendChild(_xyz.utils.html.node`
         <div class="label inline">OAC Index</div>
         <div class="val inline"
-          style="${`color:${index_color(response.oac_index)}`}">${response.oac_index}`)
+          style="${`color:${index_color(response.oac_index)}`}">${parseFloat(response.oac_index).toFixed(2)}`)
 
         entry.listview.appendChild(_xyz.utils.html.node`
         <div class="label inline">Index - NHS exempt</div>
         <div class="val inline"
-          style="${`color:${index_color(response.nhs_ex_index)}`}">${response.nhs_ex_index}`)
+          style="${`color:${index_color(response.nhs_ex_index)}`}">${parseFloat(response.nhs_ex_index).toFixed(2)}`)
 
         entry.listview.appendChild(_xyz.utils.html.node`
         <div class="label inline">Index - NHS Fee Pay</div>
         <div class="val inline" 
-          style="${`color:${index_color(response.nhs_fee_index)}`}">${response.nhs_fee_index}`)
+          style="${`color:${index_color(response.nhs_fee_index)}`}">${parseFloat(response.nhs_fee_index).toFixed(2)}`)
 
         entry.listview.appendChild(_xyz.utils.html.node`
         <div class="label inline">Index - Full Price Private</div>
         <div class="val inline"
-          style="${`color:${index_color(response.private_fp_index)}`}">${response.private_fp_index}`)
+          style="${`color:${index_color(response.private_fp_index)}`}">${parseFloat(response.private_fp_index).toFixed(2)}`)
 
         entry.listview.appendChild(_xyz.utils.html.node`
         <div class="label inline">Index - Private Affordable</div>
         <div class="val inline"
-          style="${`color:${index_color(response.private_afford_index)}`}">${response.private_afford_index}`)
+          style="${`color:${index_color(response.private_afford_index)}`}">${parseFloat(response.private_afford_index).toFixed(2)}`)
 
         entry.listview.appendChild(_xyz.utils.html.node`
         <div class="label inline">Competitiveness Index</div>
         <div class="val inline"
-          style="${`color:${index_color(response.competitiveness)}`}">${response.competitiveness}`)
+          style="${`color:${index_color(response.competitiveness)}`}">${parseFloat(response.competitiveness).toFixed(2)}`)
 
         entry.listview.appendChild(_xyz.utils.html.node`
         <div class="label inline">Gap to NHS Index</div>
         <div class="val inline"
-          style="${`color:${index_color(response.gap_to_nhs_index)}`}">${response.gap_to_nhs_index}`)          
+          style="${`color:${index_color(response.gap_to_nhs_index)}`}">${parseFloat(response.gap_to_nhs_index).toFixed(2)}`)          
 
         entry.listview.appendChild(_xyz.utils.html.node`
         <div class="label inline">Total Demand (£)</div>
