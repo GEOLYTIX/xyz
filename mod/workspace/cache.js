c216ab2f7543a6d6ee656263781f2d3da36a674e
Wagner Mattei <wagner.mattei@gmail.com>
2021-05-19 08:32:21 -0300

    fix workspace setting

--- mod/workspace/cache.js
+++ mod/workspace/cache.js
@@ -33,93 +33,89 @@ mongoClient.connect();
 module.exports = async (req) => {
   let timestamp = Date.now();
 
-  module.exports = async (req) => {
-    let timestamp = Date.now();
-
-    // If the workspace is empty or older than 1hr it needs to be cached.
-    if (
-      !workspace ||
-      timestamp - workspace.timestamp > 3600000 ||
-      process.env.WORKSPACE === 'dynamic'
-    ) {
-      if (!workspace) {
-        logger(`workspace is empty ${timestamp}`);
-      } else if (timestamp - workspace.timestamp > 3600000) {
-        logger(
-          `workspace has expired ${workspace.timestamp} | new timestamp is ${timestamp}`
-        );
-      }
-
-      if (process.env.WORKSPACE === 'dynamic') {
-        if (!req) return new Error('Not request found');
-        const { slug, project, lang } = req.query;
-        if (!slug) {
-          return new Error('No slug specified');
-        }
-        if (!project) {
-          return new Error('No project specified');
-        }
-        if (!lang) {
-          return new Error('No lang specified');
-        }
-        const Page = mongoClient.db('acorn').collection('pages');
-        const Version = mongoClient.db('acorn').collection('versions');
-        const proposalPage = await Page.findOne({
-          slug,
-          type: 'map',
-          projectId: new ObjectId(project),
-          active: true,
-        });
-        if (!proposalPage) return;
-        // get the most recent version id
-        const contentVersionId = proposalPage?.content[lang][0];
-        if (!contentVersionId) return;
-        const proposalContent = await Version.findOne({
-          _id: ObjectId(contentVersionId),
-        });
-        workspace = proposalContent.content.geolytixWorkspace;
-      } else {
-        workspace =
-          (process.env.WORKSPACE &&
-            (await getFrom[process.env.WORKSPACE.split(':')[0]](
-              process.env.WORKSPACE
-            ))) ||
-          {};
-      }
-      if (workspace instanceof Error) return workspace;
-
-      // Return the default locale
-      if (!workspace.locales) {
-        workspace = {
-          locales: {
-            zero: defaults.locale,
-          },
-        };
-        return workspace;
-      }
-
-      await assignTemplates(workspace);
-
-      await assignDefaults(workspace);
-
-      // Substitute SRC_* variables in locales.
-      workspace.locales = JSON.parse(
-        JSON.stringify(workspace.locales).replace(
-          /\$\{(.*?)\}/g,
-          (matched) =>
-            process.env[`SRC_${matched.replace(/\$|\{|\}/g, '')}`] || matched
-        )
-      );
-
-      workspace.timestamp = timestamp;
-    } else {
+  // If the workspace is empty or older than 1hr it needs to be cached.
+  if (
+    !workspace ||
+    timestamp - workspace.timestamp > 3600000 ||
+    process.env.WORKSPACE === 'dynamic'
+  ) {
+    if (!workspace) {
+      logger(`workspace is empty ${timestamp}`);
+    } else if (timestamp - workspace.timestamp > 3600000) {
       logger(
-        `workspace cached ${workspace.timestamp} | age ${
-          timestamp - workspace.timestamp
-        }`
+        `workspace has expired ${workspace.timestamp} | new timestamp is ${timestamp}`
       );
     }
 
-    return workspace;
-  };
+    if (process.env.WORKSPACE === 'dynamic') {
+      if (!req) return new Error('Not request found');
+      const { slug, project, lang } = req.query;
+      if (!slug) {
+        return new Error('No slug specified');
+      }
+      if (!project) {
+        return new Error('No project specified');
+      }
+      if (!lang) {
+        return new Error('No lang specified');
+      }
+      const Page = mongoClient.db('acorn').collection('pages');
+      const Version = mongoClient.db('acorn').collection('versions');
+      const proposalPage = await Page.findOne({
+        slug,
+        type: 'map',
+        projectId: new ObjectId(project),
+        active: true,
+      });
+      if (!proposalPage) return;
+      // get the most recent version id
+      const contentVersionId = proposalPage?.content[lang][0];
+      if (!contentVersionId) return;
+      const proposalContent = await Version.findOne({
+        _id: ObjectId(contentVersionId),
+      });
+      workspace = proposalContent.content.geolytixWorkspace;
+    } else {
+      workspace =
+        (process.env.WORKSPACE &&
+          (await getFrom[process.env.WORKSPACE.split(':')[0]](
+            process.env.WORKSPACE
+          ))) ||
+        {};
+    }
+    if (workspace instanceof Error) return workspace;
+
+    // Return the default locale
+    if (!workspace.locales) {
+      workspace = {
+        locales: {
+          zero: defaults.locale,
+        },
+      };
+      return workspace;
+    }
+
+    await assignTemplates(workspace);
+
+    await assignDefaults(workspace);
+
+    // Substitute SRC_* variables in locales.
+    workspace.locales = JSON.parse(
+      JSON.stringify(workspace.locales).replace(
+        /\$\{(.*?)\}/g,
+        (matched) =>
+          process.env[`SRC_${matched.replace(/\$|\{|\}/g, '')}`] || matched
+      )
+    );
+
+    workspace.timestamp = timestamp;
+  } else {
+    logger(
+      `workspace cached ${workspace.timestamp} | age ${
+        timestamp - workspace.timestamp
+      }`
+    );
+  }
+
+  return workspace;
 };
