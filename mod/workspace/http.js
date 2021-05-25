897cdc11aaf4b17a89d10e1c2e9a3657a5c4d74c
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2021-05-25 16:25:05 +0100

    custom_agent

--- mod/workspace/http.js
+++ mod/workspace/http.js
@@ -1,9 +1,18 @@
 const fetch = require('node-fetch')
 
+const https = require('https')
+
+const httpsAgent = new https.Agent({
+	keepAlive: true,
+  maxSockets: 1
+})
+
 module.exports = async ref => {
   try {
 
-    const response = await fetch(ref)
+    const response = await fetch(ref, {
+      agent: process.env.CUSTOM_AGENT && httpsAgent
+    })
 
     if (response.status >= 300) return new Error(`${response.status} ${ref}`)
 
