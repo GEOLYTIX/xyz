00af0c0f97f5d3ecf903c5fb40f57c28fe0f7e3e
conorheena <conor@commonplace.is>
2021-05-30 21:57:36 +0100

    passing metadata properties to geometry layers

--- mod/location/new.js
+++ mod/location/new.js
@@ -1,11 +1,10 @@
 const dbs = require('../dbs')()
 
 module.exports = async (req, res) => {
-
   const layer = req.params.layer
   var q = `
-  INSERT INTO ${req.params.table} (${layer.geom})
-  SELECT ST_SetSRID(ST_MakeValid(ST_GeomFromGeoJSON('${JSON.stringify(req.body.geometry)}')), ${layer.srid})
+  INSERT INTO ${req.params.table} (${layer.geom}, metadata)
+  (SELECT ST_SetSRID(ST_MakeValid(ST_GeomFromGeoJSON('${JSON.stringify(req.body.geometry)}')), ${layer.srid}),'${JSON.stringify(req.body.properties.metadata)}')
   RETURNING ${layer.qID} AS id;`
 
   var rows = await dbs[layer.dbs](q)
