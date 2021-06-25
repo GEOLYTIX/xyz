17aca8c2e091310f52c820109ff2569c607b620e
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2021-06-25 17:55:38 +0100

    auth0 test

--- mod/user/auth0.js
+++ mod/user/auth0.js
@@ -1,5 +1,7 @@
 const fetch = require('node-fetch')
 
+const jwt = require('jsonwebtoken')
+
 module.exports = async (req, res) => {
 
   if (req.url.match(/\/auth0\/logout/)) {
@@ -8,7 +10,7 @@ module.exports = async (req, res) => {
       `https://geolytix-saml.eu.auth0.com/v2/logout`
       + `?client_id=${process.env.AUTH_CLIENTID}`
       + `&federated=https://auth.pingone.eu/dd912efb-f9b0-4fa5-9c9c-68c317910396/saml20/idp/slo`
-      + `&returnTo=http://localhost:3000/`)
+      + `&returnTo=https://dev-auth0.vercel.app/`)
 
     return res.status(302).send()
   }
@@ -19,9 +21,9 @@ module.exports = async (req, res) => {
       `https://geolytix-saml.eu.auth0.com/authorize`
       + `?response_type=code`
       + `&client_id=${process.env.AUTH_CLIENTID}`
-      + `&scope=openid profile email`
+      + `&scope=openid email username roles`
       + `&connection=geolytix-saml-connection`
-      + `&redirect_uri=http://localhost:3000/auth0/callback`)
+      + `&redirect_uri=https://dev-auth0.vercel.app/auth0/callback`)
 
     return res.status(302).send()
   }
@@ -34,7 +36,7 @@ module.exports = async (req, res) => {
         client_id: process.env.AUTH_CLIENTID,
         client_secret: process.env.AUTH_SECRET,
         code: req.query.code,
-        redirect_uri: 'http://localhost:3000/auth0/callback'
+        redirect_uri: 'https://dev-auth0.vercel.app/auth0/callback'
       })
       .map(entry => `${encodeURIComponent(entry[0])}=${encodeURIComponent(entry[1])}`)
       .join('&')
@@ -46,10 +48,6 @@ module.exports = async (req, res) => {
     })
   
     const oauth_json = await oauth_response.json()
-
-    //console.log(oauth_json)
-
-    //return res.send(oauth_json)
  
     const userinfo_response = await fetch(
       `https://geolytix-saml.eu.auth0.com/userinfo`, {
@@ -60,8 +58,24 @@ module.exports = async (req, res) => {
       })
   
     const userinfo_json = await userinfo_response.json()
-  
-    res.send(userinfo_json)
+
+    // Create token with 8 hour expiry.
+    const token = jwt.sign({
+        email: userinfo_json.email,
+        language: userinfo_json.language || 'en',
+        roles: userinfo_json['https://geolytix.xyz/roles'],
+      },
+      process.env.SECRET, {
+        expiresIn: parseInt(process.env.COOKIE_TTL)
+      })
+
+    const cookie = `${process.env.TITLE}=${token};HttpOnly;Max-Age=${process.env.COOKIE_TTL};Path=${process.env.DIR || '/'}${!req.headers.host.includes('localhost') && ';Secure' || ''}`
+
+    res.setHeader('Set-Cookie', cookie)
+
+    res.setHeader('location', `https://dev-auth0.vercel.app/`)
+    res.status(302).send()
+
   }
 
 }
