85376ae2f98346694dfdc8b207f0d7934d83bb3e
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2021-07-28 12:33:04 +0100

    body parsing issues for queries

--- public/js/plugins/csv_import.js
+++ public/js/plugins/csv_import.js
@@ -28,49 +28,43 @@ document.dispatchEvent(new CustomEvent('csv_import', {
 
         const reader = new FileReader()
 
-        reader.onload = function() {
-          try {
+        reader.onload = async function() {
 
-            const xhr = new XMLHttpRequest()
+          var _text = this.result.split(/\r?\n/)
+        
+          var header = _text.shift().split(';')
+          
+          var _list = _text.map(row => {
 
-            xhr.open('POST', `${_xyz.host}/api/query/csv_import?statement_timeout=1000000&nonblocking=true`)
+            if (!row.length) return
 
-            xhr.setRequestHeader('Content-Type', 'application/json')
+            const _row = row.split(';')
 
-            xhr.responseType = 'json'
+            const o = {nano_id:nanoid}
 
-            xhr.onload = e => {
-
-              if (e.target.status !== 200) return alert('Import went wrong. Likely unescaped characters found in your input.')
-
-              setTimeout(ping, 10000)
+            for (let i = 0; i < header.length; i++) {
+              o[header[i]] = _row[i]
             }
-        
-            var _text = this.result.split(/\r?\n/)
-        
-            var header = _text.shift().split(';')
-            
-            var _list = _text.map(row => {
+      
+            return o
+      
+          }).filter(o => typeof o === 'object')
+         
+          const response = await _xyz.xhr({
+            method: 'POST',
+            url: `${_xyz.host}/api/query?${_xyz.utils.paramString({
+              template: 'csv_import',
+              statement_timeout: 1000000,
+              nonblocking: true,
+              stringifyBody: true,
+            })}`,
+            body: JSON.stringify(_list)
+          })
 
-              if (!row.length) return
+          console.log(response)
 
-              const _row = row.split(';')
+          setTimeout(ping, 10000)
 
-              const o = {nano_id:nanoid}
-
-              for (let i = 0; i < header.length; i++) {
-                o[header[i]] = _row[i]
-              }
-        
-              return o
-        
-            }).filter(o => typeof o === 'object')
-        
-            xhr.send(JSON.stringify(_list))
-
-          } catch (err) {
-            console.error(err)
-          }
         }
 
         reader.readAsText(this.files[0])
@@ -80,15 +74,12 @@ document.dispatchEvent(new CustomEvent('csv_import', {
 
         async function ping() {
 
-          const response = await _xyz.query({
-            query: 'csv_import_ping',
-            queryparams: {
-              nano_id: nanoid
-            }
-          })
+          const response = await _xyz.xhr(`${_xyz.host}/api/query?${_xyz.utils.paramString({
+            template: 'csv_import_ping',
+            nano_id: nanoid,
+          })}`)
 
           if (response) {
-
             console.log(response)
             alert(`The model has successfully run for batch number ${response.val}`)
             button.disabled = false
@@ -105,5 +96,113 @@ document.dispatchEvent(new CustomEvent('csv_import', {
 
   }
 }))
+
+// document.dispatchEvent(new CustomEvent('csv_import', {
+//   detail: _xyz => {
+
+//     _xyz.layers.plugins.csv_import = layer => {
+
+//       const input = _xyz.utils.html.node`
+//       <input type="file" accept=".csv" style="display: none;">`
+        
+//       const div = _xyz.utils.html.node`
+//       <div style="padding-right: 5px">`
+
+//       layer.view.appendChild(div)
+
+//       const button = _xyz.utils.html.node`
+//       <button
+//         class="btn-wide primary-colour"
+//         onclick=${e => {
+//           e.stopPropagation()
+//           input.click()
+
+//       }}>CSV Import${input}`
+
+//       div.appendChild(button)
+
+//       input.addEventListener('change', function() {
+
+//         const nanoid = _xyz.utils.nanoid(6)
+
+//         const reader = new FileReader()
+
+//         reader.onload = function() {
+//           try {
+
+//             const xhr = new XMLHttpRequest()
+
+//             xhr.open('POST', `${_xyz.host}/api/query/csv_import?statement_timeout=1000000&nonblocking=true`)
+
+//             xhr.setRequestHeader('Content-Type', 'application/json')
+
+//             xhr.responseType = 'json'
+
+//             xhr.onload = e => {
+
+//               if (e.target.status !== 200) return alert('Import went wrong. Likely unescaped characters found in your input.')
+
+//               setTimeout(ping, 10000)
+//             }
+        
+//             var _text = this.result.split(/\r?\n/)
+        
+//             var header = _text.shift().split(';')
+            
+//             var _list = _text.map(row => {
+
+//               if (!row.length) return
+
+//               const _row = row.split(';')
+
+//               const o = {nano_id:nanoid}
+
+//               for (let i = 0; i < header.length; i++) {
+//                 o[header[i]] = _row[i]
+//               }
+        
+//               return o
+        
+//             }).filter(o => typeof o === 'object')
+        
+//             xhr.send(JSON.stringify(_list))
+
+//           } catch (err) {
+//             console.error(err)
+//           }
+//         }
+
+//         reader.readAsText(this.files[0])
+
+//         input.value = null
+//         button.disabled = true
+
+//         async function ping() {
+
+//           const response = await _xyz.query({
+//             query: 'csv_import_ping',
+//             queryparams: {
+//               nano_id: nanoid
+//             }
+//           })
+
+//           if (response) {
+
+//             console.log(response)
+//             alert(`The model has successfully run for batch number ${response.val}`)
+//             button.disabled = false
+//             layer.reload()
+//             return
+//           }
+
+//           setTimeout(ping, 10000) 
+//         }
+
+//       })
+
+//     }
+
+//   }
+// }))
 
 //select * from camelot.camelot_check_process_finish WHERE id='P9nki'
