0ce810f1165104bc6929ef00fee91be50b3e2e44
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2021-08-17 14:40:37 +0100

    remove dbscan cluster method

--- public/js/plugins/cluster.js
+++ public/js/plugins/cluster.js
@@ -21,11 +21,13 @@ function detail(_xyz) {
             class="btn-header xyz-icon icon-expander primary-colour-filter">`)
 
 
-    panel.appendChild(_xyz.utils.html.node`
+      layer.cluster_kmeans && panel.appendChild(_xyz.utils.html.node`
       <div>
         <div style="display: grid; align-items: center;">
-          <div style="grid-column: 1;">kMeans</div>
-          <div style="grid-column: 2;">${layer.cluster_kmeans}</div>
+          <div
+            title="The minimum number of cluster in the viewport."
+            style="grid-column: 1;">kMeans</div>
+          <div style="grid-column: 2;">${parseInt(1 / layer.cluster_kmeans)}</div>
           <div
             class="input-range"
             style="grid-column: 3;">
@@ -33,52 +35,28 @@ function detail(_xyz) {
               type="range"
               class="secondary-colour-bg"
               min=0
-              value=${layer.cluster_kmeans}
-              max=0.5
-              step=${0.01}
+              value=${parseInt(1 / layer.cluster_kmeans)}
+              max=100
+              step=1
               oninput=${ e =>{
                         
-                layer.cluster_kmeans = e.target.value
+                layer.cluster_kmeans = 1 / e.target.value
         
                 e.target.parentNode.previousSibling.previousSibling.textContent = e.target.value
         
                 clearTimeout(timeout)
         
                 timeout = setTimeout(() => layer.reload(), 400)
-              }}>`)
-
-      panel.appendChild(_xyz.utils.html.node`
-        <div>
-          <div style="display: grid; align-items: center;">
-          <div style="grid-column: 1;">dbScan</div>
-          <div style="grid-column: 2;">${layer.cluster_dbscan}</div>
-          <div
-            class="input-range"
-            style="grid-column: 3;">
-            <input 
-              type="range"
-              class="secondary-colour-bg"
-              min=0
-              value=${layer.cluster_dbscan}
-              max=0.5
-              step=${0.01}
-              oninput=${ e =>{
-                                
-                layer.cluster_dbscan = e.target.value
-                
-                e.target.parentNode.previousSibling.previousSibling.textContent = e.target.value
-                
-                clearTimeout(timeout)
-                
-                timeout = setTimeout(() => layer.reload(), 400)
-              }}>`)               
+              }}>`)          
 
     layer.style.cluster.clusterScale = layer.style.cluster.icon?.clusterScale || layer.style.cluster.clusterScale || 2
 
     panel.appendChild(_xyz.utils.html.node`
       <div>
         <div style="display: grid; align-items: center;">
-          <div style="grid-column: 1;">Scale</div>
+          <div 
+            title="Scale applied in addition to the base scale of the largest cluster icon."
+            style="grid-column: 1;">Scale</div>
           <div style="grid-column: 2;">${layer.style.default.scale}</div>
           <div
             class="input-range"
