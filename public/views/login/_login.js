9a3afb0151ad23ce723fb1103196428555954383
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2021-08-18 15:52:19 +0100

    login messages and verify redirects

--- public/views/login/_login.js
+++ public/views/login/_login.js
@@ -1,5 +1,13 @@
 window.onload = () => {
 
+  let url = new URL(window.location.href)
+  let params = new URLSearchParams(url.search)
+  params.delete('msg')
+  window.history.pushState(
+    {},
+    document.title,
+    `?${params.toString()}`)
+
   const email = document.getElementById('auth_user_email');
   const password = document.getElementById('auth_user_password');
   const btnLogin = document.getElementById('btnLogin');
