0aff5acafdf8e1ebb6111062a119d3deb8fb184f
Agata Brok <cityremade@users.noreply.github.com>
2022-03-08 14:20:55 +0000

    Features of interest plugin.

---
+++ public/js/plugins/features_of_interest.js
@@ -0,0 +1,26 @@
+document.dispatchEvent(new CustomEvent('features_of_interest', {
+    detail: _xyz => {
+
+        _xyz.locations.plugins.features_of_interest = entry => {
+          
+          if(!_xyz.foi) _xyz.foi = []
+
+          let label = _xyz.utils.html.node`<div>Add to Features of Interest`
+
+          entry.location.view.appendChild(_xyz.utils.html.node`<button class="primary-colour" style="margin-top: 10px;"
+            onclick=${e => {
+              e.stopPropagation()
+
+              let idx = _xyz.foi.indexOf(entry.location.hook)
+
+              idx === -1 ? _xyz.foi.push(entry.location.hook) : _xyz.foi.splice(idx, 1)
+
+              label.textContent = idx === -1 ? 'Remove from Features of Interest' : 'Add to Features of Interest'
+
+            }}>
+            <div style="display: grid; grid-template-columns: 1em auto; margin:auto;">
+            <div style="height: 1em;" class="xyz-icon icon-add"></div>${label}`)
+        
+        }
+    }
+}))
