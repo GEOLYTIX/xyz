30b3f822ada5d3fcab8ad1d13d229f3bbfba5518
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2022-04-28 15:59:59 +0100

    esbuild test

--- public/js/lib/dictionaries/_dictionary.js
+++
@@ -1,9 +0,0 @@
-const dictionary = new Proxy({}, {
-  get: function(target, key, receiver) {
-    if (mapp.dictionaries[mapp.language][key]) {
-      return mapp.dictionaries[mapp.language][key];
-    }
-    return mapp.dictionaries.en[key];
-  }
-});
-export default dictionary;
