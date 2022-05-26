abf748bceda5cc5e2864ba2637081ce80def256a
Robert <robert.hurst@geolytix.co.uk>
2022-05-26 14:50:52 +0200

    Get Last Location change

--- public/js/queries/get_last_location.js
+++ public/js/queries/get_last_location.js
@@ -1,19 +1,14 @@
 module.exports = _ => {
 
   const layer = _.workspace.locales[_.locale].layers[_.layer]
-  
+
   const table = layer.table || Object.values(layer.tables).find(tab => !!tab);
 
-  const fields = layer.infoj
-    .filter(entry => !entry.query)
-    .filter(entry => entry.type !== 'key')
-    .filter(entry => entry.field)
-    .map(entry => `(${entry.fieldfx || entry.field}) AS ${entry.field}`)
-
   return `
-    SELECT
-      ${fields.join()}
-    FROM ${table}
-    ORDER BY ${layer.qID} DESC
-    LIMIT 1`
+          SELECT
+          ${layer.qID} as id
+          FROM ${table}
+          WHERE ${layer.geom} IS NOT NULL AND ${layer.qID} IS NOT NULL
+          ORDER BY ${layer.qID} DESC
+          LIMIT 1`
 }
