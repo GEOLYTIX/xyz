4cbced053586aa49dbe8b2af0c77437d5c96833a
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2022-06-06 17:55:39 +0100

    use client for pool connection in dbs module

--- mod/utils/dbs.js
+++ mod/utils/dbs.js
@@ -2,85 +2,48 @@ const { Pool } = require('pg');
 
 const dbs = {};
 
-const logger = require('./logger')
-
 module.exports = () => {
 
   Object.keys(process.env)
+
+    // Filter keys which start with DBS 
     .filter(key => key.split('_')[0] === 'DBS')
+
+    // Filter keys which are not yet assigned to dbs object.
     .filter(key => !dbs[key.split('_')[1]])
+
     .forEach(key => {
 
-      logger(key, 'dbs')
+      const pool = new Pool({
+        connectionString: process.env[key],
+        statement_timeout: parseInt(process.env.STATEMENT_TIMEOUT) || 10000
+      });
+  
+      dbs[key.split('_')[1]] = async (q, arr, timeout) => {
+  
+        // Request which accepts q and arr and will return rows or rows.err.
+        try {
 
-      if (process.env[key].match(/^postgres/)) return postgres(key.split('_')[1], process.env[key])
+          const client = await pool.connect()
+          
+          timeout && await client.query(`SET statement_timeout = $1`,[timeout])
+  
+          const { rows } = await client.query(q, arr)
+  
+          timeout && await client.query(`SET statement_timeout = 10000`)
 
-      // if (process.env[key].match(/^aurora/)) return aurora(key.split('_')[1], process.env[key])
-
-    })
-
-  function postgres(key, connectionString) {
-
-    // Create connection pool.
-    const pool = new Pool({
-      connectionString: connectionString,
-      statement_timeout: parseInt(process.env.STATEMENT_TIMEOUT) || 10000
-    });
-
-    dbs[key] = async (q, arr, timeout) => {
-
-      // Request which accepts q and arr and will return rows or rows.err.
-      try {
-        timeout && await pool.query(`SET statement_timeout = $1`,[timeout])
-
-        const { rows } = await pool.query(q, arr)
-
-        timeout && await pool.query(`SET statement_timeout = 10000`)
-
-        return rows
-
-      } catch (err) {
-        console.error(err)
-        return err
+          client.release()
+  
+          return rows
+  
+        } catch (err) {
+          console.error(err)
+          return err
+        }
+  
       }
 
-    }
-
-  }
-
-  // function aurora(key, connectionString) {
-
-  //   const keyValueArr = connectionString.split('|')
-    
-  //   const apiclient = require('data-api-client')({
-  //     engine: 'pg',
-  //     options: {
-  //       accessKeyId: keyValueArr[1],
-  //       secretAccessKey: keyValueArr[2],
-  //       region: keyValueArr[3],
-  //     },
-  //     resourceArn: keyValueArr[4],
-  //     secretArn: keyValueArr[5],
-  //     database: keyValueArr[6]
-  //   })
-
-  //   dbs[key] = async (q) => {
-
-  //     // Request which accepts q and arr and will return rows or rows.err.
-  //     try {
-  
-  //       const { records } = await apiclient.query(q);
-  
-  //       return records
-  
-  //     } catch (err) {
-  //       console.error(err)
-  //       return err
-  //     }
-  
-  //   }
-
-  // }
+    })
 
   return dbs
 
