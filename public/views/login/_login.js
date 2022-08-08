a543438d9615b8d444f2c3e79a8f3b425f41577e
roberthurstsa@gmail.com <robert.hurst@geolytix.co.uk>
2022-08-08 16:49:02 +0200

    SSO conditional change

--- public/views/login/_login.js
+++ public/views/login/_login.js
@@ -1,8 +1,9 @@
 window.onload = () => {
 
-  const saml_sso = document.getElementById('saml_sso')
-  
-  saml_sso.style.display = saml_sso.href ? 'block' : 'none';
+  if (document.getElementById('saml_sso')) {
+    const saml_sso = document.getElementById('saml_sso');
+    saml_sso.style.display = saml_sso.href ? 'block' : 'none';
+  }
 
   const email = document.getElementById('auth_user_email');
   const password = document.getElementById('auth_user_password');
