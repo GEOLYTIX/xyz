93e6becc0c142c7e7f599855f72f52d6e64b36aa
roberthurstsa@gmail.com <robert.hurst@geolytix.co.uk>
2022-11-09 17:31:54 +0200

    get_last_location query update

--- public/js/queries/get_last_location.js
+++ public/js/queries/get_last_location.js
@@ -4,11 +4,13 @@ module.exports = _ => {
 
   const table = layer.table || Object.values(layer.tables).find(tab => !!tab);
 
+  const geom = layer.geom || Object.values(layer.geoms).find(tab => !!tab);
+
   return `
           SELECT
           ${layer.qID} as id
           FROM ${table}
-          WHERE ${layer.geom} IS NOT NULL AND ${layer.qID} IS NOT NULL
+          WHERE ${geom} IS NOT NULL AND ${layer.qID} IS NOT NULL
           ORDER BY ${layer.qID} DESC
           LIMIT 1`
 }
