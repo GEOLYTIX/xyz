19ede0b65e7973e3b4c51ce8c999b9c625cf576d
roberthurstsa@gmail.com <robert.hurst@geolytix.co.uk>
2022-12-13 18:26:18 +0000

    ESLint and lint Dataviews.mjs

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    eqeqeq: 'off',
+    quotes: ['error', 'single']
+  }
+}
