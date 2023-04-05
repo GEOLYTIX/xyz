2a51348c6eb982869075099f8823100fe9cf27dd
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2023-04-05 12:26:57 +0100

    validate own property of methods object

--- mod/location/_location.js
+++ mod/location/_location.js
@@ -1,19 +1,21 @@
-const methods = new Map()
-
-methods.set('new', require('./new'))
-methods.set('get', require('./get'))
-methods.set('update', require('./update'))
-methods.set('delete', require('./delete'))
+const methods = {
+  new: require('./new'),
+  get: require('./get'),
+  update: require('./update'),
+  delete: require('./delete'),
+}
 
 module.exports = async (req, res) => {
 
-  const method = methods.get(req.params.method)
-
-  if (typeof method !== 'function') {
+  if (!methods.hasOwnProperty(req.params.method)) {
     return res.send(`Failed to evaluate 'method' param.<br><br>
     <a href="https://geolytix.github.io/xyz/docs/develop/api/location/">Location API</a>`)
   }
 
+  const method = methods[req.params.method]
+
+  if (typeof method !== 'function') return;
+  
   const locale = req.params.locale && req.params.workspace.locales[req.params.locale]
 
   const layer = locale && locale.layers[req.params.layer] ||  req.params.workspace.templates[req.params.layer]
