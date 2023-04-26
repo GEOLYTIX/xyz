f6a7cef254fe6190f0dddbc198c85eefc621dcfd
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2023-04-26 09:42:54 +0100

    Validate the locale and layer request param

--- mod/layer/_layer.js
+++ mod/layer/_layer.js
@@ -1,4 +1,4 @@
-const _format = {
+const formats = {
   cluster: require('./cluster'),
   mvt: require('./mvt'),
   geojson: require('./geojson'),
@@ -8,24 +8,43 @@ const _format = {
 
 module.exports = async (req, res) => {
 
-  const format = _format[req.params.format]
-
-  if (!format) {
-    return res.send(`Failed to evaluate 'format' param.<br><br>
-    <a href="https://geolytix.github.io/xyz/docs/develop/api/layer/">Layer API</a>`)
-  }
-
-  const locale = req.params.locale && req.params.workspace.locales[req.params.locale]
-
-  const layer = locale && locale.layers[req.params.layer] ||  req.params.workspace.templates[req.params.layer]
-
-  req.params.layer = layer
-
   if (!req.params.layer) {
     return res.send(`Failed to evaluate 'layer' param.<br><br>
     <a href="https://geolytix.github.io/xyz/docs/develop/api/layer/">Layer API</a>`)
   }
 
-  return format(req, res)
+  // The format key must be an own property of the formats object.
+  if (!Object.hasOwn(formats, req.params.format)) {
+    return res.send(`Failed to evaluate 'format' param.<br><br>
+    <a href="https://geolytix.github.io/xyz/docs/develop/api/layer/">Layer API</a>`)
+  }
 
+  if (req.params.locale) {
+    
+    // The locale key must be an own property of the workspace.locales, and must be an object.
+    if (Object.hasOwn(req.params.workspace.locales, req.params.locale)
+      && typeof req.params.workspace.locales[req.params.locale] === 'object') {
+
+        // Assign layer from locale in workspace.
+        req.params.layer = req.params.workspace.locales[req.params.locale].layers[req.params.layer]
+
+    } else {
+
+      // Terminate request if locale is defined but not valid.
+      return res.send(`Failed to evaluate locale param.`)
+    }
+
+  // A layer must be specified in the templates without a locale specifier, and must be an object
+  } else if (Object.hasOwn(req.params.workspace.templates, req.params.layer)
+    && typeof req.params.workspace.templates[req.params.layer] === 'object') {
+
+    // Assign layer object from templates.
+    req.params.layer = req.params.workspace.templates[req.params.layer]
+
+  } else {
+
+    return res.send(`Failed to evaluate layer param.`)
+  }
+    
+  return formats[req.params.format](req, res)
 }
