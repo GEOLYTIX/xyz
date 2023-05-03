1b99194e00ee7b204a92ee7e862805d9e6839bc1
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2023-05-03 17:54:23 +0100

    lookup proxy from environment settings

--- mod/_proxy.js
+++ mod/_proxy.js
@@ -1,12 +1,8 @@
 const https = require('https')
 
-const proxies = {
-    isoline_here: 'router.hereapi.com/v8/isolines'
-}
-
 module.exports = (req, res) => {
 
-  if (!Object.hasOwn(proxies, req.params.proxy)) return;
+  if (!Object.hasOwn(process.env, `PROXY_${req.params.proxy}`)) return;
 
   // Find variables to be substituted.
   const url_params = req.params.params?.replace(/\{{1}(.+?)\}{1}/g,
@@ -14,7 +10,7 @@ module.exports = (req, res) => {
     // Substitute matched variable with key value from process environment.
     matched => process.env[`KEY_${matched.replace(/\{{1}|\}{1}/g, '')}`] || matched)
 
-  const proxy = https.request(`https://${proxies[req.params.proxy]}?${url_params}`, _res => {
+  const proxy = https.request(`https://${process.env[`PROXY_${req.params.proxy}`]}?${url_params}`, _res => {
     res.writeHead(_res.statusCode, _res.headers)
     _res.pipe(res, {
       end: true
