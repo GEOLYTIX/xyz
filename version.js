b8b93bac230270d4b707c622e746de68b9be9ce6
Rob Hurst <43169158+RobAndrewHurst@users.noreply.github.com>
2023-05-31 08:02:23 +0200

    Version bump v4.5.0α

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
 
-const key = "hash:.*";
+const key = 'hash:.*';
 const hash = execSync('git rev-parse HEAD').toString().trim();
 
 let data = fs.readFileSync('./lib/mapp.mjs', 'utf-8');
