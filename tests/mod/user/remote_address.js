18176132d27b038ac5add2184ee7f830514e5cf9
Robert Hurst <robert.hurst@geolytix.co.uk>
2023-06-20 07:37:56 +0200

    Remote Address function comment

--- tests/mod/user/remote_address.js
+++ tests/mod/user/remote_address.js
@@ -1,3 +1,6 @@
+//This function is to replicate the remote address regex expression on line 87 of mod/user/login.js 
+// & line 37 of mod/user/register.js
+
 function extractRemoteAddress(req) {
     return /^[A-Za-z0-9.,_-\s]*$/.test(req.headers['x-forwarded-for']) 
       ? req.headers['x-forwarded-for'] 
