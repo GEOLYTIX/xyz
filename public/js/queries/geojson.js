dee42b95090c3c4526651384d69f2965d3020916
Rob Hurst <43169158+RobAndrewHurst@users.noreply.github.com>
2023-07-24 12:26:53 +0200

    Geojson properties correction

--- public/js/queries/geojson.js
+++ public/js/queries/geojson.js
@@ -2,17 +2,15 @@ module.exports = _ => {
 
     const layer = _.workspace.locales[_.locale].layers[_.layer]
 
-    let propertiesString = '';
-    
+    let properties = '';
+
     if (Array.isArray(layer.properties)) {
         const propertyKeyValuePairs = layer.properties.map(field => {
             const value = _.workspace.templates[field]?.template || field;
             return `'${field}',${value}`;
         });
-        propertiesString = ', json_build_object(' + propertyKeyValuePairs.join(', ') + ') as properties';
+        properties = ', json_build_object(' + propertyKeyValuePairs.join(', ') + ') as properties';
     }
-
-    const properties = propertiesString;
 
     return `
         SELECT
