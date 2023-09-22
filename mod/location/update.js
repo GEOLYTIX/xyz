b2140df151bb09aadc2a47894148602b78ef7a4c
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2023-09-22 14:26:11 +0100

    Cloudinary methods and array update.

--- mod/location/update.js
+++ mod/location/update.js
@@ -6,14 +6,42 @@ module.exports = async (req, res) => {
 
   const fields = Object.entries(req.body)
     .map(entry => {
-      if (entry[1] === null) return ` ${entry[0]} = null`
+
+      if (entry[1] === null) {
+
+        // Value is null
+        return ` ${entry[0]} = null`
+      }
+
       if (typeof entry[1] === 'string') {
 
+        // Value is string. Escape quotes.
         return ` ${entry[0]} = '${entry[1].replace(/\'/gi, `''`)}'`
       }
-      if (entry[1].coordinates) return ` ${entry[0]} = ST_SetSRID(ST_MakeValid(ST_GeomFromGeoJSON('${JSON.stringify(entry[1])}')),${layer.srid})`
-      if (typeof entry[1] === 'object') return ` ${entry[0]} = '${JSON.stringify(entry[1])}'`
-      if (typeof entry[1] === 'boolean' || typeof entry[1] === 'number') return ` ${entry[0]} = ${entry[1]}`
+
+      if (entry[1].coordinates) {
+        
+        // Value is geometry.
+        return ` ${entry[0]} = ST_SetSRID(ST_MakeValid(ST_GeomFromGeoJSON('${JSON.stringify(entry[1])}')),${layer.srid})`
+      }
+
+      if (Array.isArray(entry[1])) {
+
+        // Value is an array (of strings)
+        return ` ${entry[0]} = ARRAY['${entry[1].join("','")}']`
+      }
+
+      if (typeof entry[1] === 'object') {
+       
+        // Value is an object and must be stringified.
+        return ` ${entry[0]} = '${JSON.stringify(entry[1])}'`
+      }
+
+      if (typeof entry[1] === 'boolean' || typeof entry[1] === 'number') {
+
+        // Value is boolean or number.
+        return ` ${entry[0]} = ${entry[1]}`
+      }
     })
 
   // Validate dynamic method call.
