e5ace68e60900f5dd8cf4b1a4aaff26c73b10cd0
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2023-10-20 18:02:07 +0100

    remove assignDefaults

--- mod/workspace/cache.js
+++ mod/workspace/cache.js
@@ -13,7 +13,7 @@ const getFrom = {
   'mongodb': ref => mongodb(ref.split(/:(.*)/s)[1])
 }
 
-const assignDefaults = require('./assignDefaults')
+const merge = require('../utils/merge')
 
 process.env.WORKSPACE_AGE ??= 3600000
 
@@ -111,7 +111,40 @@ async function cache() {
     ...workspace.templates
   }
 
-  await assignDefaults(workspace)
+  workspace.locale ??= {
+    layers: {}
+  }
+
+  workspace.locales ??= {
+    locale: workspace.locale
+  }
+
+  // Loop through locale keys in workspace.
+  Object.keys(workspace.locales).forEach(locale_key => {
+
+    // Get locale object from key.
+    const locale = workspace.locales[locale_key]
+
+    // A default locale has been defined in the workspace.
+    if (typeof workspace.locale === 'object') {
+
+      // Merge the workspace template into workspace.
+      merge(locale, workspace.locale)
+    }
+
+    // A template exists for the locale key.
+    if (Object.hasOwn(workspace.templates, locale_key) && typeof workspace.templates[locale_key] === 'object') {
+
+      // Merge the workspace template into workspace.
+      merge(locale, workspace.templates[locale_key])
+    }
+
+    // Assign key value as key on locale object.
+    locale.key = locale_key
+
+    // Assign locale key as name with no existing name on locale object.
+    locale.name = locale.name || locale_key
+  })
 
   if (workspace.plugins) {
 
