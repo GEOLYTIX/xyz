f812b073d49eede1fce835ea5152dd666211aee7
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2023-12-12 12:30:51 +0000

    Update location_get query

--- mod/workspace/templates/location_get.js
+++ mod/workspace/templates/location_get.js
@@ -1,11 +1,15 @@
 module.exports = _ => {
 
   const fields = _.layer.infoj
-    .filter(entry => !_.fields || _.fields?.split(',').includes(entry.field))
-    .filter(entry => !entry.query)
-    .filter(entry => entry.type !== 'key')
+
+    // Entry must have a field defined.
     .filter(entry => entry.field)
-    .map(entry => `(${entry.fieldfx || entry.field}) AS ${entry.field}`)
+
+    // Entry must NOT have a query defined.
+    .filter(entry => !entry.query)
+
+    // Map either fieldfx or template SQL if available.
+    .map(entry => `(${entry.fieldfx || _.workspace.templates[entry.field]?.template || entry.field}) AS ${entry.field}`)
 
   return `
     SELECT ${fields.join()}
