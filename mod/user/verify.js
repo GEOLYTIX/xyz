1bdb5581fe9bbae985b93eb0a478eccd9b45acdf
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-03-06 11:57:58 +0200

    Verify docs

--- mod/user/verify.js
+++ mod/user/verify.js
@@ -1,4 +1,7 @@
 /**
+ * ### verify
+
+This module exports a function that gets triggers from an endpoint to verify and reset a user's password email and password.
 @module /user/verify
 */
 
@@ -10,6 +13,12 @@ const languageTemplates = require('../utils/languageTemplates')
 
 const login = require('./login')
 
+/**
+ * @function verify
+ * @param {Object} req 
+ * @param {Object} res 
+ * @returns {Obect} res
+ */
 module.exports = async (req, res) => {
 
   if (!acl) return res.status(500).send('ACL unavailable.')
