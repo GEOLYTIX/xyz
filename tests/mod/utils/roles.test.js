7eb98288ffd9432eaffcb1c39bec3353dd8c4680
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2024-03-18 11:21:29 +0000

    update obj merge test

--- tests/mod/utils/roles.test.js
+++ tests/mod/utils/roles.test.js
@@ -1,36 +1,41 @@
 const {
-    check,
-    filter,
-    get
+  check,
+  get,
+  objMerge
 } = require('../../../mod/utils/roles');
 
 describe('Testing module functions', () => {
 
-    describe('Testing check function', () => {
-      it('should return the object when all roles are negated', () => {
-        const obj = {roles: {'!admin': true}};
-        const roles = ['user'];
-        expect(check(obj, roles)).toEqual(obj);
-      });
-       
+  describe('Testing check function', () => {
+    it('should return the object when all roles are negated', () => {
+      const obj = { roles: { '!admin': true } };
+      const roles = ['user'];
+      expect(check(obj, roles)).toEqual(obj);
     });
-  
-    describe('Testing filter function', () => {
-      it('should return filter objects for user_roles matched with layer.roles', () => {
-        const layer = {roles: {'user': { filter: 'test' }}};
-        const user_roles = ['user'];
-        expect(filter(layer, user_roles)).toEqual({});
-      });
-  
-       
-    });
-  
-    describe('Testing get function', () => {
-      it('should return array of roles from the object', () => {
-        const obj = {roles: {'user': true}, test: {roles: {'admin': true}}};
-        expect(get(obj)).toEqual(['user', 'admin']);
-      });
-  
-    });
-  
+
   });
+
+  describe('Testing get function', () => {
+    it('should return array of roles from the object', () => {
+      const obj = { roles: { 'user': true }, test: { roles: { 'admin': true } } };
+      expect(get(obj)).toEqual(['user', 'admin']);
+    });
+
+  });
+
+  describe('Testing objMerge function', () => {
+    it('should return object with merged object roles', () => {
+      const obj = {
+        roles: {
+          test: {
+            foo: 'bar'
+          }
+        }
+      };
+      const roles = ['test']
+      expect(objMerge(obj, roles)).toEqual({ foo: 'bar' });
+    });
+
+  });
+
+});
