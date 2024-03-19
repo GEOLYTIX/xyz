2d79c89492c8b5ed2fee7b77f4663e1978678b9b
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2024-03-19 11:51:27 +0000

    remove roles merge test

--- tests/mod/utils/roles.test.js
+++ tests/mod/utils/roles.test.js
@@ -1,7 +1,6 @@
 const {
   check,
-  get,
-  objMerge
+  get
 } = require('../../../mod/utils/roles');
 
 describe('Testing module functions', () => {
@@ -19,21 +18,6 @@ describe('Testing module functions', () => {
     it('should return array of roles from the object', () => {
       const obj = { roles: { 'user': true }, test: { roles: { 'admin': true } } };
       expect(get(obj)).toEqual(['user', 'admin']);
-    });
-
-  });
-
-  describe('Testing objMerge function', () => {
-    it('should return object with merged object roles', () => {
-      const obj = {
-        roles: {
-          test: {
-            foo: 'bar'
-          }
-        }
-      };
-      const roles = ['test']
-      expect(objMerge(obj, roles)).toEqual({ foo: 'bar' });
     });
 
   });
