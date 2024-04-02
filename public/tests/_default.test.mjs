3fbf96db788e35e050ae682ae26e18e7b7f4d2be
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-04-02 11:09:50 +0200

    Update codi version

--- public/tests/_default.test.mjs
+++ public/tests/_default.test.mjs
@@ -1,4 +1,4 @@
-import { describe, it, assertEqual, assertNotEqual, assertTrue, assertFalse, assertThrows } from 'https://esm.sh/codi-test-framework@0.0.12';
+import { describe, it, assertEqual, assertNotEqual, assertTrue, assertFalse, assertThrows } from 'https://esm.sh/codi-test-framework@0.0.14';
 import { layerTest } from './layer.test.mjs';
 
 describe('Mapview test', async () => {
