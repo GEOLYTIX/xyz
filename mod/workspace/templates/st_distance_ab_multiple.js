1f1b8c677f66aed0f8d56b043b4bcc7af6a643e7
Simon Leech <56163132+simon-leech@users.noreply.github.com>
2024-04-24 10:49:39 +0100

    Add new multiple query

---
+++ mod/workspace/templates/st_distance_ab_multiple.js
@@ -0,0 +1,17 @@
+module.exports = `
+SELECT 
+    closest.field_b AS \${as},
+    ST_DISTANCE(st_transform(a.\${geom},4326)::geography, closest.geom ) AS dist
+FROM 
+    \${table} a
+LEFT JOIN LATERAL (
+    SELECT
+        b.\${field_b} as field_b,
+        st_transform(b.\${geom_b},4326)::geography as geom
+    FROM \${table_b} b 
+    ORDER BY st_transform(b.\${geom_b},4326)::geography <-> st_transform(a.\${geom},4326)::geography
+    LIMIT \${limit}
+) closest
+on true
+WHERE a.\${qID} = %{id}
+`
