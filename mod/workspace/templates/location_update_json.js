b8ae8a96a411ca1782650139cf838ed05955cbac
AlexanderGeere <alex.geere@geolytix.co.uk>
2024-05-16 09:51:20 +0200

    changing to jsonb_set

--- mod/workspace/templates/location_update_json.js
+++ mod/workspace/templates/location_update_json.js
@@ -8,15 +8,15 @@ module.exports = _ => {
 
     Object.keys(body).forEach( key => {
         if(typeof body[key] !== 'string'){
-            values.push(`"${key}":${body[key]}`)
+            values.push(body[key])
         }
         else{
-            values.push(`"${key}":"${body[key]}"`)
+            values.push(`"${body[key]}"`)
         }
     })
 
     return `
             UPDATE ${_.table}
-            SET ${field} = (${field}::jsonb || '{${values.join(',')}}'::jsonb)::json
+            SET ${field} = jsonb_set(${field}::jsonb ,'{${Object.keys(body)}}'::text[],[${values.join(',')}]::jsonb)::json
             WHERE ${_.layer.qID} = %{id};`
 }
