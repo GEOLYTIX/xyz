140c7ae9494c5d8d66a2d2143ec621843723af63
Simon Leech <56163132+simon-leech@users.noreply.github.com>
2024-06-03 15:36:30 +0100

    _.filter needs to be checked to be a string before being applied

--- mod/workspace/templates/locations_delete.js
+++ mod/workspace/templates/locations_delete.js
@@ -5,6 +5,18 @@ module.exports = _ => {
     throw new Error(`You cannot delete locations data without providing the layer parameter.`)
   }
 
+  let where;
+  
+  // If filter is not empty, set the where clause
+  if (_.filter.length > 1) {
+    // If the filter begins ' AND ' remove it
+    if (_.filter.startsWith(' AND ')) {
+      _.filter = _.filter.slice(5)
+    }
+    // Set the where clause
+    where = `WHERE ${_.filter}`
+  } 
+
   // Delete from the table where data matches the filter
-  return `DELETE FROM ${_.table} WHERE ${filter};`
+  return `DELETE FROM ${_.table} ${where};`
 }
