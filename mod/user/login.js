8be8949e68d9b467b35bc1a9a73d55fc00b4d3ef
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2024-06-12 17:20:16 +0100

    redirect documentation

--- mod/user/login.js
+++ mod/user/login.js
@@ -27,9 +27,7 @@ The login method will request the fromACL() method for a user with login details
 
 A user cookie will be assigned for the user returned from the fromACL() method and the response will be redirected to the intended target location.
 
-Any existing user cookie will be removed from the response header if no post request body is provided or if the fromACL() method failed to get a validated user object.
-
-The default `login_view` is set a template request parameter before the XYZ View API method is returned.
+The loginView method will be returned with a message from a failed user validation or if no login post request body is provided.
 
 @param {Object} req HTTP request.
 @param {Object} req.params HTTP request parameter.
@@ -87,8 +85,35 @@ module.exports = async function login(req, res) {
     return;
   }
 
+  return loginView(req, res)
+}
+
+
+/**
+@function loginView
+
+@description
+Any existing user cookie for the XYZ instance will be removed [set to null].
+
+A redirect cookie will be set to the response header for a redirect to the location after sucessful login.
+
+The default `login_view` will be set as template request parameter before the XYZ View API method will be returned.
+
+@param {Object} req HTTP request.
+@param {Object} req.params HTTP request parameter.
+@param {Object} res HTTP response.
+*/
+
+function loginView(req, res) {
+
   // Clear user token cookie.
   res.setHeader('Set-Cookie', `${process.env.TITLE}=null;HttpOnly;Max-Age=0;Path=${process.env.DIR || '/'}`)
+
+  // The redirect for a successful login.
+  const redirect = req.url && decodeURIComponent(req.url).replace(/login\=true/, '')
+
+  // Set cookie with redirect value.
+  res.setHeader('Set-Cookie', `${process.env.TITLE}_redirect=${redirect};HttpOnly;Max-Age=60000;Path=${process.env.DIR || '/'}`)
 
   req.params.template = 'login_view'
 
