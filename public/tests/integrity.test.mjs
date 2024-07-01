1d6558224c4ceee9aebb328c64927d3cf964fab6
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-07-01 12:23:06 +0200

    Move tests

--- public/tests/integrity.test.mjs
+++ public/tests/integrity.test.mjs
@@ -1,7 +1,5 @@
 import { base } from './_base.test.mjs';
 import { layerTest } from './layer.test.mjs';
-import { uiElementsTest } from './lib/ui/_ui.test.mjs';
 
 const mapview = await base();
 await layerTest(mapview);
-await uiElementsTest(mapview);
