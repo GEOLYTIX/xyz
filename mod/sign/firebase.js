10115cc6ad882c878172afcb91642f0db78f8645
AlexanderGeere <alex.geere@geolytix.co.uk>
2024-07-03 16:08:02 +0200

    Warnings and writing to the store

--- mod/sign/firebase.js
+++ mod/sign/firebase.js
@@ -3,6 +3,23 @@
 */
 
 async function sign_firebase(req){
+
+    let firebase_keys = 'FIREBASE_AUTH_URL,FIREBASE_API_KEY,TRANSPORT_EMAIL,FIREBASE_PASSWORD,FIREBASE_DB_URL'
+
+    Object.keys(process.env).forEach( 
+        element => {
+            if(firebase_keys.includes(element)){
+                firebase_keys = firebase_keys.replace(`,${element}`,'')
+                firebase_keys = firebase_keys.replace(`${element},`,'')
+                firebase_keys = firebase_keys.replace(element,'')
+            }
+        }
+    )
+
+    if(firebase_keys.length > 4){
+        return {signedUrl: null, missing_keys: firebase_keys.split(',')}
+    }
+
     let auth_url = `${process.env.FIREBASE_AUTH_URL}${process.env.FIREBASE_API_KEY}`
     let email = process.env.TRANSPORT_EMAIL
     let password = process.env.FIREBASE_PASSWORD
