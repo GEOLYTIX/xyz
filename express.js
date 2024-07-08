ed48123cfffc41b2b80c6c9fe680135e8536d77c
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2024-07-08 11:26:51 +0100

    use both body parser

--- express.js
+++ express.js
@@ -46,7 +46,7 @@ app.get(`${process.env.DIR||''}/api/workspace/:key?`, api)
 app.get(`${process.env.DIR||''}/api/user/:method?`, api)
 
 // this route allows to update user data with payload but is in conflict with the one below
-app.post(`${process.env.DIR||''}/api/user/:method?`, express.json({limit: '5mb'}), api)
+app.post(`${process.env.DIR||''}/api/user/:method?`, [express.urlencoded({extended: true}), express.json({limit: '5mb'})], api)
 
 //sudo ./caddy_linux_amd64 reverse-proxy --from localhost:443 --to localhost:3000
 
