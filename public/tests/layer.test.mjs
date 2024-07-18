082fc8222535cf91b64ce0a3a9ef9f57f666cd7b
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-07-18 19:20:31 +0200

    Remove unwated directories from cli tests

--- public/tests/layer.test.mjs
+++ public/tests/layer.test.mjs
@@ -1,4 +1,4 @@
-import { describe, it, assertEqual, assertNotEqual, assertTrue, assertFalse, assertThrows } from 'https://esm.sh/codi-test-framework@0.0.26';
+import { describe, it, assertEqual, assertNotEqual, assertTrue, assertFalse, assertThrows } from 'https://esm.sh/codi-test-framework@0.0.28';
 
 export async function layerTest(mapview) {
 
