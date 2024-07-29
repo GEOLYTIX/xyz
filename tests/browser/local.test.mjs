dc158551dadad6e7bacaaff01d78f779bb99c60f
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-07-29 17:29:29 +0200

    Update tests

--- tests/browser/local.test.mjs
+++ tests/browser/local.test.mjs
@@ -4,39 +4,41 @@ import { layerTest } from '../lib/layer/_layer.test.mjs';
 import { locationTest } from '../lib/location/_location.test.mjs';
 import { mapviewTest } from '../lib/mapview/_mapview.test.mjs';
 import { setView } from '../utils/view.js';
+import { delayFunction } from '../utils/delay.js';
 
 // import { booleanTest } from '../lib/ui/locations/entries/boolean.test.mjs';
 
 const mapview = await base();
 //await baseDictionaryTest();
-setView(mapview, 2.56, 'default');
+setView(mapview, 2, 'default');
 await layerTest.changeEndTest(mapview);
 
-setView(mapview, 2.56, 'default');
+setView(mapview, 2, 'default');
 await layerTest.decorateTest(mapview);
 
-await layerTest.fadeTest();
-await layerTest.featureFieldsTest();
-await layerTest.featureFilterTest();
-await layerTest.featureFormatsTest();
-await layerTest.featureHoverTest();
-await layerTest.featureStyleTest();
-await layerTest.styleParserTest();
+setView(mapview, 2, 'default');
+// await layerTest.fadeTest();
+// await layerTest.featureFieldsTest();
+// await layerTest.featureFilterTest();
+// await layerTest.featureFormatsTest();
+// await layerTest.featureHoverTest();
+// await layerTest.featureStyleTest();
+// await layerTest.styleParserTest();
 
-await locationTest.createTest();
-await locationTest.getTest();
-await locationTest.decorateTest();
-await locationTest.nnearestTest();
+// await locationTest.createTest();
+// await locationTest.getTest();
+// await locationTest.decorateTest();
+// await locationTest.nnearestTest();
 
-await mapviewTest.addLayerTest();
-await mapviewTest.allfeaturesTest();
-await mapviewTest.attributionTest();
-await mapviewTest.fitViewTest();
-await mapviewTest.geoJSONTest();
-await mapviewTest.geometryTest();
-await mapviewTest.getBoundsTest();
-await mapviewTest.infotipTest();
-await mapviewTest.locateTest();
-await mapviewTest.popupTest();
+// await mapviewTest.addLayerTest();
+// await mapviewTest.allfeaturesTest();
+// await mapviewTest.attributionTest();
+// await mapviewTest.fitViewTest();
+// await mapviewTest.geoJSONTest();
+// await mapviewTest.geometryTest();
+// await mapviewTest.getBoundsTest();
+// await mapviewTest.infotipTest();
+// await mapviewTest.locateTest();
+// await mapviewTest.popupTest();
 // await booleanTest();
 
--- tests/lib/layer/decorate.test.mjs
+++ tests/lib/layer/decorate.test.mjs
@@ -1,6 +1,9 @@
-import { it, describe, assertTrue } from 'https://esm.sh/codi-test-framework@0.0.29';
+import { it, describe, assertEqual, assertTrue, assertFalse } from 'https://esm.sh/codi-test-framework@0.0.29';
+import { setView } from '../../utils/view.js';
+
 export async function decorateTest(mapview) {
     await describe('Layer: decorateTest', async () => {
+        const decorated_layer = mapview.layers['decorate'];
         const workspace = await mapp.utils.xhr(`/test/tests/workspace.json`);
         const decorate_layer = workspace.locale.layers['decorate'];
         decorate_layer.mapview = mapview;
@@ -11,15 +14,21 @@ export async function decorateTest(mapview) {
         });
 
         await it('Should have a show function', async () => {
-            assertTrue(!!layer.show, 'The layer should have a show method by default')
+            assertTrue(!!layer.show, 'The layer should have a show method by default');
+
+            decorated_layer.show();
+            assertTrue(decorated_layer.display, 'The layer should display as true');
         });
 
         await it('Should have a hide function', async () => {
             assertTrue(!!layer.hide, 'The layer should have a hide method by default')
+
+            decorated_layer.hide();
+            assertFalse(decorated_layer.display, 'The layer should display as false');
         });
 
-        await it('Should not have an edit function', async () => {
-            assertTrue(!layer.edit, 'The layer should have a hide method by default')
+        await it('Should not have an edit object', async () => {
+            assertTrue(!layer.edit, 'The layer should not have an edit object as its been deprecated.')
         });
 
         await it('Should add the skipEntry flag to the textArea entry', async () => {
@@ -28,6 +37,21 @@ export async function decorateTest(mapview) {
             });
         });
 
-        mapview.layers['decorate'].show();
+        setView(mapview, 11, 'london');
+
+        await it('Should get test.public tabel at zoom level 11', async () => {
+            const table = decorated_layer.tableCurrent();
+            assertEqual(table, 'test.scratch')
+        });
+
+        await it('Should get geom_3857 geom at zoom level 11', async () => {
+            const geom = decorated_layer.geomCurrent();
+            assertEqual(geom, 'geom_3857', 'The Geometry at zoom level 11 should be geom_3857')
+        });
+
+        await it('Should be able to zoomToExtent', async () => {
+            const success = await decorated_layer.zoomToExtent();
+            assertTrue(success)
+        });
     });
 }
