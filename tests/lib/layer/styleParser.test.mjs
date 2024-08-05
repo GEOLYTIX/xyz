42678a1213abead10e9546ed951ce663cf06d873
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-08-05 16:29:35 +0200

    Update Style Parse test

--- tests/lib/layer/styleParser.test.mjs
+++ tests/lib/layer/styleParser.test.mjs
@@ -3,10 +3,23 @@ export async function styleParserTest(mapview) {
   await describe('TODO: Layer: styleParserTest', async () => {
     const workspace = await mapp.utils.xhr(`/test/tests/workspace.json`);
     const layer = workspace.locale.layers['styleParser'];
+    layer.key = 'Style Parser Test';
     await mapp.layer.styleParser(layer);
 
     await it('The Layer should have a default object', () => {
       assertTrue(!!layer.style.default, 'The Layer should always have a default object')
     });
+
+    await it('The parser should remove the hover object, and add it into the layer.style object', () => {
+      assertTrue(!layer.hover, 'The layer shouldnt have a layer.hover object')
+      assertTrue(!!layer.style.hover, 'The layer should have a layer.style.hover object')
+    });
+
+    await it('The parse should remove the label property if both labels and label are present', () => {
+      assertTrue(!layer.style.label, 'The layer shouldnt have a layer.style.label object')
+      assertTrue(!!layer.style.labels, 'The layer should have a layer.style.labels object')
+    });
+
+    console.warn('The Style parse modules need to be broken up and exported for easier testability');
   });
 }
