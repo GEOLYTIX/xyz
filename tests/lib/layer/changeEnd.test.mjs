b59c92be31707ed709405cfb1e0130aa05572f44
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-08-06 15:40:32 +0200

    Update tests docs

--- tests/lib/layer/changeEnd.test.mjs
+++ tests/lib/layer/changeEnd.test.mjs
@@ -8,13 +8,21 @@ import { setView } from '../../utils/view.js';
 import { delayFunction } from '../../utils/delay.js';
 
 /**
+ * This function is used as an entry point for the changeEndTest
  * @function changeEndTest 
  * @param {Object} mapview 
 */
 export async function changeEndTest(mapview) {
 
     await describe('Layer: changeEndTest', async () => {
-        await it('After dispatching the event the layer should not display.', async () => {
+
+        /**
+         * ### should dispatch the event and the layer should not display
+         * 1. We create the `changeEnd` event and dispatch it.
+         * 2. We assert that the `layer.display` is not set to true.
+         * @function it
+         */
+        await it('should dispatch the event and the layer should not display.', async () => {
             const layer = mapview.layers['changeEnd'];
             const changeEndEvent = new Event('changeEnd');
             const target = layer.mapview.Map.getTargetElement();
@@ -26,7 +34,14 @@ export async function changeEndTest(mapview) {
             await delayFunction(1000);
         });
 
-        await it('The layer should display at zoom level 6', async () => {
+        /**
+         * ### should display at zoom level 6.
+         * 1. The Test sets the mapview to London at zoom level 11.
+         * 2. Creates the `changeEnd` event and dispatches it.
+         * 3. And asserts that the `layer.display` is true.
+         * @function it
+         */
+        await it('should display at zoom level 6', async () => {
             setView(mapview, 11, 'london')
             const layer = mapview.layers['changeEnd'];
             const changeEndEvent = new Event('changeEnd');
