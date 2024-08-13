91e93b0fdfae967973dbe4fcd8cee8148b2bed84
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-08-13 16:36:20 +0200

    Update tests for Workspace API's

--- tests/mod/workspace/_workspace.test.mjs
+++ tests/mod/workspace/_workspace.test.mjs
@@ -5,6 +5,7 @@
 
 /**
  * This function is used as an entry point for the changeEndTest
+ * This function is also in a function as to not execute in the CLI environment and only execute in a browser.
  * @function workspaceTest 
  * @param {Object} mapview 
 */
@@ -24,6 +25,17 @@ export async function workspaceTest() {
             codi.assertTrue(!!locale.name, 'The locale should have a name');
             codi.assertTrue(!!locale.plugins, 'The locale should have plugins');
             codi.assertTrue(!!locale.syncPlugins, 'The locale should have syncPlugins');
+        });
+
+        await codi.it('Workspace: Getting Layer', async () => {
+            const layer = await mapp.utils.xhr(`/test/api/workspace/layer?layer=fade`);
+            codi.assertEqual(layer.key, 'fade', 'Ensure that we get the fade layer from the API')
+        });
+
+        await codi.it('Workspace: Getting Roles', async () => {
+            const roles = await mapp.utils.xhr(`/test/api/workspace/roles`);
+            const expected_roles = ['test', 'super_test']
+            codi.assertEqual(roles, expected_roles, 'Ensure that we get the correct roles from the API')
         });
     });
 }
