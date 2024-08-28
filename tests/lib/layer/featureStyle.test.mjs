a04eadea3f434a4c6ebe752b77c7e4b1692f75d3
Simon Leech <56163132+simon-leech@users.noreply.github.com>
2024-08-28 09:48:11 +0100

    Remove featureStyle test as featureFormats now used

--- tests/lib/layer/featureStyle.test.mjs
+++ tests/lib/layer/featureStyle.test.mjs
@@ -10,48 +10,5 @@
 */
 export async function featureStyleTest(mapview) {
     await codi.describe('TODO: Layer: featureStyleTest', async () => {
-
-        /**
-         * ### The max value should be set after we update a location.
-         * 1. Reset the datasets's icon_scaling field to null
-         * 2. Get the layer from the mapview
-         * 3. Get an id to perform a select on.
-         * 4. Get the location.
-         * 5. Set the newValue of the icon_scale field 
-         * 6. Update the location.
-         * 7. Assert that we have a new max value
-         * 8. Reset the icon_scale field to null
-         * @function it
-        */
-        await codi.it('featureStyle: Icon Scaling', async () => {
-            //Setting the dataset's icon_scaling value to null
-            await mapp.utils.xhr(`/test/api/query?template=icon_scaling_scratch&value=null`);
-            //Getting the layer from the mapview
-            let layer = mapview.layers['icon_scaling'];
-            //Get a location id to perform a get with
-            const lastLocation = await mapp.utils.xhr(`${mapp.host}/api/query?template=get_last_location&locale=locale&layer=icon_scaling`);
-
-            //Get location
-            const location = await mapp.location.get({
-                layer: layer,
-                id: lastLocation.id,
-            });
-
-            //Set the icon_scale newValue to 300
-            location.infoj = location.infoj.map(entry => {
-                if (entry.field === 'icon_scale') {
-                    entry.newValue = 300;
-                }
-                return entry;
-            });
-
-            //Update the location (This should retrigger the process)
-            await location.update();
-            console.log(mapview);
-            //WE NEED AN ASSERTION HERE
-            // codi.assertEqual(CONDITION, EXPECT, `We are expecting the condition to equal the expect`);
-
-            await mapp.utils.xhr(`/test/api/query?template=icon_scaling_scratch&value=null`);
-        });
     });
 }
