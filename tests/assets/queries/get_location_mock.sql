8741b55db11a180277fcd7e5ad0eb38d49aa6e9a
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-09-03 07:01:47 +0200

    Location mock

---
+++ tests/assets/queries/get_location_mock.sql
@@ -0,0 +1,7 @@
+select
+  7 as "id",
+  null as "icon_scale",
+  ARRAY[-15322.318564101091, 6710796.777772117] as "pin",
+  'asdasd' as "textarea",
+  null as "char_field",
+  4 as "integer_field";
--- tests/lib/location/get.test.mjs
+++ tests/lib/location/get.test.mjs
@@ -1,8 +1,27 @@
-export async function getTest() {
-    codi.describe('TODO: Location: getTest', () => {
-        codi.it('Should should test for something', () => {
-            //TODO
+/**
+ * This test is used to test the mapp.location.get function
+ * @function getTest 
+ * @param {object} mapview 
+ */
+export async function getTest(mapview) {
+    await codi.describe('TODO: Location: getTest', async () => {
+
+        /**
+         * This tests the functionality to mock a location by passing in a template that returns values from the query
+         * @function it
+         */
+        await codi.it('We should be able to mock a location get.', async () => {
+            const locationLayer = mapview.layers['location_get_test'];
+            //Get the location with the id returned from the query above
+            const location = await mapp.location.get({
+                layer: locationLayer,
+                getTemplate: 'get_location_mock',
+                id: 7,
+            });
+
+            codi.assertEqual(location.infoj.length, 3, 'We expect to see three infoj entries');
+            codi.assertTrue(!!location.record, 'The location needs a record object');
+            codi.assertTrue(!!location.remove, 'The location needs a remove function');
         });
     });
 }
-
