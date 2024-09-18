1471c06230f864d6842259d14ed07270f925733d
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-09-18 17:22:54 +0100

    Update filters test and generateMinMax function to return filter

--- tests/lib/ui/layers/filters.test.mjs
+++ tests/lib/ui/layers/filters.test.mjs
@@ -1,17 +1,18 @@
 export async function filtersTest(mapview) {
-    codi.describe('UI Layers: Filters test', async () => {
-        codi.it('Should generate correct min/max values', async () => {
+    await codi.describe('UI Layers: Filters test', async () => {
+        await codi.it('Should generate correct min/max values', async () => {
 
             const filter = {
-                field: 'field'
+                field: 'field',
+                minmax_query: 'minmax_query_mock'
             };
 
             const layer = mapview.layers['location_get_test'];
 
-            console.log(layer);
+            const minMax = await mapp.ui.layers.filters.generateMinMax(layer, filter);
 
-            const minMax = mapp.ui.layers.filters.generateMinMax(layer, filter);
-            console.log(minMax)
+            codi.assertEqual(minMax.min, 100, 'The min should return 100');
+            codi.assertEqual(minMax.max, 500, 'The max should return 500');
         });
     });
 }
