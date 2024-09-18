69d8f5ed5fc3b4b185b526fde85c1e1f4e505c8f
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-09-18 11:41:54 +0100

    test docs

--- tests/lib/mapview/addLayer.test.mjs
+++ tests/lib/mapview/addLayer.test.mjs
@@ -1,10 +1,26 @@
+
+/**
+* This is the add layer test module for the mapview. 
+ * @module lib/mapview/addLayer
+*/
+
+/**
+ * The entry point test that is used to test the add layer module for a mapview. 
+ * @param {Object} mapview 
+ * @function addLayerTest
+ */
 export async function addLayerTest(mapview) {
   await codi.describe('Mapview: addLayerTest', async () => {
+
+    /**
+     * This test is used to check if we can add a single layer to the mapview.
+     * @function it
+     */
     await codi.it('Add single layer to mapview.', async () => {
 
       const layer = {
-        "format": "tiles",
-        "URI": "https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        'format': 'tiles',
+        'URI': 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       }
 
       const layers = await mapview.addLayer(layer)
@@ -14,11 +30,15 @@ export async function addLayerTest(mapview) {
       codi.assertTrue(Object.hasOwn(mapview.layers, layers[0].key), 'The layer has been added to the mapview.');
     });
 
+    /**
+   * This test is used to check if we can add multiple layers to the mapview.
+   * @function it
+   */
     await codi.it('Add multiple layer to mapview.', async () => {
 
       const layer = {
-        "format": "tiles",
-        "URI": "https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        'format': 'tiles',
+        'URI': 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       }
 
       const layers = await mapview.addLayer([layer, layer])
