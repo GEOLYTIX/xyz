efbe8a5aab2d416b05651a9b88d0d69915ed7d4f
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-09-18 17:14:33 +0100

    Filter test

--- tests/lib/ui/layers/filters.test.mjs
+++ tests/lib/ui/layers/filters.test.mjs
@@ -1,8 +1,16 @@
-export async function filtersTest() {
-    codi.desribe('UI Layers: Filters test', async () => {
+export async function filtersTest(mapview) {
+    codi.describe('UI Layers: Filters test', async () => {
         codi.it('Should generate correct min/max values', async () => {
 
-            const minMax = mapp.ui.layers.filters.generateMinMax();
+            const filter = {
+                field: 'field'
+            };
+
+            const layer = mapview.layers['location_get_test'];
+
+            console.log(layer);
+
+            const minMax = mapp.ui.layers.filters.generateMinMax(layer, filter);
             console.log(minMax)
         });
     });
