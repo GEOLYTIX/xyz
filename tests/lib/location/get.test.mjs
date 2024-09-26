03bb7d4890afcff9b366c0fb3f97d58b9c39611c
AlexanderGeere <alex.geere@geolytix.co.uk>
2024-09-26 11:08:56 +0200

    Test for no infoj

--- tests/lib/location/get.test.mjs
+++ tests/lib/location/get.test.mjs
@@ -6,12 +6,13 @@
 export async function getTest(mapview) {
     await codi.describe('Location: getTest', async () => {
 
+        const locationLayer = mapview.layers['location_get_test'];
+
         /**
          * This tests the functionality to mock a location by passing in a template that returns values from the query
          * @function it
          */
         await codi.it('We should be able to mock a location get.', async () => {
-            const locationLayer = mapview.layers['location_get_test'];
 
             //Get the location with the id returned from the query above
             const location = await mapp.location.get({
@@ -32,6 +33,29 @@ export async function getTest(mapview) {
             codi.assertTrue(!location.record.hook, 'The hook should be removed from the location record.');
 
             codi.assertTrue(!location.removeCallbacks, 'removeCallbacks should have removed themselves.');
+        });
+
+        /**
+         * This tests that no location is returned if no infoj is provided
+         * @function it
+         */
+        await codi.it('Location get should not return anything if no inoj is present', async () => {
+
+            //Keep the infoj and remove it for the test
+            const infoj = {'infoj': locationLayer.infoj}
+            delete locationLayer.infoj
+
+            //Get the location with the id returned from the query above
+            const location = await mapp.location.get({
+                layer: locationLayer,
+                getTemplate: 'get_location_mock',
+                id: 6,
+            });
+
+            codi.assertEqual(location, undefined, 'We expect to see no infoj');
+
+            //Restore the infoj for subsequent tests
+            Object.assign(locationLayer, infoj)
         });
     });
 }
