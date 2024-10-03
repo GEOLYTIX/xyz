6ee2b1a818ab3e32af43203b9f96a3654d2b8e11
AlexanderGeere <alex.geere@geolytix.co.uk>
2024-10-03 15:13:52 +0200

    and tests to runner

--- tests/browser/local.test.mjs
+++ tests/browser/local.test.mjs
@@ -58,7 +58,7 @@ await runAllTests(formatTest, mapview);
 await runAllTests(ui_locations, mapview);
 
 //Utils Tests
-await runAllTests(utilsTest);
+await runAllTests(utilsTest, mapview);
 
 /**
  * This function is used to execute all the test functions on the exported test object. 
