337ae0a7c702bf79da353888f446fff1828b789f
AlexanderGeere <alex.geere@geolytix.co.uk>
2024-10-07 16:33:30 +0200

    dialog show method and tests

--- tests/lib/ui/elements/dialog.test.mjs
+++ tests/lib/ui/elements/dialog.test.mjs
@@ -5,24 +5,25 @@
  */
 export async function dialogTest() {
     await codi.describe('UI Elements: dialog/modal', async () => {
+
+        const params = {
+            target: document.getElementById('Map'),
+            closeBtn: true,
+            headerDrag: true,
+            header: 'I am a header',
+            content: 'I am so content',
+            top: '5em',
+            left: '5em',
+            contained: true
+        };
+
         /**
          * We should be able to create a basic dialog with some params
          * @function it
          */
         await codi.it('Should create a basic dialog', async () => {
 
-            const params = {
-                target: document.getElementById('Map'),
-                closeBtn: true,
-                headerDrag: true,
-                header: 'I am a header',
-                content: 'I am so content',
-                top: '5em',
-                left: '5em',
-                contained: true
-            };
-
-            const dialog = mapp.ui.elements.dialog(params);
+            const dialog = mapp.ui.elements.dialog({...params});
 
             /**
              * The dialog should be able to close
@@ -33,6 +34,32 @@ export async function dialogTest() {
                 const dialog_element = document.querySelector('#Map > dialog');
                 codi.assertEqual(dialog_element, null, 'The dialog should be removed from the DOM on close');
             });
+
+            /**
+             * The dialog should be able to be shown again
+             * @function it
+             */
+            await codi.it('Dialog should be able to be shown again', async () => {
+                dialog.show();
+                const dialog_element = document.querySelector('#Map > dialog');
+                codi.assertEqual(dialog_element, dialog.node, 'The dialog should be in the DOM again');
+            });
+
+            dialog.close();
+        });
+
+        /**
+         * Created dialog should not have a show method
+         * @function it
+         */
+        await codi.it('Should create a basic dialog without a show method', async () => {
+
+            params.new = true;
+            const dialog = mapp.ui.elements.dialog({...params});
+            
+            codi.assertEqual(typeof dialog.show, 'undefined', 'The dialog show method should not be available');
+
+            dialog.close()
         });
     });
 }
