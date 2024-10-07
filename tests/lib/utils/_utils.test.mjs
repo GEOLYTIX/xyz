84ccc7349a10626cefa5053dfd271bf14917f813
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-10-07 10:56:42 +0200

    compose utility test

--- tests/lib/utils/_utils.test.mjs
+++ tests/lib/utils/_utils.test.mjs
@@ -1,11 +1,13 @@
 import { numericFormatterTest } from './numericFormatter.test.mjs';
-import {mergeTest} from './merge.test.mjs';
+import { mergeTest } from './merge.test.mjs';
 import { paramStringTest } from './paramString.test.mjs';
 import { queryParamsTest } from './queryParams.test.mjs';
+import { composeTest } from './compose.test.mjs';
 
-export const utilsTest = { 
-    numericFormatterTest,
-    mergeTest,
-    paramStringTest,
-    queryParamsTest
- }
+export const utilsTest = {
+  numericFormatterTest,
+  mergeTest,
+  paramStringTest,
+  queryParamsTest,
+  composeTest
+}
