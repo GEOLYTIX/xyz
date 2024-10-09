7a2aaba117aee4eaf7d454a4fd33a1b187c7b4bb
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-10-09 07:14:11 +0200

    Update tests

--- tests/lib/utils/_utils.test.mjs
+++ tests/lib/utils/_utils.test.mjs
@@ -3,11 +3,13 @@ import { mergeTest } from './merge.test.mjs';
 import { paramStringTest } from './paramString.test.mjs';
 import { queryParamsTest } from './queryParams.test.mjs';
 import { composeTest } from './compose.test.mjs';
+import { svgTemplatesTest } from './svgTemplates.test.mjs';
 
 export const utilsTest = {
     numericFormatterTest,
     mergeTest,
     paramStringTest,
     queryParamsTest,
-    composeTest
+    composeTest,
+    svgTemplatesTest
 }
