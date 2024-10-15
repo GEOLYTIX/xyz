09a583780a9223811efbed3dd42a516b37968211
Simon Leech <56163132+simon-leech@users.noreply.github.com>
2024-10-15 08:35:08 +0100

    Correct Tests to split versions and compare separately

--- tests/lib/utils/versionCheck.mjs
+++ tests/lib/utils/versionCheck.mjs
@@ -10,8 +10,8 @@ export async function versionCheck() {
 
     await codi.describe('Utils: versionCheck Test', async () => {
 
-        await codi.it('should return true if the current version is more than or equal to the given value (passing a number)', async () => {
-            const value = 480;
+        await codi.it('should return true if the major version is more than', async () => {
+            const value = '3.9.0';
 
             // Set mapp.version for the test
             mapp.version = '4.9.1';
@@ -21,15 +21,37 @@ export async function versionCheck() {
             codi.assertEqual(result, true);
         });
 
-        await codi.it('should return false if the current version is less than the given value (passing a string)', async () => {
-            const value = '4.12.0';
+        await codi.it('should return false if the major version is the same and the minor version is less', async () => {
+            const value = '4.10.0';
+
+            // Set mapp.version for the test
+            mapp.version = '4.9.0';
+
+            const result = mapp.utils.versionCheck(value);
+
+            codi.assertEqual(result, false);
+        });
+
+        await codi.it('should return true if the major version is the same and the minor version is more', async () => {
+            const value = '4.10.0';
 
             // Set mapp.version for the test
             mapp.version = '4.11.0';
 
             const result = await mapp.utils.versionCheck(value);
 
-            codi.assertEqual(result, false);
+            codi.assertEqual(result, true);
+        });
+
+        await codi.it('should return true if the major version is the same and the minor version is the same', async () => {
+            const value = '4.11.1';
+
+            // Set mapp.version for the test
+            mapp.version = '4.11.2';
+
+            const result = await mapp.utils.versionCheck(value);
+
+            codi.assertEqual(result, true);
         });
     });
 }
