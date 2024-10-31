c575df58f5e655db3626d313c0f346d3c3499204
Latta <info@latta.ai>
2024-10-31 16:33:58 +0100

    Remove node-fetch reference and add documentation in cloudfront.js

--- mod/provider/cloudfront.js
+++ mod/provider/cloudfront.js
@@ -1,22 +1,23 @@
 /**
-@module /provider/cloudfront
-*/
+ * @module /provider/cloudfront
+ * @description Provides functionality to interact with Amazon CloudFront, including generating signed URLs and fetching content.
+ */
 
 const { readFileSync } = require('fs')
-
 const { join } = require('path')
-
 const { getSignedUrl } = require('@aws-sdk/cloudfront-signer');
-
 const logger = require('../utils/logger')
 
-const nodeFetch = require('node-fetch')
-
+/**
+ * @function
+ * @async
+ * @param {Object} ref - Reference object containing URL and parameters
+ * @returns {Promise<string|Object|Buffer>} - The fetched content
+ * @throws {Error} If the fetch request fails
+ */
 module.exports = async ref => {
-
   try {
-
-    // Subtitutes {*} with process.env.SRC_* key values.
+    // Substitutes {*} with process.env.SRC_* key values.
     const url = (ref.params?.url || ref).replace(/{(?!{)(.*?)}/g,
       matched => process.env[`SRC_${matched.replace(/(^{)|(}$)/g, '')}`])
 
@@ -32,24 +33,21 @@ module.exports = async ref => {
 
     // Return signedURL only from request.
     if (ref.params?.signedURL) {
-
       return signedURL;
     }
 
-    const response = await nodeFetch(signedURL)
+    const response = await fetch(signedURL)
 
-    logger(`${response.status} - ${url}`,'cloudfront')
+    logger(`${response.status} - ${url}`, 'cloudfront')
 
     if (response.status >= 300) return new Error(`${response.status} ${ref}`)
 
     if (url.match(/\.json$/i)) return await response.json()
 
-    if (ref.params?.buffer) return await response.buffer()
+    if (ref.params?.buffer) return await response.arrayBuffer()
 
     return await response.text()
-
   } catch(err) {
     console.error(err)
   }
-
 }
