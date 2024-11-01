2e8672481051b0033438d73ccf777549a8224cd8
Dennis Bauszus <dennis.bauszus@geolytix.co.uk>
2024-11-01 11:06:11 +0000

    layer query and templates (#1588)
    * layer query and templates
    
    * check whether layer template is instance of error
    
    * parse layer param as string
    
    * Parse error message as string on return
    
    * remove string parser
    
    * update layer queries
    
    * add test
    
    ---------
    
    Co-authored-by: Rob Hurst <43169158+RobAndrewHurst@users.noreply.github.com>
    Co-authored-by: Robert Hurst <robert.hurst@geolytix.co.uk>

--- mod/workspace/templates/geojson.js
+++ mod/workspace/templates/geojson.js
@@ -1,23 +1,30 @@
+/**
+### /workspace/templates/geojson
+
+The geojson layer query template returns an array of records including a geojson geometry.
+
+@module /workspace/templates/geojson
+*/
 module.exports = _ => {
 
-    let properties = '';
+  let properties = '';
 
-    if (_.fields) {
-        const propertyKeyValuePairs = _.fields?.split(',').map(field => {
-            const value = _.workspace.templates[field]?.template || field;
-            return `'${field}',${value}`;
-        });
-        properties = ', json_build_object(' + propertyKeyValuePairs.join(', ') + ') as properties';
-    }
+  if (_.fields) {
+    const propertyKeyValuePairs = _.fields?.split(',').map(field => {
+      const value = _.workspace.templates[field]?.template || field;
+      return `'${field}',${value}`;
+    });
+    properties = ', json_build_object(' + propertyKeyValuePairs.join(', ') + ') as properties';
+  }
 
-    const where = _.viewport || `AND ${_.geom || _.layer.geom} IS NOT NULL`
+  const where = _.viewport || `AND ${_.geom || _.layer.geom} IS NOT NULL`
 
-    return `
-        SELECT
-        'Feature' AS type,
-        \${qID} AS id,
-        ST_asGeoJson(${_.geom || _.layer.geom})::json AS geometry
-        ${properties}
-        FROM \${table}
-        WHERE TRUE ${where} \${filter};`
+  return `
+    SELECT
+    'Feature' AS type,
+    \${qID} AS id,
+    ST_asGeoJson(${_.geom || _.layer.geom})::json AS geometry
+    ${properties}
+    FROM \${table}
+    WHERE TRUE ${where} \${filter};`
 }
