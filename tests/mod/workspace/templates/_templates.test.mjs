41650adcee85242836f0182333940da0527552c1
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-11-07 12:07:46 +0200

    WKT Template test

--- tests/mod/workspace/templates/_templates.test.mjs
+++ tests/mod/workspace/templates/_templates.test.mjs
@@ -3,11 +3,13 @@ import { clusterHexTemplate } from './cluster_hex.test.mjs';
 import { geojsonTemplate } from './geojson.test.mjs';
 import { locationGetTemplate } from './location_get.test.mjs';
 import { mvtTemplate } from './mvt.test.mjs';
+import { wktTemplate } from './wkt.test.mjs';
 
 export const templatesTest = {
     clusterTemplate,
     clusterHexTemplate,
     geojsonTemplate,
     locationGetTemplate,
-    mvtTemplate
+    mvtTemplate,
+    wktTemplate
 }
