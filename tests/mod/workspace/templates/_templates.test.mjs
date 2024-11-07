49d156e750acaa7424accb4ae1fa3ac576435630
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-11-07 10:32:25 +0200

    Update tests

--- tests/mod/workspace/templates/_templates.test.mjs
+++ tests/mod/workspace/templates/_templates.test.mjs
@@ -1,5 +1,9 @@
 import { clusterTemplate } from './cluster.test.mjs';
+import { clusterHexTemplate } from './cluster_hex.test.mjs';
+import { geojsonTemplate } from './geojon.test.mjs';
 
 export const templatesTest = {
-    clusterTemplate
+    clusterTemplate,
+    clusterHexTemplate,
+    geojsonTemplate
 }
