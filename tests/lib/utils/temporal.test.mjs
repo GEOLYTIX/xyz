0b6f7e13c781af6a669c60d0f10d76017d8a7ce2
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-12-13 16:18:35 +0200

    Update temporal module & tests

--- tests/lib/utils/temporal.test.mjs
+++ tests/lib/utils/temporal.test.mjs
@@ -40,19 +40,69 @@ export function temporal() {
         codi.it('Should convert date string to Unix timestamp', () => {
 
             const testCases = [
-                { input: '2024-01-01', expected: 1704067200 },           // ISO date
-                { input: '01/01/2024', expected: 1704067200 },          // MM/DD/YYYY
-                { input: 'January 1, 2024', expected: 1704067200 },     // Month DD, YYYY
-                { input: '2024.01.01', expected: 1704067200 },          // YYYY.MM.DD
-                { input: '1 Jan 2024', expected: 1704067200 },          // D MMM YYYY
+                { input: '2024-01-01 GMT', expected: 1704067200 },           // ISO date
+                { input: '01/01/2024 GMT', expected: 1704067200 },          // MM/DD/YYYY
+                { input: 'January 1, 2024 GMT', expected: 1704067200 },     // Month DD, YYYY
+                { input: '2024.01.01 GMT', expected: 1704067200 },          // YYYY.MM.DD
+                { input: '1 Jan 2024 GMT', expected: 1704067200 },          // D MMM YYYY
             ];
 
             testCases.forEach(({ input, expected }) => {
 
-                input = mapp.utils.temporal.dateToUnixEpoch(input);
-
-                codi.assertEqual(input, expected)
+                const epoch = mapp.utils.temporal.dateToUnixEpoch(input);
+                codi.assertEqual(epoch, expected)
             });
+
+        });
+
+        codi.it('Should throw an error for invalid date strings', () => {
+            const errors = [];
+
+            const invalidDates = [
+                'not a date',
+                '2024-13-01',        // invalid month
+                '2024-01-32',        // invalid day
+                '01/32/2024',        // invalid day US format
+                'February 30, 2024', // invalid day for February
+                '2024/13/01',        // invalid month
+                {},                  // object
+                [],                  // array
+                null,               // null
+                undefined,          // undefined
+                '',                 // empty string
+                '   ',              // whitespace
+                '2024-01-01Z',      // malformed ISO
+                'Tomorrow',         // relative dates not supported
+            ];
+
+            invalidDates.forEach(invalidDate => {
+
+                try {
+                    mapp.utils.temporal.dateToUnixEpoch(invalidDate), 'Invalid date string provided';
+                } catch (error) {
+                    errors.push(error.message);
+                }
+            });
+
+            codi.assertTrue((errors.length === 9), 'We expect to have 9 errors logged back')
+
+        });
+
+        codi.it('Should format unix timestamps to date time', () => {
+
+            const epoch = 1704067200;
+            const datetime = mapp.utils.temporal.datetime({ value: epoch });
+
+            codi.assertEqual(datetime, '2024-01-01T00:00:00', 'The datetime should b be the 1st of January 2024')
+
+        });
+
+        codi.it('Should format unix timestamps to date', () => {
+
+            const epoch = 1704067200;
+            const datetime = mapp.utils.temporal.date({ value: epoch });
+
+            codi.assertEqual(datetime, '2024-01-01', 'The datetime should b be the 1st of January 2024')
 
         });
 
