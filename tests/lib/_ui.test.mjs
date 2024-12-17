a7e51021a5d11f1a3a27dbbfb54a30c6beb761e6
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-12-17 15:38:49 +0200

    Update tests

--- tests/lib/_ui.test.mjs
+++ tests/lib/_ui.test.mjs
@@ -1,13 +1,14 @@
 import { elements } from './ui/elements/_elements.test.mjs';
 import { layers } from './ui/layers/_layers.test.mjs';
 import { locations } from './ui/locations/_locations.test.mjs';
-import { Tabview } from './ui/Tabview.test.mjs';
-import { DataviewTest } from './ui/Dataview.test.mjs';
 
 export const uiTest = {
+    setup,
     layers,
     locations,
-    elements,
-    Tabview,
-    DataviewTest
+    elements
+}
+
+function setup() {
+    codi.describe({ name: 'UI:', id: 'ui' }, () => { });
 }
