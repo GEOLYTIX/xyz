a74b75ba28bf427f5faa160c2f95cefa672d2e01
Robert Hurst <robert.hurst@geolytix.co.uk>
2025-01-16 20:01:08 +0200

    express rate limiter

--- express.js
+++ express.js
@@ -4,7 +4,18 @@ const express = require('express');
 
 const cookieParser = require('cookie-parser');
 
+const rateLimit = require('express-rate-limit');
+
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false,
+});
+
+app.use(limiter);
 
 app.use(
   '/xyz',
