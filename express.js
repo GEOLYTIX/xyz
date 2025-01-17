9b01a8ee08a231ad0243729dd81f937a6ea1d77b
Robert Hurst <robert.hurst@geolytix.co.uk>
2025-01-17 14:43:22 +0200

    Take away powered by header

--- express.js
+++ express.js
@@ -8,6 +8,8 @@ const rateLimit = require('express-rate-limit');
 
 const app = express();
 
+app.disable('x-powered-by');
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
