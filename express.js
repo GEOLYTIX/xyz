f83fae35e1c420e2da1193861a3a851f443d565a
Robert Hurst <robert.hurst@geolytix.co.uk>
2025-01-17 14:35:21 +0200

    Add in rate-limit library & formatting

--- express.js
+++ express.js
@@ -1,65 +1,99 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
+const express = require('express');
 
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 
-const app = express()
+const rateLimit = require('express-rate-limit');
 
-app.use('/xyz', express.static('docs', {
-    extensions: ['html']
-}))
+const app = express();
 
-app.use(`${process.env.DIR || ''}/public`, express.static('public'))
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false,
+});
 
-app.use(process.env.DIR || '', express.static('public'))
+app.use(limiter);
 
-app.use(`${process.env.DIR || ''}/tests`, express.static('tests'))
+app.use(
+  '/xyz',
+  express.static('docs', {
+    extensions: ['html'],
+  }),
+);
 
-app.use(process.env.DIR || '', express.static('tests'))
+app.use(`${process.env.DIR || ''}/public`, express.static('public'));
 
-app.use(cookieParser())
+app.use(process.env.DIR || '', express.static('public'));
 
-const api = require('./api/api')
+app.use(`${process.env.DIR || ''}/tests`, express.static('tests'));
 
-app.get(`${process.env.DIR || ''}/api/provider/:provider?`, api)
+app.use(process.env.DIR || '', express.static('tests'));
 
-app.post(`${process.env.DIR || ''}/api/provider/:provider?`, express.json({ limit: '5mb' }), api)
+app.use(cookieParser());
 
-app.get(`${process.env.DIR || ''}/api/sign/:provider?`, api)
+const api = require('./api/api');
 
-app.post(`${process.env.DIR || ''}/api/sign/:provider?`, express.json({ limit: '5mb' }), api)
+app.get(`${process.env.DIR || ''}/api/provider/:provider?`, api);
 
+app.post(
+  `${process.env.DIR || ''}/api/provider/:provider?`,
+  express.json({ limit: '5mb' }),
+  api,
+);
 
-app.get(`${process.env.DIR || ''}/api/query/:template?`, api)
+app.get(`${process.env.DIR || ''}/api/sign/:provider?`, api);
 
-app.post(`${process.env.DIR || ''}/api/query/:template?`, express.json({ limit: '5mb' }), api)
+app.post(
+  `${process.env.DIR || ''}/api/sign/:provider?`,
+  express.json({ limit: '5mb' }),
+  api,
+);
 
+app.get(`${process.env.DIR || ''}/api/query/:template?`, api);
 
-app.get(`${process.env.DIR || ''}/api/fetch/:template?`, api)
+app.post(
+  `${process.env.DIR || ''}/api/query/:template?`,
+  express.json({ limit: '5mb' }),
+  api,
+);
 
-app.post(`${process.env.DIR || ''}/api/fetch/:template?`, express.json({ limit: '5mb' }), api)
+app.get(`${process.env.DIR || ''}/api/fetch/:template?`, api);
 
+app.post(
+  `${process.env.DIR || ''}/api/fetch/:template?`,
+  express.json({ limit: '5mb' }),
+  api,
+);
 
-app.get(`${process.env.DIR || ''}/api/workspace/:key?`, api)
+app.get(`${process.env.DIR || ''}/api/workspace/:key?`, api);
 
+app.get(`${process.env.DIR || ''}/api/user/:method?/:key?`, api);
 
-app.get(`${process.env.DIR || ''}/api/user/:method?/:key?`, api)
+app.post(
+  `${process.env.DIR || ''}/api/user/:method?`,
+  [express.urlencoded({ extended: true }), express.json({ limit: '5mb' })],
+  api,
+);
 
-app.post(`${process.env.DIR || ''}/api/user/:method?`, [express.urlencoded({ extended: true }), express.json({ limit: '5mb' })], api)
+app.get(`${process.env.DIR || ''}/saml/metadata`, api);
 
-app.get(`${process.env.DIR || ''}/saml/metadata`, api)
+app.get(`${process.env.DIR || ''}/saml/logout`, api);
 
-app.get(`${process.env.DIR || ''}/saml/logout`, api)
+app.get(`${process.env.DIR || ''}/saml/login`, api);
 
-app.get(`${process.env.DIR || ''}/saml/login`, api)
+app.post(
+  `${process.env.DIR || ''}/saml/acs`,
+  express.urlencoded({ extended: true }),
+  api,
+);
 
-app.post(`${process.env.DIR || ''}/saml/acs`, express.urlencoded({ extended: true }), api)
+app.get(`${process.env.DIR || ''}/view/:template?`, api);
 
-app.get(`${process.env.DIR || ''}/view/:template?`, api)
+app.get(`${process.env.DIR || ''}/:locale?`, api);
 
-app.get(`${process.env.DIR || ''}/:locale?`, api)
+process.env.DIR && app.get(`/`, api);
 
-process.env.DIR && app.get(`/`, api)
-
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
