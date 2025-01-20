194fe26d4b63a2890f3823a97ae3a5a17d2ca641
Robert Hurst <robert.hurst@geolytix.co.uk>
2025-01-20 17:54:34 +0200

    Revert "Husky test"
    This reverts commit 6621f45d6dc4a967d30997cca9e110208c4b82d8.

--- tests/mod/query.test.mjs
+++ tests/mod/query.test.mjs
@@ -1,3 +1,4 @@
+
 /**
  * ## queryTest()
  * @module mod/query
@@ -5,80 +6,52 @@
 
 /**
  * This function is used to test the query API endpoint
- * @function querytest
- * @param {Object} mapview
- */
+ * @function querytest 
+ * @param {Object} mapview 
+*/
 export async function queryTest() {
-  await codi.describe('Query: Testing Query API', async () => {
-    /**
-     * @description Query: Testing Query defined on infoj entry
-     * @function it
-     */
-    await codi.it('Query: Testing Query defined on infoj entry', async () => {
-      const expected_result = [1, 2, 5, 3, 4];
-      const results = await mapp.utils.xhr(
-        `/test/api/query?template=data_array`,
-      );
-      codi.assertEqual(
-        results,
-        expected_result,
-        'We should be able to just call the template even if its not part of the workspace.templates object in configuration',
-      );
-    });
-    /**
-     * @description Query: Testing Module defined in templates
-     * @function it
-     */
-    await codi.it('Query: Testing Module defined in templates', async () => {
-      const expected_result = {
-        bar: 'foo',
-      };
-      const results = await mapp.utils.xhr(
-        `/test/api/query?template=module_test`,
-      );
-      codi.assertEqual(
-        results,
-        expected_result,
-        'The Module should return the basic query',
-      );
-    });
 
-    /**
-     * @description Query: Testing a query with a bogus dbs string via the req params
-     * @function it
-     */
-    await codi.it(
-      'Query: Testing a query with a bogus dbs string via the req params',
-      async () => {
-        const expected_result = {
-          bar: 'foo',
-        };
-        const results = await mapp.utils.xhr(
-          `/test/api/query?template=module_test&dbs=bogus`,
-        );
-        codi.assertEqual(
-          results,
-          expected_result,
-          'The Module should return the basic query',
-        );
-      },
-    );
+    await codi.describe('Query: Testing Query API', async () => {
+        /**
+         * @description Query: Testing Query defined on infoj entry
+         * @function it 
+         */
+        await codi.it('Query: Testing Query defined on infoj entry', async () => {
+            const expected_result = [1, 2, 5, 3, 4];
+            const results = await mapp.utils.xhr(`/test/api/query?template=data_array`);
+            codi.assertEqual(results, expected_result, 'We should be able to just call the template even if its not part of the workspace.templates object in configuration');
+        });
+        /**
+         * @description Query: Testing Module defined in templates
+         * @function it 
+         */
+        await codi.it('Query: Testing Module defined in templates', async () => {
+            const expected_result = {
+                'bar': 'foo'
+            }
+            const results = await mapp.utils.xhr(`/test/api/query?template=module_test`);
+            codi.assertEqual(results, expected_result, 'The Module should return the basic query');
+        });
 
-    /**
-     * @description Query: Testing a query with a bogus dbs on the template
-     * @function it
-     */
-    await codi.it(
-      'Query: Testing a query with a bogus dbs on the template',
-      async () => {
-        const results = await mapp.utils.xhr(
-          `/test/api/query?template=bogus_data_array`,
-        );
-        codi.assertTrue(
-          results instanceof Error,
-          'We should return an error for a bogus DBS connection',
-        );
-      },
-    );
-  });
+        /**
+         * @description Query: Testing a query with a bogus dbs string via the req params
+         * @function it 
+         */
+        await codi.it('Query: Testing a query with a bogus dbs string via the req params', async () => {
+            const expected_result = {
+                'bar': 'foo'
+            }
+            const results = await mapp.utils.xhr(`/test/api/query?template=module_test&dbs=bogus`);
+            codi.assertEqual(results, expected_result, 'The Module should return the basic query');
+        });
+
+        /**
+         * @description Query: Testing a query with a bogus dbs on the template
+         * @function it 
+         */
+        await codi.it('Query: Testing a query with a bogus dbs on the template', async () => {
+            const results = await mapp.utils.xhr(`/test/api/query?template=bogus_data_array`);
+            codi.assertTrue(results instanceof Error, 'We should return an error for a bogus DBS connection');
+        });
+    });
 }
