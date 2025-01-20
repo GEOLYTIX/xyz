1e52d9286ad38b743bb791216128651c0318283a
Robert Hurst <robert.hurst@geolytix.co.uk>
2025-01-20 14:25:13 +0200

    Update dataviews tests

--- tests/lib/ui/layers/_layers.test.mjs
+++ tests/lib/ui/layers/_layers.test.mjs
@@ -1,16 +1,18 @@
 import { filters } from './filters.test.mjs';
-import { filter } from './panels/filter.test.mjs'
+import { filter } from './panels/filter.test.mjs';
+import { dataviews } from './panels/dataviews.test.mjs';
 import { view } from './view.test.mjs';
 
 export const layers = {
-    setup,
-    filters,
-    panels: {
-        filter,
-    },
-    view
-}
+  setup,
+  filters,
+  panels: {
+    filter,
+    dataviews,
+  },
+  view,
+};
 
 function setup() {
-    codi.describe({ name: 'UI Layers:', id: 'ui_layers' }, () => { });
+  codi.describe({ name: 'UI Layers:', id: 'ui_layers' }, () => {});
 }
