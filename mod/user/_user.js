2a1db148ae659c395f2d4d5fca99d7c0325ec8c0
Robert Hurst <robert.hurst@geolytix.co.uk>
2025-01-24 15:44:31 +0200

    formatting

--- mod/user/_user.js
+++ mod/user/_user.js
@@ -21,7 +21,7 @@ The _user module exports the user method to route User API requests.
 @module /user
 */
 
-const reqHost = require('../utils/reqHost')
+const reqHost = require('../utils/reqHost');
 
 const methods = {
   add: require('./add'),
@@ -36,9 +36,9 @@ const methods = {
   token: require('./token'),
   update: require('./update'),
   verify: require('./verify'),
-}
+};
 
-const previousAddress = {}
+const previousAddress = {};
 
 /**
 @function user
@@ -59,29 +59,25 @@ Requests to the user module are debounced by 5 seconds preventing registration, 
 @param {string} req.params.method Method request parameter.
 */
 module.exports = async function user(req, res) {
-
   if (!Object.hasOwn(methods, req.params.method)) {
-
-    return res.send(`Failed to evaluate 'method' param.`)
+    return res.send(`Failed to evaluate 'method' param.`);
   }
 
   if (new Set(['login', 'register']).has(req.params.method)) {
-
-    debounceRequest(req, res)
+    debounceRequest(req, res);
 
     if (res.finished) return;
   }
 
-  req.params.host = reqHost(req)
+  req.params.host = reqHost(req);
 
-  const method = await methods[req.params.method](req, res)
+  const method = await methods[req.params.method](req, res);
 
   if (method instanceof Error) {
-
-    req.params.msg = method.message
-    methods.login(req, res)
+    req.params.msg = method.message;
+    methods.login(req, res);
   }
-}
+};
 
 /**
 @function debounceRequest
@@ -95,28 +91,29 @@ The remote_address determined from the request header is stored in the previousA
 @property {Object} req.header HTTP request header.
 */
 function debounceRequest(req, res) {
-
   if (!req.headers['x-forwarded-for']) {
-
-    req.params.remote_address = 'unknown'
+    req.params.remote_address = 'unknown';
   } else {
-
-    req.params.remote_address = /^[A-Za-z0-9.,_-\s]*$/.test(req.headers['x-forwarded-for']) 
+    req.params.remote_address = /^[A-Za-z0-9.,_-\s]*$/.test(
+      req.headers['x-forwarded-for'],
+    )
       ? req.headers['x-forwarded-for']
-      : 'invalid'
+      : 'invalid';
   }
 
   // The remote_address has been previously used
-  if (Object.hasOwn(previousAddress, req.params.remote_address)
-
+  if (
+    Object.hasOwn(previousAddress, req.params.remote_address) &&
     // within 5 seconds or less.
-    && new Date() - previousAddress[req.params.remote_address] < 5000) {
-
-    res.status(429).send(`Address ${req.params.remote_address} temporarily locked.`)
+    new Date() - previousAddress[req.params.remote_address] < 5000
+  ) {
+    res
+      .status(429)
+      .send(`Address ${req.params.remote_address} temporarily locked.`);
 
     return;
   }
 
   // Log the remote_address with the current datetime.
-  previousAddress[req.params.remote_address] = new Date()
+  previousAddress[req.params.remote_address] = new Date();
 }
