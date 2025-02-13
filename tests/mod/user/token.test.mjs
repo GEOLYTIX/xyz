56f9c14141f3911ad4965dee02bde61e224cb53d
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2025-02-13 17:30:35 +0000

    update token module

--- tests/mod/user/token.test.mjs
+++ tests/mod/user/token.test.mjs
@@ -7,14 +7,6 @@ globalThis.xyzEnv.SECRET = secret;
 await codi.describe(
   { name: 'token:', id: 'user_token', parentId: 'user' },
   async () => {
-    await codi.it(
-      { name: 'TEST ME! @dbauszus-glx', parentId: 'user_token' },
-      () => {
-        //TODO: @dbauszus-glx Please test me
-        codi.assertTrue(false);
-      },
-    );
-
     await codi.it({ name: 'no user', parentId: 'user_token' }, async () => {
       const { req, res } = codi.mockHttp.createMocks({
         params: {},
@@ -29,9 +21,12 @@ await codi.describe(
       );
     });
 
+    let token;
+
     await codi.it({ name: 'admin user', parentId: 'user_token' }, async () => {
       const { req, res } = codi.mockHttp.createMocks({
         params: {
+          expiresin: '10hr',
           user: {
             email: 'test@geolytix.co.uk',
             roles: [],
@@ -44,12 +39,37 @@ await codi.describe(
 
       await userToken(req, res);
 
-      const token = res._getData();
+      token = res._getData();
 
       const user = jwt.verify(token, xyzEnv.SECRET);
 
-      // token expires in 8hr.
-      codi.assertTrue(user.exp - user.iat === 28800);
+      // token expires in 10hr.
+      codi.assertTrue(user.exp - user.iat === 36000);
+
+      // user from token must not have admin rights.
+      codi.assertTrue(!user.admin);
+    });
+
+    await codi.it({ name: 'token user', parentId: 'user_token' }, async () => {
+      const user = jwt.verify(token, xyzEnv.SECRET);
+
+      //TODO: get user from login with token.
+
+      const { req, res } = codi.mockHttp.createMocks({
+        params: {
+          user,
+        },
+      });
+
+      const { default: userToken } = await import('../../../mod/user/token.js');
+
+      const response = await userToken(req, res);
+
+      codi.assertTrue(
+        response instanceof Error &&
+          response.message ===
+            'Token may not be generated from token authentication.',
+      );
     });
   },
 );
