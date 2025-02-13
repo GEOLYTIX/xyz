fb68128e273ed9d40349da7f6590accd76e5ae00
Dennis Bauszus <dennis.bauszus@geolytix.co.uk>
2025-02-13 11:11:50 +0000

    no user token test

--- tests/mod/user/token.test.mjs
+++ tests/mod/user/token.test.mjs
@@ -1,9 +1,26 @@
 await codi.describe(
   { name: 'token:', id: 'user_token', parentId: 'user' },
   async () => {
-    codi.it({ name: 'TEST ME! @dbauszus-glx', parentId: 'user_token' }, () => {
-      //TODO: @dbauszus-glx Please test me
-      codi.assertTrue(false);
+    await codi.it(
+      { name: 'TEST ME! @dbauszus-glx', parentId: 'user_token' },
+      () => {
+        //TODO: @dbauszus-glx Please test me
+        codi.assertTrue(false);
+      },
+    );
+
+    await codi.it({ name: 'no user', parentId: 'user_token' }, async () => {
+      const { req, res } = codi.mockHttp.createMocks({
+        params: {},
+      });
+
+      const { default: userToken } = await import('../../../mod/user/token.js');
+
+      const response = await userToken(req, res);
+
+      codi.assertTrue(
+        response instanceof Error && response.message === 'login_required',
+      );
     });
   },
 );
