7f06d54a5f3d099f0d947921cf9e9242e3cba06f
Robert Hurst <robert.hurst@geolytix.co.uk>
2025-02-27 15:04:39 +0200

    Update version script

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 /**
 ## Version script
 This script is used for developers to update the commit SHA in the attribution of the mapp bundle.
-This is used to track deployed applications and what exact version of the xyz framework it is using. 
+This is used to track deployed applications and what exact version of the xyz framework it is using.
 To run this script you can execute `node version.js` in your terminal.
 
 @requires module:fs
@@ -10,8 +10,8 @@ To run this script you can execute `node version.js` in your terminal.
 */
 
 // File system module for reading/writing files
-const fs = require('fs');
-const { execSync } = require('child_process'); // For executing shell commands synchronously
+import { readFileSync, writeFileSync } from 'fs';
+import { execSync } from 'child_process'; // For executing shell commands synchronously
 
 // Regular expression pattern to match 'hash: ' followed by any characters
 const key = 'hash:.*';
@@ -26,7 +26,7 @@ const hash = execSync('git rev-parse HEAD')
   .trim();
 
 // Read the contents of mapp.mjs file
-let data = fs.readFileSync(
+let data = readFileSync(
   // Path to the file
   './lib/mapp.mjs',
 
@@ -44,7 +44,7 @@ data = data.replace(
 );
 
 // Write the updated content back to the file
-fs.writeFileSync(
+writeFileSync(
   // Path to the file
   './lib/mapp.mjs',
 
