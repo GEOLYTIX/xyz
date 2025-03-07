f93249e1d5ae1b24e894121c23e06565cd5e8a3e
Dennis Bauszus <dennis.bauszus@geolytix.co.uk>
2025-03-07 12:19:07 +0000

    provider/file documentation

--- mod/provider/file.js
+++ mod/provider/file.js
@@ -3,19 +3,30 @@
 
 The file provider module exports a method to fetch resources from the local file system.
 
-Since node v21.2.0 & v20.11.0
-node added the `import.meta.dirname` property into their [import attributes](https://nodejs.org/api/esm.html#import-attributes).
-It provides the directory name of the current module.
-This is the same as the `path.dirname()` of the `import.meta.filename`.
-
 @module /provider/file
-@requires module:/utils/processEnv
 */
 
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
-export default async function file(ref) {
+/**
+@function file
+
+@description
+The file method requires a reference for a file in the local filesystem. A path for the file will be extracted from the reference.
+
+Matched `SRC_*` variables in the src reference string are replaced with values from the global xyzEnv.
+
+The file is read synchronous from the filesystem.
+
+The buffer returned from the read process will be parsed as json if the referenced filename extension is matched. Otherwise the buffer is returned as string.
+
+The [node import module attributes]{@link https://nodejs.org/api/esm.html#import-attributes} allow access to the directory name through the meta property.
+
+@param {object|string} ref The file reference maybe defined as an object or string. A string is assumed to be the params.url property of the file location.
+@returns {Promise<File|Error>} The file will be returned as parsed json or string if the readFileSync process succeeds.
+*/
+export default function file(ref) {
   try {
     // Subtitutes {*} with xyzEnv.SRC_* key values.
     const path = (ref.params?.url || ref).replace(
@@ -23,6 +34,7 @@ export default async function file(ref) {
       (matched) => xyzEnv[`SRC_${matched.replace(/(^{)|(}$)/g, '')}`],
     );
 
+    // Join the releative path with the import directory name to generate a path with platform specific separator as delimiter.
     const file = readFileSync(join(import.meta.dirname, `../../${path}`));
 
     if (path.match(/\.json$/i)) {
