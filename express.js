5a68805a10464e44369370c461ef1c523d21bfc8
Dennis Bauszus <dennis.bauszus@geolytix.co.uk>
2025-03-14 11:17:34 +0000

    Format document

--- express.js
+++ express.js
@@ -51,7 +51,7 @@ import rateLimit from 'express-rate-limit';
 import api from './api/api.js';
 
 if (process.versions.node.split('.')[0] < 22) {
-  console.warn(`Process Node version below 22.`)
+  console.warn(`Process Node version below 22.`);
 }
 
 const app = express();
