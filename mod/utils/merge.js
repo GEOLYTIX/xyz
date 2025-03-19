ce94a38181c6a7740d98941f583b4a60506adea7
Dennis Bauszus <dennis.bauszus@geolytix.co.uk>
2025-03-19 16:39:35 +0000

    Reduce complexity in the mergedeep utility module.

--- mod/utils/merge.js
+++ mod/utils/merge.js
@@ -1,7 +1,27 @@
 /**
+## /utils/merge
+
+An XYZ utility module to merge complex objects [including function parameters] deeply.
+
+The utility is required to compose nested workspace object properties [eg locales, layer] from templates.
+
 @module /utils/merge
 */
 
+/**
+@function mergeDeep
+
+@description
+Method receives a target object and sources arguments spread into an array.
+
+The target will be returned if the sources array argument has no length.
+
+A source will be shited from the sources array argument and processed against the target object.
+
+Each source object property will be iterated if both the target and source are object instances.
+
+The method will iterate over itself until the object arguments are merged into the target.
+*/
 export default function mergeDeep(target, ...sources) {
   // No sources to merge.
   if (!sources.length) {
@@ -15,33 +35,71 @@ export default function mergeDeep(target, ...sources) {
   if (isObject(target) && isObject(source)) {
     // Iterate over object keys in source.
     for (const key in source) {
-      if (isObject(source[key])) {
-        // Assign empty object on key.
-        if (!target[key]) Object.assign(target, { [key]: {} });
 
-        // Call recursive merge for target key object.
-        Object.hasOwn(target, key) && mergeDeep(target[key], source[key]);
+      // Spread source array into target array.
+      if (arrayMerge(source, target, key)) continue;
 
-        // Target and Source are both arrays.
-      } else if (Array.isArray(source[key]) && Array.isArray(target[key])) {
-        // Target and Source arrays are equal.
-        if (source[key].every((item) => target[key].includes(item))) {
-          // Do not merge.
-          target[key] = source[key];
-        } else {
-          // Merge unequal arrays.
-          target[key] = [...(target[key] || []), ...source[key]];
-        }
-      } else {
-        // Assign key if not an object itself.
-        Object.assign(target, { [key]: source[key] });
-      }
+      // mergeDeep source object into target object.
+      if (objectMerge(source, target, key)) continue;
+    
+      // Assign the source[key] property to target object.
+      Object.assign(target, { [key]: source[key] });
     }
   }
 
   return mergeDeep(target, ...sources);
 }
 
+/**
+@function arrayMerge
+
+@description
+Method checks whether the provided source and target objects have array properties with the same key.
+
+The source array will be spread into the target array only if it contains items which are not included in the target array.
+*/
+function arrayMerge(source, target, key) {
+  // Check whether source and target are both arrays.
+  if (Array.isArray(source[key]) && Array.isArray(target[key])) {
+    // Check whether the source and target arrays are equal.
+    if (source[key].every((item) => target[key].includes(item))) {
+      // Do not merge.
+      target[key] = source[key];
+    } else {
+      // Merge unequal arrays.
+      target[key] = [...(target[key] || []), ...source[key]];
+    }
+
+    return true;
+  }
+}
+
+/**
+@function objectMerge
+
+@description
+The method will create an empty object if the target object does not have a key property.
+
+The mergeDeep method will be called recursive to merge the source key object into the target key object property.
+*/
+function objectMerge(source, target, key) {
+  if (isObject(source[key])) {
+    // Assign empty object on key.
+    if (!target[key]) Object.assign(target, { [key]: {} });
+
+    // Call recursive merge for target key object.
+    Object.hasOwn(target, key) && mergeDeep(target[key], source[key]);
+
+    return true
+  }
+}
+
+/**
+@function isObject
+
+@description
+Method checks whether the item param is an Object but not an Array.
+*/
 function isObject(item) {
   return !Array.isArray(item) && item instanceof Object;
 }
