71658dc2e62f98f8536c57f4942b938ba49f6bcc
Robert Hurst <robert.hurst@geolytix.co.uk>
2025-03-19 16:31:50 +0200

    fs docs

--- tests/__preload/mod/provider/fs.mjs
+++ tests/__preload/mod/provider/fs.mjs
@@ -1,9 +1,17 @@
 import file from '../../../../mod/provider/file.js';
 
+/**
+ * @type {import('../../../../mod/provider/file.js').default}
+ */
 const filefn = codi.mock.fn(file);
 
 codi.mock.module('../../../../mod/provider/file.js', {
   defaultExport: filefn,
 });
 
+/**
+ * A globally exposed mock function for the file module.
+ * @global
+ * @type {import('../../../../mod/provider/file.js').default}
+ */
 globalThis.fileFn = filefn;
