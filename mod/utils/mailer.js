44f74ca268f20c3487b8757277d94157792cbbf4
Robert Hurst <robert.hurst@geolytix.co.uk>
2025-04-01 09:50:52 +0200

    mailer transport update
    Updated docs and added in two keys
    
    TRANSPORT_USERNAME
    TRANSPORT_NAME

--- mod/utils/mailer.js
+++ mod/utils/mailer.js
@@ -1,5 +1,6 @@
 /**
 @module /utils/mailer
+@description
 The mailer module provides a way to send emails to clients/admins, etc.
 
 @requires module:/utils/logger
@@ -20,6 +21,7 @@ let nodeMailer, transport;
 if (
   xyzEnv.TRANSPORT ||
   xyzEnv.TRANSPORT_EMAIL ||
+  xyzEnv.TRANSPORT_USERNAME ||
   xyzEnv.TRANSPORT_HOST ||
   xyzEnv.TRANSPORT_PASSWORD
 ) {
@@ -39,7 +41,7 @@ export default mailer;
 @description
 Function which sends email using the nodemailer dependancy.
 
-@param {Object} params 
+@param {Object} params
 @property {String} params.to The email recipient.
 @property {String} params.template The html that will make up the body of the email.
 @property {String} params.language The language to be used.
@@ -73,12 +75,12 @@ async function mailer(params) {
   if (!transport) {
     transport = nodeMailer.createTransport({
       host: xyzEnv.TRANSPORT_HOST,
-      name: xyzEnv.TRANSPORT_EMAIL.split('@')[0],
+      name: xyzEnv.TRANSPORT_NAME || xyzEnv.TRANSPORT_EMAIL.split('@')[0],
       port: xyzEnv.TRANSPORT_PORT || 587,
       secure: false,
       requireTLS: xyzEnv.TRANSPORT_TLS,
       auth: {
-        user: xyzEnv.TRANSPORT_EMAIL,
+        user: xyzEnv.TRANSPORT_USERNAME,
         pass: xyzEnv.TRANSPORT_PASSWORD,
       },
     });
@@ -115,7 +117,7 @@ async function mailer(params) {
 @description
 Retrieves the body of the text from the provided url/file.
 
-@param {Object} template 
+@param {Object} template
 @property {String} template.text The url from which to retrieve the text content using {@link module:/provider/getFrom~flyTo}.
 @property {String} template.html The url from which to retrieve the html content using {@link module:/provider/getFrom~flyTo}.
 */
@@ -142,7 +144,7 @@ async function getBody(template) {
 @description
 Substitutes supplied params into a supplied string.
 
-@param {String} string  
+@param {String} string
 @property {Object} params The url from which to retrieve the text content using {@link module:/provider/getFrom~flyTo}.
 
 @returns {String} The string with substitutions made.
