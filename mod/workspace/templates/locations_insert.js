c35655a8949ec05e6b3c3593ffede4ab4995afcc
AlexanderGeere <alex.geere@geolytix.co.uk>
2025-04-01 16:55:48 +0200

    make the template layer agnostic

--- mod/workspace/templates/locations_insert.js
+++ mod/workspace/templates/locations_insert.js
@@ -8,6 +8,7 @@ The locations_insert layer query inserts a record in the layer table.
 export default (_) => {
   // The location ID must not be inserted.
   if (
+    _.layer &&
     Object.keys(_.body.columns).some(
       (key) => key === _.layer.qID || key === 'id',
     )
@@ -17,10 +18,20 @@ export default (_) => {
     );
   }
 
-  if (!Array.isArray(_.body.data)) _.body.data = [_.body.data];
-
-  const data = _.body.data;
+  let data = _.body.data || _.body.values;
   const columns = _.body.columns;
+  const table = _.layer?.table || _.table;
 
-  return `INSERT INTO ${_.layer.table} (${columns.join(',')}) VALUES ${data.join()};`;
+  if (!Array.isArray(data)) data = [data];
+
+  for (const [values, indx] of Object.entries(data)) {
+    if (typeof values === 'string') continue;
+
+    data[indx] = Object.keys(data[indx]).map((key) => data[indx][key]);
+    data[indx] = `(${data[indx].join()})`;
+  }
+
+  if (!table) throw new Error('insert template requires a table');
+
+  return `INSERT INTO ${table} (${columns.join(',')}) VALUES ${data.join()};`;
 };
