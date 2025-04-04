6a077333b5f9d33a1a58b9020510d9344a867034
AlexanderGeere <alex.geere@geolytix.co.uk>
2025-04-04 13:31:15 +0200

    adjust jsonDataview for testing

--- mod/workspace/templates/sql_table_insert.js
+++ mod/workspace/templates/sql_table_insert.js
@@ -1,27 +1,29 @@
 export default (_) => {
   console.log(_);
-  if (!Array.isArray(_.body)) _.body = [_.body];
+  let data = _.body.data;
+  const columns = _.body.fields;
 
-  const fields = Object.keys(_.body[0]);
+  if (!Array.isArray(data)) data = [data];
+
+  const fields = Object.keys(columns);
 
   const data_map = {};
-  for (const values of _.body) {
+  for (const values of data) {
     Object.keys(values).forEach((key) => {
       data_map[key] ??= [];
-      data_map[key].push(values[key] || 'null');
+      data_map[key].push(values[key] || null);
     });
   }
 
   const select_stmnt = [];
   Object.keys(data_map).forEach((field) => {
-    _[field] = data_map[field].join();
-    select_stmnt.push(`unnest(ARRAY[%{${field}}]) as ${field}`);
+    _[field] = data_map[field];
+    select_stmnt.push(`unnest(%{${field}}::${columns[field]}[]) as ${field}`);
   });
 
   const sql = `INSERT INTO ${_.table} 
     (${fields.join(',')})
     SELECT ${select_stmnt.join(',')}`;
 
-  console.log(sql);
   return sql;
 };
