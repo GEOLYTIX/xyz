9c754147bf80a1942be2c2f5053d94d28467150f
AlexanderGeere <alex.geere@geolytix.co.uk>
2025-04-04 09:43:26 +0200

    Use an unnest query

--- mod/workspace/templates/sql_table_insert.js
+++ mod/workspace/templates/sql_table_insert.js
@@ -1,14 +1,27 @@
 export default (_) => {
+  console.log(_);
+  if (!Array.isArray(_.body)) _.body = [_.body];
 
-  const fields = Object.keys(_.body)
+  const fields = Object.keys(_.body[0]);
 
-  fields.forEach(field => {
-    _[field] = _.body[field];
-  })
+  const data_map = {};
+  for (const values of _.body) {
+    Object.keys(values).forEach((key) => {
+      data_map[key] ??= [];
+      data_map[key].push(values[key] || 'null');
+    });
+  }
+
+  const select_stmnt = [];
+  Object.keys(data_map).forEach((field) => {
+    _[field] = data_map[field].join();
+    select_stmnt.push(`unnest(ARRAY[%{${field}}]) as ${field}`);
+  });
 
   const sql = `INSERT INTO ${_.table} 
-    (${fields.join(',')}) 
-    VALUES (%{${fields.join('},%{')}});`;
+    (${fields.join(',')})
+    SELECT ${select_stmnt.join(',')}`;
 
-  return sql
+  console.log(sql);
+  return sql;
 };
