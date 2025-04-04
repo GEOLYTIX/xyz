88f53bfb282db281adc2add96a3a5c1b7a8da766
Dennis Bauszus <dennis.bauszus@geolytix.co.uk>
2025-04-04 16:38:39 +0100

    sql_table_insert

--- mod/workspace/templates/sql_table_insert.js
+++ mod/workspace/templates/sql_table_insert.js
@@ -1,28 +1,49 @@
+/**
+## /workspace/templates/sql_table_insert
+
+The sql_table_insert query template module allows to insert multiple records provided as a request POST body into a SQL table.
+
+The POST body must contain properties for each field. The key will be split on :: to provide the field name and the cast for values in the values array.
+```
+{
+  "letters::varchar": [
+    "a",
+    "b"
+  ],
+  "drop table (table)": [1,2], // Potential SQL Injection
+  "numbers::int": [
+    1,
+    2
+  ]
+}
+```
+
+@module /workspace/templates/sql_table_insert
+*/
 export default (_) => {
-  let data = _.body.data;
-  const columns = _.body.fields;
 
-  if (!Array.isArray(data)) data = [data];
+  const fields = [];
+  const unnests = [];
 
-  const fields = Object.keys(columns);
+  for (const [key, value] of Object.entries(_.body)) {
 
-  const data_map = {};
-  for (const values of data) {
-    Object.keys(values).forEach((key) => {
-      data_map[key] ??= [];
-      data_map[key].push(values[key] || null);
-    });
+    // Split key into field and type array
+    const field_type = key.split('::')
+
+    // Check for SQL Injection in the property key.
+    if (field_type.some((string) => !/^[A-Za-z0-9_-]*$/.exec(string))) {
+      console.warn(`Potential SQL Injection sql_table_insert body.`)
+      continue;
+    }
+
+    _[field_type[0]] = value
+    fields.push(field_type[0])
+    unnests.push(`unnest(%{${field_type[0]}}::${field_type[1]}[]) as ${field_type[0]}`);
   }
-
-  const select_stmnt = [];
-  Object.keys(data_map).forEach((field) => {
-    _[field] = data_map[field];
-    select_stmnt.push(`unnest(%{${field}}::${columns[field]}[]) as ${field}`);
-  });
 
   const sql = `INSERT INTO ${_.table} 
     (${fields.join(',')})
-    SELECT ${select_stmnt.join(',')}`;
+    SELECT ${unnests.join(',')}`;
 
   return sql;
 };
