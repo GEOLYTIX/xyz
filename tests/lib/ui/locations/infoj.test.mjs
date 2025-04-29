cfc6085128753e3e9d7cdf0ed772acf2bc37eb66
Dennis Bauszus <dennis.bauszus@geolytix.co.uk>
2025-04-29 10:08:22 +0100

    entryQuery method in mapp/infoj module; add runOnce flag, cancel currently running query in xhr util. (#2071)
    Co-authored-by: Agata Brok <cityremade@users.noreply.github.com>
    Co-authored-by: Robert Hurst <robert.hurst@geolytix.co.uk>
    Co-authored-by: AlexGeere <alex.geere@gmail.com>

--- tests/lib/ui/locations/infoj.test.mjs
+++ tests/lib/ui/locations/infoj.test.mjs
@@ -4,8 +4,18 @@
  */
 export function infoj() {
   codi.describe(
-    { name: 'infoj test:', id: 'ui_locations_infoj', parentId: 'ui_locations' },
+    { id: 'ui_locations_infoj', name: 'infoj test:', parentId: 'ui_locations' },
     () => {
+      const originalConsole = console.warn;
+
+      const mockWarns = [];
+
+      console.warn = (message) => {
+        mockWarns.push(message);
+      };
+
+      const originalXHR = mapp.utils.xhr;
+
       /**
        * ### It should create an infoj with a correct order
        * 1. We define an infoj with a combination of different entries with keys, fields and queries
@@ -42,9 +52,9 @@ export function infoj() {
                 value: 'value_4',
               },
               {
+                location: {},
                 query: 'query_5',
                 value: 'value_5',
-                location: {},
               },
             ],
           };
@@ -116,9 +126,9 @@ export function infoj() {
                 value: 'value_4',
               },
               {
+                location: {},
                 query: 'query_5',
                 value: 'value_5',
-                location: {},
               },
             ],
           };
@@ -162,6 +172,59 @@ export function infoj() {
           );
         },
       );
+
+      codi.it(
+        {
+          name: 'Entry query method',
+          parentId: 'ui_locations_infoj',
+        },
+        async () => {
+          const location = {
+            infoj: [
+              {
+                field: 'test_field',
+                location: {},
+                query: 'json_query',
+                queryparams: {
+                  id: true,
+                  table: true,
+                },
+                runOnce: true,
+                template: {
+                  key: 'json_query',
+                  template:
+                    'SELECT ST_asGeoJSON(geom_3857) FROM ${table} WHERE glx_id = %{id}',
+                  value_only: true,
+                },
+                type: 'text',
+              },
+            ],
+          };
+
+          mapp.utils.xhr = () => {
+            return new Promise((resolve) => {
+              resolve({ test_field: '1000' });
+            });
+          };
+
+          // Set the order on the layer
+          location.layer = {};
+          location.layer.infoj_order = ['json_query'];
+
+          // Get listview element from the infoj object
+          const infoj = await mapp.ui.locations.infoj(location);
+
+          // Get textvalues from location listview elements.
+          const results = Array.from(infoj.children).map((el) =>
+            el.firstChild.innerText.trim(),
+          );
+
+          codi.assertEqual(results[0], '1000', 'Ensure the entry runs');
+        },
+      );
+
+      console.warn = originalConsole;
+      mapp.utils.xhr = originalXHR;
     },
   );
 }
