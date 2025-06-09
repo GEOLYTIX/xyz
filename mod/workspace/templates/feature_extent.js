3b6a7162095cb641ea320808fb1777807b20790d
AlexGeere <alex.geere@gmail.com>
2025-06-09 10:43:48 +0200

    missing table from query

--- mod/workspace/templates/feature_extent.js
+++ mod/workspace/templates/feature_extent.js
@@ -2,6 +2,7 @@ export default (_) => {
   const ids = _.body.ids;
 
   return `select Box2D(ST_Transform(ST_SetSRID(ST_Extent(${_.geom}),${_.srid}), ${_.proj}))
+                   from ${_.table}
                    where true ${_.filter} 
                    and ${_.qid} in (${ids.join(',')})`;
 };
