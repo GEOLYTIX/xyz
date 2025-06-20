dbea09930570fc0aad3119aeca5d8afe834171a2
Dennis Bauszus <dennis.bauszus@geolytix.co.uk>
2025-06-20 16:30:48 +0100

    sql injection check

--- mod/workspace/templates/feature_extent.js
+++ mod/workspace/templates/feature_extent.js
@@ -1,8 +1,11 @@
 export default (_) => {
 
+  // Check potential SQL Injection.
+  const ids = _.body.ids.filter(id => /^[A-Za-z0-9,"'._-\s]*$/.test(id))
+  
   return `
   SELECT 
     Box2D(ST_Transform(ST_SetSRID(ST_Extent(${_.geom}),${_.srid}), ${_.proj}))
   FROM ${_.table}
-  WHERE \${qID} IN ('${_.body.ids.join("','")}') \${filter}`;
+  WHERE \${qID} IN ('${ids.join("','")}') \${filter}`;
 };
