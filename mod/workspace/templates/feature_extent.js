df37615e0297e3367f7d15d77761ccd80a351d67
Dennis Bauszus <dennis.bauszus@geolytix.co.uk>
2025-06-20 16:41:19 +0100

    format query

--- mod/workspace/templates/feature_extent.js
+++ mod/workspace/templates/feature_extent.js
@@ -1,8 +1,7 @@
 export default (_) => {
-
   // Check potential SQL Injection.
-  const ids = _.body.ids.filter(id => /^[A-Za-z0-9,"'._-\s]*$/.test(id))
-  
+  const ids = _.body.ids.filter((id) => /^[A-Za-z0-9,"'._-\s]*$/.test(id));
+
   return `
   SELECT 
     Box2D(ST_Transform(ST_SetSRID(ST_Extent(${_.geom}),${_.srid}), ${_.proj}))
