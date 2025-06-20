5b0590894f8be90c3e466172c6d28a75e33fa612
Dennis Bauszus <dennis.bauszus@geolytix.co.uk>
2025-06-20 16:22:13 +0100

    clean up feature_extent query

--- mod/workspace/templates/feature_extent.js
+++ mod/workspace/templates/feature_extent.js
@@ -1,9 +1,8 @@
 export default (_) => {
-  const ids = _.body.ids;
 
   return `
   SELECT 
     Box2D(ST_Transform(ST_SetSRID(ST_Extent(${_.geom}),${_.srid}), ${_.proj}))
   FROM ${_.table}
-  WHERE true \${filter} and \${qID} in ('${ids.join("','")}')`;
+  WHERE \${qID} IN ('${_.body.ids.join("','")}') \${filter}`;
 };
