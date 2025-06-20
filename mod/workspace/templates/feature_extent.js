0a9c58d9b7fcd68731613bd22a5eb86648c55bd5
Dennis Bauszus <dennis.bauszus@geolytix.co.uk>
2025-06-20 15:50:59 +0100

    get geom

--- mod/workspace/templates/feature_extent.js
+++ mod/workspace/templates/feature_extent.js
@@ -1,8 +1,9 @@
 export default (_) => {
   const ids = _.body.ids;
 
-  return `select Box2D(ST_Transform(ST_SetSRID(ST_Extent(${_.geom}),${_.srid}), ${_.proj}))
-                   from ${_.table}
-                   where true \${filter}
-                   and \${qID} in (${ids.join(',')})`;
+  return `
+  SELECT 
+    Box2D(ST_Transform(ST_SetSRID(ST_Extent(${_.geom}),${_.srid}), ${_.proj}))
+  FROM ${_.table}
+  WHERE true \${filter} and \${qID} in (${ids.join(',')})`;
 };
