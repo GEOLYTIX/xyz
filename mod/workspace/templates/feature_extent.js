3603ad526988dc095ed69d5bb02980b1410ff6a5
Dennis Bauszus <dennis.bauszus@geolytix.co.uk>
2025-06-20 16:02:17 +0100

    id join

--- mod/workspace/templates/feature_extent.js
+++ mod/workspace/templates/feature_extent.js
@@ -5,5 +5,5 @@ export default (_) => {
   SELECT 
     Box2D(ST_Transform(ST_SetSRID(ST_Extent(${_.geom}),${_.srid}), ${_.proj}))
   FROM ${_.table}
-  WHERE true \${filter} and \${qID} in (${ids.join(',')})`;
+  WHERE true \${filter} and \${qID} in ('${ids.join("','")}')`;
 };
