0672799248f3666aacbcc212247d21821a29a4cb
AlexGeere <alex.geere@gmail.com>
2025-06-23 10:44:00 +0200

    remove feature ids

--- mod/workspace/templates/_queries.js
+++ mod/workspace/templates/_queries.js
@@ -8,7 +8,6 @@ import clusterHex from './cluster_hex.js';
 import distinctValues from './distinct_values.js';
 import distinctValuesJson from './distinct_values_json.js';
 import featureExtent from './feature_extent.js';
-import featureIds from './feature_ids.js';
 import fieldMax from './field_max.js';
 import fieldMin from './field_min.js';
 import fieldMinmax from './field_minmax.js';
@@ -55,10 +54,6 @@ export default {
   feature_extent: {
     layer: true,
     render: featureExtent,
-  },
-  feature_ids: {
-    layer: true,
-    render: featureIds,
   },
   field_max: {
     template: fieldMax,
--- mod/workspace/templates/feature_ids.js
+++
@@ -1,3 +0,0 @@
-export default (_) => {
-  return `select \${qID} as id from ${_.table} where true \${filter};`;
-};
