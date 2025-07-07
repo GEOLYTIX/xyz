ee1e8263c1cddebf4fb4aaa9618fb4c919275d5f
AlexGeere <alex.geere@gmail.com>
2025-07-07 09:24:23 +0200

    fix tests and document query

--- mod/workspace/templates/layer_extent.js
+++ mod/workspace/templates/layer_extent.js
@@ -1,7 +1,18 @@
+/**
+### /workspace/templates/layer_extent
+
+The layer extent query method returns a bounding box that describes the extent of a range of geometries. 
+
+a body with ids can be supplied optionally to limit the geometries considered. 
+
+@module /workspace/templates/layer_extent
+*/
 export default (_) => {
   let condition = 'true';
 
+  //use ids where supplied
   if (Array.isArray(_.body?.ids)) {
+    //Remove ids with invalid characters
     const ids = _.body.ids.filter((id) => /^[A-Za-z0-9,"'._-\s]*$/.test(id));
 
     condition = `\${qID} IN ('${ids.join("','")}')`;
