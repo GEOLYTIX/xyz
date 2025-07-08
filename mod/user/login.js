6d0eee747df77171d445e32d303de3c335a49195
Rob Hurst <43169158+RobAndrewHurst@users.noreply.github.com>
2025-07-08 12:11:22 +0200

    correcting Content-type to Content-Type for consistency (#2261)

--- mod/user/login.js
+++ mod/user/login.js
@@ -93,7 +93,7 @@ async function loginBody(req, res) {
       return loginView(req, res);
     }
 
-    return res.status(401).set('Content-type', 'text/plain').send(user.message);
+    return res.status(401).set('Content-Type', 'text/plain').send(user.message);
   }
 
   const token = sign(
