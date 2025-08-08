e72245179dc5fa0ca454508f565573fcd580bae1
RobAndrewHurst <robert.hurst@geolytix.co.uk>
2025-08-08 09:35:57 +0200

    reorder tests

--- tests/browser/integrity/_integrity.test.mjs
+++ tests/browser/integrity/_integrity.test.mjs
@@ -2,6 +2,6 @@ import { layerTest } from './layer.test.mjs';
 import { workspaceTest } from './workspace.test.mjs';
 
 export async function integrityTests(mapview) {
-  await layerTest(mapview);
   await workspaceTest(mapview);
+  await layerTest(mapview);
 }
