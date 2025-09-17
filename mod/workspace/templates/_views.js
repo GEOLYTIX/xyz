17fd7806a07df14ab828cd3df63c3b901e0b4bde
Agata Brok <cityremade@users.noreply.github.com>
2025-09-17 11:24:48 +0200

    Login template now supporting registration view.

--- mod/workspace/templates/_views.js
+++ mod/workspace/templates/_views.js
@@ -10,16 +10,10 @@ export default {
     en: 'file:/public/views/_login.html',
   },
   password_reset_view: {
-    de: 'file:/public/views/register/_register_de.html',
-    en: 'file:/public/views/register/_register_en.html',
-    fr: 'file:/public/views/register/_register_fr.html',
-    ja: 'file:/public/views/register/_register_ja.html',
-    ko: 'file:/public/views/register/_register_ko.html',
-    pl: 'file:/public/views/register/_register_pl.html',
-    zh: 'file:/public/views/register/_register_zh.html',
+    en: 'file:/public/views/_login.html',
   },
   register_view: {
-    en: 'file:/public/views/_register.html',
+    en: 'file:/public/views/_login.html',
   },
   user_admin_view: {
     en: 'file:/public/views/_user.html',
