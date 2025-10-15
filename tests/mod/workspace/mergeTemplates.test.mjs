b900b767de084f9e2c259990cbf759fa18087333
RobAndrewHurst <robert.hurst@geolytix.co.uk>
2025-10-15 12:45:43 +0200

    additional mergeTemplate tests

--- tests/mod/workspace/mergeTemplates.test.mjs
+++ tests/mod/workspace/mergeTemplates.test.mjs
@@ -5,6 +5,10 @@ await codi.describe(
     parentId: 'workspace',
   },
   async () => {
+    const { default: mergeTemplates } = await import(
+      '../../../mod/workspace/mergeTemplates.js'
+    );
+
     await codi.it(
       {
         name: 'get template from workspace',
@@ -18,13 +22,43 @@ await codi.describe(
           },
         };
 
-        const { default: mergeTemplates } = await import(
-          '../../../mod/workspace/mergeTemplates.js'
-        );
-
         const template = await mergeTemplates(obj, null, false);
 
         codi.assertFalse(Object.hasOwn(template, 'style'));
+      },
+    );
+
+    await codi.it(
+      {
+        name: 'mergeTemplates with roles',
+        parentId: 'workspace_mergeTemplates',
+      },
+      async () => {
+        const obj = {
+          localeRole: 'locale',
+          role: 'layer',
+          name: 'Layer Name',
+          template: {
+            src: 'file:./tests/assets/layers/template_test/layer.json',
+            exclude_props: ['style'],
+            role: 'template',
+          },
+          infoj: [
+            {
+              name: 'You should see me',
+              edit: true,
+              role: 'edit',
+            },
+          ],
+        };
+
+        const roles = ['locale', 'layer', 'template'];
+
+        const template = await mergeTemplates(obj, roles, false);
+
+        delete template.template;
+
+        console.log(template);
       },
     );
   },
