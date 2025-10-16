a4fecc2c7966698658618794bfa8fd600b422ecc
Simon Leech <56163132+simon-leech@users.noreply.github.com>
2025-10-16 12:15:30 +0100

    3 levels of nesting roles test

--- tests/mod/workspace/mergeTemplates.test.mjs
+++ tests/mod/workspace/mergeTemplates.test.mjs
@@ -58,7 +58,65 @@ await codi.describe(
 
         delete template.template;
 
-        console.log(template);
+        // console.log(template);
+      },
+    );
+
+    await codi.it(
+      {
+        name: 'mergeTemplates with 3 levels nesting of roles',
+        parentId: 'workspace_mergeTemplates',
+      },
+      async () => {
+        // Inside the layer is a template which contains another template that is used to control whether or not the draw object is seen.
+        // The nested template is a separate file that has the "role": "layer_a".
+        // Within the nested template is two templates that have the "role": "draw_point" and "draw_circle" and provides a draw object with different properties.
+        const obj = {
+          localeRole: 'locale',
+          template: {
+            key: 'layer_a',
+            src: 'file:./tests/assets/layers/template_test/nested_templates.json',
+          },
+        };
+
+        const roles = [
+          'locale',
+          'locale.layer_a',
+          'locale.layer_a.draw_point',
+          'locale.layer_a.draw_circle',
+        ];
+
+        const template = await mergeTemplates(obj, roles, false);
+
+        delete template.template;
+
+        // Check template in template has the draw.point object.
+        codi.assertTrue(
+          Object.hasOwn(template, 'draw') &&
+            Object.hasOwn(template.draw, 'point'),
+          'Need to ensure draw.point is present from the merge',
+        );
+
+        // Check template in template has the draw.circle object.
+        codi.assertTrue(
+          Object.hasOwn(template, 'draw') &&
+            Object.hasOwn(template.draw, 'circle'),
+          'Need to ensure draw.circle is present from the merge',
+        );
+
+        // Check the roles object contains nested roles.
+        const expectedRoles = [
+          'locale.layer_a',
+          'layer_a.draw_point',
+          'locale.layer_a.draw_point',
+          'layer_a.draw_circle',
+          'locale.layer_a.draw_circle',
+        ];
+
+        codi.assertTrue(
+          expectedRoles.every((r) => Object.keys(template.roles).includes(r)),
+          `Need to ensure all expected roles are present: ${expectedRoles.join(', ')}`,
+        );
       },
     );
   },
