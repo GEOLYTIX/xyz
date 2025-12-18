e44ebf4c120b7946842f1ba1917f42b253ce8b98
Dennis Bauszus <dennis.bauszus@geolytix.co.uk>
2025-12-18 13:35:58 +0000

    Create assignLocaleLayer method in the layer entry module.

--- mod/workspace/templates/location_count.js
+++ mod/workspace/templates/location_count.js
@@ -6,6 +6,6 @@ The location_count layer query returns the count of table records which pass the
 @module /workspace/templates/location_count
 */
 export default `
-  SELECT count(*) as location_count
+  SELECT count(id) as location_count
   FROM \${table}
   WHERE true \${filter} \${viewport}`;
