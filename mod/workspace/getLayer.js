e6ce4e753a081dfa386dff30dbc32ccbe8d33580
Simon Leech <56163132+simon-leech@users.noreply.github.com>
2026-01-15 13:26:56 +0000

    Move duplicate code into `mergeTemplates`

--- mod/workspace/getLayer.js
+++ mod/workspace/getLayer.js
@@ -94,12 +94,10 @@ export default async function getLayer(params, locale) {
   // The roles property maybe assigned from a template. Templates must be merged prior to the role check.
   layer = await mergeTemplates(layer, params.user?.roles);
 
-  //If the user is an admin we don't need to check roles
-  if (!Roles.check(layer, params.user?.roles)) {
-    return new Error('Role access denied.');
+  // The mergeTemplates method returned an Error.
+  if (layer instanceof Error) {
+    return layer;
   }
-
-  layer = Roles.objMerge(layer, params.user?.roles);
 
   // Assign layer key as name with no existing name on layer object.
   layer.name ??= layer.key;
