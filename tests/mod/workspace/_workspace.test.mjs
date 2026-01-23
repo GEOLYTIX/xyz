862aab79d2738642b03297f3e3fd373d8d17c19c
Rob Hurst <robert.hurst@geolytix.co.uk>
2026-01-23 12:04:19 +0200

    update tests

--- tests/mod/workspace/_workspace.test.mjs
+++ tests/mod/workspace/_workspace.test.mjs
@@ -4,7 +4,7 @@ import checkWorkspaceCache from '../../../mod/workspace/cache.js';
 await codi.describe({ name: 'workspace:', id: 'workspace' }, async () => {
   globalThis.xyzEnv = {
     TITLE: 'WORKSPACE TEST',
-    WORKSPACE: 'file:./tests/assets/workspace_nested_locales.json',
+    WORKSPACE: 'file:./tests/assets/_workspace.json',
   };
 
   //Calling the cache method with force to reload a new workspace
@@ -43,139 +43,6 @@ await codi.describe({ name: 'workspace:', id: 'workspace' }, async () => {
           },
         );
       });
-    },
-  );
-
-  await codi.it(
-    { name: 'nested locales', parentId: 'workspace', id: 'workspace_locales' },
-    async () => {
-      const expectedLayers = ['OSM', 'brand_a_layer', 'brand_b_layer'];
-      const expectedKeys = ['europe', 'brand_a_locale', 'brand_b_locale'];
-      const expectedName = 'europe/brand_a_locale/brand_b_locale';
-
-      const { req, res } = codi.mockHttp.createMocks({
-        params: {
-          key: 'locale',
-          locale: ['europe', 'brand_a_locale', 'brand_b_locale'],
-          user: {
-            roles: ['europe', 'brand_b'],
-          },
-        },
-      });
-
-      await getKeyMethod(req, res);
-
-      let result = res._getData();
-
-      result = JSON.parse(result);
-
-      codi.assertEqual(
-        result.layers,
-        expectedLayers,
-        `We expect to get ${expectedLayers}, received: ${result.layers}`,
-      );
-
-      codi.assertEqual(
-        result.keys,
-        expectedKeys,
-        `We expect to get ${expectedKeys}, received: ${result.keys}`,
-      );
-
-      codi.assertEqual(
-        result.name,
-        expectedName,
-        `We expect to get ${expectedName}, received: ${result.name}`,
-      );
-
-      codi.assertTrue(
-        typeof result.checksum !== null,
-        'We expect to see a checksum on the locale ',
-      );
-    },
-  );
-
-  await codi.it(
-    { name: 'nested locales', parentId: 'workspace', id: 'workspace_locales' },
-    async () => {
-      const expectedMessage = 'Role access denied.';
-
-      const { req, res } = codi.mockHttp.createMocks({
-        params: {
-          key: 'locale',
-          locale: ['us', 'brand_a_locale', 'brand_b_locale', 'UK_locale'],
-          user: {
-            roles: ['us', 'brand_b'],
-          },
-        },
-      });
-
-      await getKeyMethod(req, res);
-
-      const message = res._getData();
-      const code = res.statusCode;
-
-      codi.assertEqual(code, 400, 'We expect to get a bad request.');
-
-      codi.assertEqual(
-        message,
-        expectedMessage,
-        'We should get a roles denial message',
-      );
-    },
-  );
-
-  await codi.it(
-    {
-      name: 'nested locales bogus roles',
-      parentId: 'workspace',
-      id: 'workspace_locales',
-    },
-    async () => {
-      const expectedMessage = 'Role access denied.';
-      const { req, res } = codi.mockHttp.createMocks({
-        params: {
-          key: 'locale',
-          locale: ['europe', 'brand_a_locale', 'brand_b_locale'],
-          user: {
-            roles: ['us', 'brand_b'],
-          },
-        },
-      });
-
-      await getKeyMethod(req, res);
-
-      const message = res._getData();
-      const code = res.statusCode;
-
-      codi.assertEqual(code, 400, 'We expect to get a bad request.');
-
-      codi.assertEqual(
-        message,
-        expectedMessage,
-        'We should get a roles denial message',
-      );
-    },
-  );
-
-  await codi.it(
-    {
-      name: 'nested locales bogus locale',
-      parentId: 'workspace',
-      id: 'workspace_locales',
-    },
-    async () => {
-      const { req, res } = codi.mockHttp.createMocks({
-        params: {
-          key: 'locale',
-          locale: ['notALocale', 'anotherincorrectone', 'Idontexist'],
-        },
-      });
-
-      await getKeyMethod(req, res);
-
-      const code = res.statusCode;
-
-      codi.assertEqual(code, 400, 'We expect to get a bad request.');
     },
   );
 });
