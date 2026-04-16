4724bb0cd4ec514cfdf13ad197355bffefd44dc5
Dennis Bauszus <dennis.bauszus@geolytix.co.uk>
2026-04-16 16:14:13 +0100

    move vitest.config.mjs to root

---
+++ vitest.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['apps/xyz/tests/mod/**/*.test.mjs', 'apps/xyz/tests/plugins/**/*.test.mjs'],
+    exclude: ['apps/xyz/tests/lib/**', 'apps/xyz/tests/browser/**', 'apps/xyz/tests/_mapp.test.mjs'],
+    setupFiles: ['apps/xyz/tests/setup.mjs'],
+    testTimeout: 10000,
+    fileParallelism: true,
+    coverage: {
+      provider: 'v8',
+    },
+  },
+});
