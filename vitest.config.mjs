e69a0a3a464551765f935396c43d7277b99c37c5
Robert Hurst <robert.hurst@geolytix.co.uk>
2026-04-20 09:14:13 +0200

    fix test references

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,17 +1,21 @@
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vitest/config';
 
+const rootDir = fileURLToPath(new URL('.', import.meta.url));
+
 export default defineConfig({
+  root: rootDir,
   test: {
     include: [
-      'apps/xyz/tests/mod/**/*.test.mjs',
-      'apps/xyz/tests/plugins/**/*.test.mjs',
+      './apps/xyz/tests/mod/**/*.test.mjs',
+      './apps/xyz/tests/plugins/**/*.test.mjs',
     ],
     exclude: [
-      'apps/xyz/tests/lib/**',
-      'apps/xyz/tests/browser/**',
-      'apps/xyz/tests/_mapp.test.mjs',
+      './apps/xyz/tests/lib/**',
+      './apps/xyz/tests/browser/**',
+      './apps/xyz/tests/_mapp.test.mjs',
     ],
-    setupFiles: ['apps/xyz/tests/setup.mjs'],
+    setupFiles: ['./apps/xyz/tests/setup.mjs'],
     testTimeout: 10000,
     fileParallelism: true,
     coverage: {
